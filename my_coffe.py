dd9e8c23b244186c97e9e15816ad8f3ada9e0131
goxzero8 <goxzero8@gmail.com>
2026-03-12 11:08:38 -0300

    mi primer programa de python

---
+++ my_coffe.py
@@ -0,0 +1,16 @@
+producto1 = "1. Expreso"
+producto2 = "2. moca"
+producto3 = "3. te"
+producto4 = "4. whisky coffe"
+bebida_seleccionada = input("que bebida desea?")
+
+
+print ("================BLUES AND JAZZ COFFE==========")
+
+print (producto1)
+print (producto2)
+print (producto3)
+print (producto4)
+
+
+
