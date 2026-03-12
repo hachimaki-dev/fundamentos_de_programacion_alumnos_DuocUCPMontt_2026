03a04c6287286999f802f253de8c02eb9a67f3a0
Nerok001 <pablom15khaos@gmail.com>
2026-03-12 16:35:45 -0300

    Mi primer intento de programa python

---
+++ Maquina_expendedora.py
@@ -0,0 +1,18 @@
+opcion1 = "Café"
+opcion2 = "Té"
+opcion3 = "Milo"
+opcion4 = "Capuccino"
+
+
+
+
+print("===Bienvenido a Cafe==")
+
+print("Opcion 1:" + opcion1)
+print("Opcion 2:" + opcion2)
+print("Opcion 3:" + opcion3)
+print("Opcion 4:" + opcion4)
+seleccion = input()
+
+
+print("*****FIN DE PROGRAMA*****")
