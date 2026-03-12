ecff8262c93799b0cd332a2045aae1ad6bc42d06
Shiekd-L <ordonezbenja22@gmail.com>
2026-03-12 16:35:34 -0300

    mi primer programa de python

---
+++ maquina.py
@@ -0,0 +1,13 @@
+opcion1 = "cafe"
+opcion2 = "te"
+opcion3 = "milo" 
+opcion4 = "late"
+
+print("###BIENVENIDO### ")
+
+print("Opcion1:" + opcion1)
+print("opcion2")
+print("opcion3")
+print("opcion4")
+
+print("=====fin======")
