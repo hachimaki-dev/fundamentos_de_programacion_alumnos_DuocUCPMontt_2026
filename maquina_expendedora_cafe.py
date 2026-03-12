166cbc08ea0066662e3a129582f0255cd2f87aea
4l3xZzz <alexandervillalon54@gmail.com>
2026-03-12 11:09:12 -0300

    mi primer programa de python

---
+++ maquina_expendedora_cafe.py
@@ -0,0 +1,17 @@
+producto1 = "Café"
+producto2 = "Té"
+producto3 = "Milo"
+
+print("============= LOS PRODUCTOS DISPONIBBLES SON ==============")
+print(producto1)
+print(producto2)
+print(producto3)
+
+print("============= POR FAVOR SELECCIONE SU BEBIDA ==============")
+
+
+
+bebida_seleccionada = input("Ingrese número de BEBIDAclear")
+
+print("VAMOS A VERIFICAR LA OPCIÓN DEL CLIENTE")
+print(bebida_seleccionada)
