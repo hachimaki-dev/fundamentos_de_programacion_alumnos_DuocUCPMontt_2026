394e50b065b8c56fd44f25f49dbb5ab4cb407595
Batman <batmanjohan12@gmail.com>
2026-03-12 16:35:30 -0300

    Mi primer programa de python gracias profe

---
+++ maquina_expendedora.py
@@ -0,0 +1,14 @@
+opcion1 = "café"
+opcion2 = "té"
+opcion3 = "milo"
+
+print("Bienvenido a café sabrosura")
+
+
+print(opcion1)
+print(opcion2)
+print(opcion3)
+print("por favor seleccione su producto")
+seleccion_de_producto = input("Ingrese su opcion: ")
+
+print("FIN del programa")
