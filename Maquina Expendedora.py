15acbe0ff691e0fbdb6a234ed971671083cb5d3c
Amaro2039 <ovandoamaro0@gmail.com>
2026-03-12 16:35:32 -0300

    mi primer programa de python

---
+++ Maquina Expendedora.py
@@ -0,0 +1,14 @@
+opcion1 = "cafe"
+opcion2 = "Te"
+opcion3 = "Milo"
+opcion4 = "capuccino"
+print ("===bienvenido a cafe bonito===")
+
+print("opcion 1: " + opcion1)
+print("opcion 2: " + opcion2)
+print("opcion 3: " + opcion3)
+print("opcion 4: " + opcion4) 
+print("por favor seleccione su cafe")  
+seleccion_de_bebida = input("Ingrese su opcion:")
+print("la opcion del usuario fue:")
+print("fin del programa")  
