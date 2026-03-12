eeee25de4603dc62773eccf5f1e61f072df4a321
gabriela-pezo <gabrielapezo69@gmail.com>
2026-03-12 16:36:39 -0300

    miprimer programa d python

---
+++ maquina_expendedora.py
@@ -0,0 +1,27 @@
+opcion1 = "cafe"
+opcion2 = "te"
+opcion3 = "milo"
+opcion4 = "capuccino  malo"
+
+
+
+
+
+print("===Bienvenido a cafe feo===")
+print("opcion 1: " + opcion1)
+
+print("opcion 2: " + opcion2)
+
+print("opcion 4: " + opcion4)
+
+print("opcion 3: " + opcion3)
+print ("porfavor seleccione su cafe")
+
+seleccion_de_bebida = input("ingrese su opcion: ")
+
+print(" la opcion del usuario fue: " + seleccion_de_bebida)
+
+
+
+print("*****FIN DEL PROGRAMA*****")
+
