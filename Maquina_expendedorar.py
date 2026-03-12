17ed1b6cce621d06d736c7f8253a1c85b91c318a
Alvas-cod3 <alvargonz507@gmail.com>
2026-03-12 16:35:32 -0300

    mi primer programa de phyton

---
+++ Maquina_expendedorar.py
@@ -0,0 +1,13 @@
+opcion1 = "Café"
+opcion2 = "Té"
+opcion3 = "Capuccino rico"
+
+print("=====Bienvenido al Cafe Peruano=====")
+print("opcion1: " + opcion1)
+print("opcion2: " + opcion2)
+print("opcion3: " + opcion3)
+print("Seleccione su cafè")
+
+seleccion_de_bebida = input("ingrese su opcion: ")
+print("la opcion del usuario fue: " + seleccion_de_bebida)
+print("****FIN DEL CAFE****")
