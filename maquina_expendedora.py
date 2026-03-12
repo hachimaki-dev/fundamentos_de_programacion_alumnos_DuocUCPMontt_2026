45c0b28fcea031a1a20b1d28524834c02a5fc0dd
JoakoHT <hjoaquin115@gmail.com>
2026-03-12 16:36:38 -0300

    MI PRIMERA MAQUINA EXPENDEDORA

---
+++ maquina_expendedora.py
@@ -0,0 +1,17 @@
+opcion1 =  "cafe"
+opcion2 =  "té"
+opcion3 = "capuccino rico"
+
+print("===Bienvenido a cafe Bonito===")
+
+print( "opcion 1: " + opcion1)
+print( "opcion 2: " + opcion2)
+print("opcion 3: " + opcion3)
+print("Por favor seleccione su cafe")
+
+seleccion_de_bebida = input("ingrese su opcion")
+
+print("La opcion del usuario fue: " + seleccion_de_bebida)
+
+
+print("****FIN DEL PROGRAMA****")
