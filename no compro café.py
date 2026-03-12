b688c59f18aaa1e976677d5b5b2ac631c855a3bc
Happy210906 <olaguierpro@gmail.com>
2026-03-12 16:35:27 -0300

    No tomo mucho café

---
+++ no compro café.py
@@ -0,0 +1,10 @@
+print ("===Bienvenido a café bonito ===")
+
+print ("Café")
+print ("Capucchino")
+print ("Milo")
+print ("Chocolate con leche")
+print ("Chocolate fuerte")
+print ("Té")
+
+print ("===Fin del programa===")
