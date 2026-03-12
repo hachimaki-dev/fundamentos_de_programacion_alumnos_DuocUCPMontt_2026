a461c447fefc9a94d2ef1a9049689b05a7eaf009
fe.macias <fernando.macias19@icloud.comgit config --global user.name fernando.macias19@icloud.com>
2026-03-12 11:07:02 -0300

    mi primer programa en python

---
+++ maquina.py
@@ -0,0 +1,11 @@
+p1 = "Cafe"
+p2 = "Te"
+p3 = "Milo"
+print("productos disponible")
+print(p1)
+print(p2)
+print(p3)
+print("===Escoja una de las tres opciones===")
+input ("Ingrese opcion : ")
+p_select = input ("Ingrese opcion : ")
+print(p_select)
