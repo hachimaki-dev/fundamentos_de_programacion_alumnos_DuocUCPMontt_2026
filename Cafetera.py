64409a4512d20ea1b3eadbd61a56403d13923178
4zocar <pixi84527@gmail.com>
2026-03-12 16:36:41 -0300

    mi primer pograma en python

---
+++ Cafetera.py
@@ -0,0 +1,19 @@
+opcion1 = "Cafe"
+opcion2 = "Te"
+opcion3 = "Milo"
+opcion4 = "Capuccino"
+
+print("===Bienvenido a la Cafetera====")
+
+print("opcion 1: " + opcion1)
+print("opcion 2: " + opcion2)
+print("opcion 3: " + opcion3)
+print("opcion 4: " + opcion4)
+
+print("Por Favor seleccione su cafe")
+
+seleccion_de_bebida - input("ingrese su opcion: ")
+
+print("La ocpion del usuario fue: " + seleccion de bebida)
+
+print("***FIN DEL PROGRAMA***")
