59f7ab6a9aa75f451423e20d103f58dd82c71233
Gtvq32 <gtvq1718x@gmail.com>
2026-03-12 16:36:43 -0300

    Mi primer programa de phyton

---
+++ maquina_expendedora.py
@@ -0,0 +1,17 @@
+OPCION1="Café"
+OPCION2="Milo"
+OPCION3="Té"
+
+
+print("Bienvenido a cafe bonito")
+
+print("Opcion 1 : " + OPCION1)
+print("Opcion 2 : " + OPCION2)
+print("Opcion 3 : " + OPCION3)
+print("Por favor seleccione su café")
+
+Seleccion_de_producto = input("Ingrese su opcion: ")
+
+print("La opcion del usuario fue : " + Seleccion_de_producto) 
+
+print("Fin del programa")
