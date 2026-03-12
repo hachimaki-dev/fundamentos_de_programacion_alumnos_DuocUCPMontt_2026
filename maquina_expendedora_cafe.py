a60f0aa0184761ce93dc9b4fc30360cba641f721
figueroawa <selena.ale.figueroa@gmail.com>
2026-03-12 11:07:09 -0300

    Máquina expendedora de café en Python

---
+++ maquina_expendedora_cafe.py
@@ -0,0 +1,14 @@
+# Ejemplo de programa de máquina expendedora de café (WIP)
+
+producto1 = "Café"
+producto2 = "Té"
+producto3 = "Milo"
+
+print("Los productos disponibles son:")
+print("1. " + producto1)
+print("2. " + producto2)
+print("3. " + producto3)
+print("Por favor seleccione su bebida.")
+
+bebida_seleccionada = input("Ingrese número de bebida: ")
+print(bebida_seleccionada)
