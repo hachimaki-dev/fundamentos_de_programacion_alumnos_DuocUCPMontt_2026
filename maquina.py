4b12ff3c83d222f02d20945da11f050fc9928ac4
Shiekd-L <ordonezbenja22@gmail.com>
2026-03-12 16:37:23 -0300

    mi primer programa de python

--- maquina.py
+++ maquina.py
@@ -6,8 +6,10 @@ opcion4 = "late"
 print("###BIENVENIDO### ")
 
 print("Opcion1:" + opcion1)
-print("opcion2")
-print("opcion3")
-print("opcion4")
+print("Opcion2" + opcion2)
+print("Opcion3" + opcion3)
+print("Opcion4" + opcion4)
 
+seleccion_de_bebida = input("ingrese su opcion:")
+print("la opcion del usuario fue:" + seleccion_de_bebida)
 print("=====fin======")
