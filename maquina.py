ce76457a654202198f15aa3005a2beb4e1f0472e
maximobv <barrigamaximo845@gmail.com>
2026-03-12 16:41:10 -0300

    hola]

---
+++ maquina.py
@@ -0,0 +1,19 @@
+opcion1 = "cafe"
+opcion2 = "milo"
+opcion3 = "capuccino"
+opcion4 = "te"
+
+print("===bienvenido===")
+
+print(opcion1)
+print(opcion2)
+print(opcion3)
+print(opcion4)
+
+print("porfavor seleccione su cafe)") 
+
+seleccion de productos = input(ingrese su opcion)
+
+print("la opcion del usuario fue: "  )
+
+print("===FIN DEL PROGRAMA===")
