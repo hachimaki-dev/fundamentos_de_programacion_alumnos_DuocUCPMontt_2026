5238f3fa995df3d3a1bc79f09a3949fe258091f8
BlazingFunk2005 <mtkonar@gmail.com>
2026-03-12 16:35:09 -0300

    Maquina expendedora de cafe en python!

---
+++ maquina_de_cafe.py
@@ -0,0 +1,16 @@
+opcion1 = "Café"
+opcion2 = "Té"
+opcion3 = "Milo"
+opcion4 = "Capuccino"
+
+print("- - - Bienvenido a Cafe & Gatos! - - -")
+
+print("Opcion 1:", opcion1)
+print("Opcion 2:", opcion2)
+print("Opcion 3:", opcion3)
+print("Opcion 4:", opcion4)
+
+seleccion = input("Profavor seleccione su cafe: ")
+print("La seleccion del usuario fue", seleccion)
+
+print("*||* FIN *||*")
