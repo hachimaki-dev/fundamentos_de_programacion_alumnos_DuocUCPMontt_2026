e6cbbba97978903a49ff7878dc2e0bd6dc970464
Evelin-vidal <evelynpatriciavidalbustamante@gmail.com>
2026-03-12 16:35:29 -0300

    Mi primer programa de python

---
+++ Maquina_expendedora.py
@@ -0,0 +1,15 @@
+
+opcion1 = "cafe"
+opcion2 = "te"
+opcion3 = "capuccino rico"
+
+print ("==Bienvenido a Cafe Bonito===")
+
+print ("opcion 1: " + opcion1)
+print ("opcion 2: " + opcion2)
+print ("opcion 3: " + opcion3)
+print ("porfavor seleccione su cafe")
+
+Seleccion_de_bebida = input("Ingrese su opcion: ")
+print ("La opcion del usuario fue: " + Seleccion_de_bebida)
+print ("*****FIN DEL PROGRAMA*****")
