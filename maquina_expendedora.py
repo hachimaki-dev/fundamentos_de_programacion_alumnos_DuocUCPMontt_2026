afe8b78296881f9ff4624e18297052a7ddc86fbb
HachiMaki-dev <hachimakidev@gmail.com>
2026-03-12 16:35:28 -0300

    Mi primer y hermoso programita de Python

---
+++ maquina_expendedora.py
@@ -0,0 +1,16 @@
+opcion1 = "Cafécito"
+opcion2 = "Té"
+opcion3 = "Capuccino rico"
+
+print("===Bienvenido a Cafe Bonito===")
+
+print("Opcion 1: " + opcion1)
+print("Opcion 2: " + opcion2)
+print("Opcion 3: " + opcion3)
+print("Por favor seleccion su cafe")
+
+seleccion_de_bebida =  input("Ingrese su opcion: ")
+
+print("LA opcion del usuario fue: " + seleccion_de_bebida)
+
+print("*****FIN DEL PROGRAMA*****")
