212a23429c789a6b1d95ca92a559e0eac8ae1224
Diego <ignacionacho2000x@gmail.comgit config --global user.name Diegogit push origin 003d/clase1_Diego git push origin 003D/clase1_Diegogit config --global user.email ignacionacho2000x@gmail.com>
2026-03-12 16:38:37 -0300

    Mi primer maquina expendedora

---
+++ maquina_expendedora.py
@@ -0,0 +1,19 @@
+opcion1 = "cafe"
+opcion2 = "Te"
+opcion3 = "Capuccino Rico"
+
+print("===Bienvenido a cafe bonito===")
+
+print(  "Opcion 1 : " + opcion1)
+print(  "Opcion 2 : " + opcion2)
+print(  "Opcion 3 : " + opcion3)
+print("Por favor seleccione su cafe")
+
+seleccion_de_bebida = input("Ingrese su accion: ")
+
+print("la opcion del usuario fue: " + seleccion_de_bebida)
+
+
+
+
+print("***** Fin del Programa*****")
