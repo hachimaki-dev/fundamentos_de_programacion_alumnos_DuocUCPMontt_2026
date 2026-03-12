4b6288f375195b6815afd11f6c1409f4c2afdb08
bisente777 <vic.aponte@duocuc.cl>
2026-03-12 16:38:44 -0300

    noseqseponia

---
+++ comoustedequieran.py
@@ -0,0 +1,17 @@
+opcion1 = "cafe"
+opcion2 = "te"
+opcion3 = "capuccino rico"
+
+
+print("===Bienvenido a cafe bonito===")
+
+print("opcion 1: " + opcion1)
+print("opcion 2: " + opcion2)
+print("opcion 3: " + opcion3)
+
+
+
+print(" la opcion escogida fue: " + seleccion_de_bebida)
+seleccion_de_bebida = input("ingrese su opcion:")
+print ("por favor seleccione su cafe")
+print ("*****fin del programa*****")
