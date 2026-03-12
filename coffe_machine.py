081d35009b83876ea58b393f9dbe88af72508e6d
RubiZero <abcmudkip@gmail.com>
2026-03-12 16:41:57 -0300

    Desarrollando_maquina_cafe

---
+++ coffe_machine.py
@@ -0,0 +1,17 @@
+opcion1 = "Cafè"
+opcion2 = "Tè"
+opcion3 = "Capuchino rico"
+
+print ("===Bienvenido a Cafe Bonito")
+
+print ( "Opcion 1: " +"Cafè")
+print ( "Opcion 2: " +"Tè")
+print ( "Opcion 3: " +"Milo")
+print ( "Opcion 4: " +"Capuchino rico")
+print ("Por favor seleccione su cafe")
+
+seleccion_de_bebida =  input("Ingrese su opcion")
+
+print("La opcion del usuario fue: " + seleccion_de_bebida)
+
+print ("*****FIN DEL PROGRAMA*****")
