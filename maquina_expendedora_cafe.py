1e69b64366e3f1cfec093b7c57b9c50321c3c48f
Martin Muñoz <martin.andres.munoz.c@gmail.com>
2026-03-12 11:07:01 -0300

    mi primer programa de python

---
+++ maquina_expendedora_cafe.py
@@ -0,0 +1,11 @@
+producto1 = "1. Expreso"
+producto2 = "2. Té"
+producto3 = "3. Milo"
+
+print("----- Productos disponibles -----")
+print(producto1)
+print(producto2)
+print(producto3)
+
+bebida_seleccionada = int(input("Seleccione el número de bebida: "))
+print(bebida_seleccionada)
