8ae7074c21b1d451a823335df12ce6df106a2866
Neiram374 <neiramatias553@gmail.com>
2026-03-12 16:37:04 -0300

    mi primer programa de pytohn

---
+++ maquina_expendedora.py
@@ -0,0 +1,21 @@
+opcion1 = "cafe"
+opcion2 = "te"
+opcion3 = "milo"
+opcion4 = "expreso"
+
+
+
+print("==Bienvenido a cafe backdraft==")
+
+print("opcion 1: " + opcion1)
+print("opcion 2: " + opcion2)
+print("opcion 3: " + opcion3)
+print("opcion 4: " + opcion4)
+print("por favor seleccione su café")
+
+seleccion_de_bebida = input ("ingrese su opción:")
+
+print("la opcion del usuario fue: " + seleccion_de_bebida)
+
+
+print("==Fin del programa==")
