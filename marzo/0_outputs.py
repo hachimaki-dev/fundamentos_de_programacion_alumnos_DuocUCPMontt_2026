eee411d53cc66b33953c52f8834f3ba89eb8c46c
figueroawa <selena.ale.figueroa@gmail.com>
2026-03-16 13:35:55 -0300

    Ordenar archivos en carpetas ordenadas por mes y añadir nuevas tareas

---
+++ marzo/0_outputs.py
@@ -0,0 +1,15 @@
+# En Python, se pueden hacer comentarios con "#"
+print("Hello World")
+
+# Los print pueden concatenar
+print("Hola " + ":D")
+
+usuario1 = "Pepito"
+usuario2 = "Maria"
+usuario3 = "Alan"
+
+print("El primer usuario es " + usuario1 + ", el segundo usuario es " + usuario2 + " y el tercer usuario es " + usuario3 + ".")
+
+# Forma más elegante: INTERPOLACIÓN
+# Permite imprimir el valor de una variable dentro de una cadena de texto
+print(f"El primer usuario es {usuario1}, el segundo usuario es {usuario2} y el tercer usuario es {usuario3}.")
