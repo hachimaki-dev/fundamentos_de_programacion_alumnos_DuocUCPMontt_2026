c118a4830bc22b8d0503768ce7c97aff34ab08ed
Her-cloud <carcamoh78@gmail.com>
2026-03-16 10:32:36 -0300

    en esta clase aprendi a usar python basico

---
+++ 0_diagrama_suma.py
@@ -0,0 +1,13 @@
+#vamos a recrear el diagrama de flujo que suma dos numeros 
+
+numero1 = input("por favor ingrese numero 1")
+
+numero2 = input("ingrese numero 2")
+
+resultado = numero1 + numero2 
+
+resultado = int(numero1) + int(numero2)
+
+print("el resultado es:")
+print(resultado)
+
---
+++ 0_inpust.py
@@ -0,0 +1,8 @@
+#la funcion input permite al usuario ingresar datos por teclado
+
+
+
+opcion_elegida = input("ingrese algo:")
+
+print("La opccion que el usuario Eligio es")
+print(opccion_elegida)
