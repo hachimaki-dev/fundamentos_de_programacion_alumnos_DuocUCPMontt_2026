c8846bada91891bbb9d1beb43afc974aac694c0b
LEANDRO-JARA <jaraleandro77@gmail.com>
2026-03-16 18:03:22 -0300

    En esta clase aprendimos a emprimir en el print aprendimos a concatenar y aprendimos lo que es la interpolacion de cadenas de texto, ademas vimos el uso de input y el parseo e el int

---
+++ 0_outputs.py
@@ -0,0 +1,21 @@
+#Esto es un output básico
+print("hola mundo")
+
+#Estos son variables y puedo guardar lo que me de en gana
+
+usuario_1 = "juan"
+usuario_2 = "Matias"
+usuario_3 = "Yosmer"
+
+es_mayor_de_edad = True
+estatura_usuario_1 = 170.5
+edad_usuario_1 = 27
+
+
+#A continuacion aprenderemos a concatenar
+#Con mis propias palabras explicar lo que netinedo con concatenar
+    #es encadenar codigo como usuario_1 + usuaario_2 etc
+
+#hay una fomrma mas elegante 
+print(f"el nombre del primer usuario es {usuario_1} y el nombre del segundo usuario es {usuario_2} y el nombre del tercer usuario {usuario_3}")      
+
---
+++ 1_calucladora_sumar.py
@@ -0,0 +1,4 @@
+numero_1 = input("ingrese el primer numero: ")
+numero_2 = input("ingrese el seguno numero: ")
+resultado = int(numero_1) + int(numero_2)
+print(f"El resultado de la suma es {resultado}")
