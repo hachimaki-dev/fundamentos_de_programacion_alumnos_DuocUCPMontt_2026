535767dc7aae2c147aff9a39c076c7f10ad4c76c
BlazingFunk2005 <mtkonar@gmail.com>
2026-03-16 18:03:15 -0300

    Calculadora basica en Python

---
+++ 1_calculadora_sumar.py
@@ -0,0 +1,4 @@
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+resultado = num1 + num2
+print(f"El resultado de la suma es {resultado}")
---
+++ 1_outputs.py
@@ -0,0 +1,5 @@
+# Input basico!!!
+
+print("Bienvenido al recorrido Puerto Montt - Alerce")
+eleccion = input("A donde desea ir?: ")
+print(f"Usted a elejido ir a '{eleccion}'!")
