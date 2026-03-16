23aecd3ba64902d90584ba4fe88684cd06882ae9
LeCoward <sebabarri2710@gmail.com>
2026-03-16 15:12:19 -0300

    Clase 4 input output D.F

---
+++ 0_outputs.py
@@ -0,0 +1,19 @@
+# gato para apuntes que no se muestran al ejecutar el codigo
+print("Oyasumin Za Wardo")
+
+# Los print's pueden concatenar
+print("Hola " + "c:")
+
+# Variables
+
+usuario1 = "Pepito"
+usuario2 = "Maria"
+usuario3 = "Alan"
+
+print(" El primer usuario es " + usuario1 + " y el segundo usuario es " + usuario2 + " y el tercer usuario es " + usuario3)
+
+# Forma más elegante
+# INTERPOLACIÓN
+print(f"El primer usuario es {usuario1}, y el segundo usuario es {usuario2} y el tercer usuario es {usuario3}")
+
+# Crear programa
---
+++ RecorridoAlercePuertomontt.py
@@ -0,0 +1,5 @@
+print("Bienvenido al recorrido Alerce-Puerto Monnt mi estimado.")
+
+Destino_Usuario = input("¿Cuál es su destino el día de hoy?")
+
+print("Su destino seleccionado es " + Destino_Usuario)
