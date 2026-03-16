3fd1c570bb6029dafbfff3a7a36c919a10c30533
figueroawa <selena.ale.figueroa@gmail.com>
2026-03-16 14:08:43 -0300

    Renombrar archivos y añadir tarea: Suma de dos números

---
+++ marzo/03_suma_de_dos_numeros.py
@@ -0,0 +1,9 @@
+# Sumar 2 números y mostrarle el resultado al usuario, basado en el diagrama de flujo visto en clase.
+# Este programa usa parsing de cáracteres a números.
+
+numero1 = input("Ingrese su primer número: ")
+numero2 = input("Ingrese su segundo número: ")
+
+resultado = int(numero1) + int(numero2)
+
+print(f"\nEl resultado de la suma entre {numero1} y {numero2} es {resultado}.")
