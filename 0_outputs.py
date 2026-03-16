ac1966523c86b0366533bfa67b77e1410baa3530
gabriela-pezo <gabrielapezo69@gmail.com>
2026-03-16 18:08:01 -0300

    en esta clase aprendimos a imprimir con el print aprendimos a concatenar y aprendimos lo q es la interpolacion de cadenas de texto y ademas vinos el uso de input y el parseo o transformacion de texto a enteros y tmbn un poco de diagramas de flujo

---
+++ 0_outputs.py
@@ -0,0 +1,18 @@
+#este es un output basiko
+print("hello world")
+
+#esto son variables y puedo guardar lo que quiera
+usuario_1 = "Kayn"
+usuario_2 = "Kaisa"
+usuario_3 = "Teemo"
+
+es_mayor_de_edad = True
+estatura_usuario_1 = 170.5
+edad_usuario_1 = 27
+
+#a continuacion aprenderemos a concatenar
+#con mis propias palabras explicar lo que entiendo por contatenar 
+print("el nombre del primer usuario es " + usuario_1 + " y el nombre de segundo usuario es " + usuario_2 + " y el nombre del tercer usuario es " + usuario_3)
+
+#hay una forma mas elegante
+print(f"el nombre del primer usuario es {usuario_1} y el nombre del segundo usuario es {usuario_2} y el nombre del tercer usuario es {usuario_3}")
---
+++ 1_inputs.py
@@ -0,0 +1,4 @@
+#los inputs son todas aquellas acciones que recibe el sistema por parte del usuario
+nombre_del_usuario = input("Porfavor ingrese su nombre de usuario")
+print(f"el nombre del usuario es {nombre_del_usuario}")
+      
