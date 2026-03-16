af981452798d6536cad82a1940e5603518ebd629
bisente777 <vic.aponte@duocuc.cl>
2026-03-16 18:05:44 -0300

    en esta clase aprendi diagrama de flujos, parce, y mas cosas xd

---
+++ 0_outputs.py
@@ -0,0 +1,18 @@
+#este es un output basico
+print("hello world")
+
+#Esto son variables y puedo guardar lo que me de en gana
+usuario_1 = "juan"
+usuario_2 = "matias"
+usuario_3 = "yosmer"
+
+es_mayor_de_edad = True
+estatura_usuario_1 = 170.5
+edad_usuario_1 = 27
+
+#a continuacion aprenderemos a concatenar
+#es unir variables en una cadena de texto
+print ("el mombre del primer usuario es " + usuario_1 + " el nombre del segundo usuario es " + usuario_2 + " el nombre del tercer usuario es " + usuario_3)
+
+#otra forma mas elegante, esta es la interpolacion de variables
+print(f"el nombre del primer usuario es {usuario_1} y el nombre del segundo usuario es {usuario_2} y el nombre del tercer usuario es {usuario_3}")
---
+++ 1_calculadora_sumar.py
@@ -0,0 +1,5 @@
+print("calculadora")
+numero_1 = input("ingrese numero 1")
+numero_2 = input("ingrese numero 2")
+resultado =int(numero_1) + int(numero_2)
+print(f"el resultado de la suma es"{resultado})
