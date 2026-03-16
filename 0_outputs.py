05eca7e146d277f5d33e7717575e209a70def29e
Tomy30b <barriatomy2@gmail.com>
2026-03-16 18:07:35 -0300

    En este clase aprendimos a imprimir con el print, aprendimos a concatenar y aprendimos que es la interpolacion de cadenas de texto, igual hicimos una calculadora en python B)

---
+++ 0_outputs.py
@@ -0,0 +1,17 @@
+#Este es un output basico
+print("Hola a todos")
+
+usuario_1 = "juan"
+usuario_2 = "Matias"
+usuario_3 = "Yosmer"
+
+es_mayor_de_edad = True
+
+estatura_usuario_1 = 170.5
+edad_usuario_1 = 27
+
+print("el nombre del primer usuario es" + usuario_1 + "el nombre del segundo usuario es " + usuario_2 + "el nombre del tercer usuario es" + usuario_3)
+
+#hay una forma mas elegante
+
+print(f"El nombre del primer usuario es {usuario_1} y el nombre del segundo usuario es {usuario_2} y el nombre del tercer usuario es {usuario_3}")
---
+++ 1_input.py
@@ -0,0 +1,8 @@
+#los inputs son todas aquellas acciones que recibe el sistema por parte del usuario
+
+
+#crear un programa
+
+print("hola bienvenido al recorrido puerto montt alerce")
+Respuesta_del_usuario = input("Cual es su destino")
+print(f"el usuario quiere ir a {Respuesta_del_usuario}")
