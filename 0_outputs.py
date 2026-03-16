7c2e78a27efe1998a835a8284df72a3f79bbdc2b
KAT-000 <sebastianignaciocorderospier@gmail.com>
2026-03-16 18:04:59 -0300

    hoy aprendi a imprimir, a concatenar, la interpolacion, el input, el output, etccc...

---
+++ 0_outputs.py
@@ -0,0 +1,18 @@
+#Esto es un comentario, sirve para dejar un mensaje en el proyecto, el codigo lo ignora completamente.
+#Esto es un output basico, el output es un comando donde el programa te entrega informacion.
+#print, sirve para imprimir un mensaje. (TODAS LOS LENGUAJES USAN PARENTESIS!!)
+print("¡Hola!")
+
+usuario_1 = "Seba"
+usuario_2 = "Matias"
+usuario_3 = "Yosmer"
+
+es_mayor_de_edad = True
+estatura_usuario_1 = 170.5
+edad_usuario_1 = 21
+
+#A continuacion aprenderemos a concatenar, concatenar es unir variables en una cadena de texto.
+print("el nombre del primer usuario es " + usuario_1 + " y el nombre del segundo usuario es " + usuario_2 + " ,el tercer usuario se llama" + usuario_3)
+
+#Hay una manera mas elegante, esta es la interpolacion de variables
+print(f"el nombre del primer usuario es {usuario_1} y el nombre del segundo usuario es {usuario_2} ,el tercer usuraio se llama {usuario_3}" )
---
+++ 1_inputs.py
@@ -0,0 +1,14 @@
+#Los inputs son todas aquellas acciones que recibe el sistema por parte del usuraio u otro sistema
+print("Bienvenido al recorrido de buces!!")
+
+nombre_de_usuario = input("Porfavor ingrese su nombre de usuario:")
+
+print(f"¡¡¡Bienvenido al terminal de buces {nombre_de_usuario} !!!")
+
+edad = input("Por favor ingresa tu edad:")
+
+if edad == "<= 18:"
+    print("Disfrute el recorrido!!")
+else:
+    if edad == ">= 18:"
+        print("Lo sentimos pero necesita a un adulto para ingresar")
