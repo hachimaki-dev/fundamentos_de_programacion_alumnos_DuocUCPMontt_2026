7082093c148d5ec3a61ee263689aec2c7a9e511b
Happy210906 <olaguierpro@gmail.com>
2026-03-16 18:04:47 -0300

    En esta clase aprendi a imprimir, concatenar, y cadenas de texto, input, output y parseo y trans de texto y entero, diagramas de flujo y comandos de git

---
+++ 0_outputs.py
@@ -0,0 +1,16 @@
+#Este es un output basico
+print("Saludos")
+
+
+user_1 = "Illojuan"
+user_2 = "Eso"
+user_3 = "Omg"
+
+es_mayor_de_edad = True 
+estatura_user_1 = 170.5
+edad_user_1 = 27
+
+print("El nombre del primer usuario es "+  user_1 + " y el nombre del segundo usuario es "+  user_2+" el nombre del tercer usuario es "+ user_3)
+
+
+print(f"El nombre del primer usuario es { user_1} y el nombre del segundo usuario es {user_2} y el nombre del tercer usuario es {user_3}")
---
+++ 1_inputs.py
@@ -0,0 +1,9 @@
+print("Bienvenido al recorrido alerce puerto montt")
+
+input("A donde desea ir?  ")
+
+
+hacia_donde_va = input("hacia donde va ")
+
+
+print(f"Usted va hacia {hacia_donde_va} ")
