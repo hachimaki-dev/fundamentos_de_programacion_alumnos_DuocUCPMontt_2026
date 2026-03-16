3996e4b553733abe92900e95200e870a9ca1520a
CamiloDiaz2 <cf.diazs@duocuc.cl>
2026-03-16 18:03:32 -0300

    En esta clase aprendimos a imprimir con el print, aprendimos a concatenar y aprendimos lo que es la interpolaciòn de cadenas de texto, ademàs vimos el uso de pnput y el parseo o transformaciòn de texto a enteros. Igual vimos un poco de diagramas de flujo y comandos de git

---
+++ 1_calculadora_sumar.py
@@ -0,0 +1,13 @@
+'''
+num1 = int (input("Ingrese el primer número: "))
+num2 = int (input("Ingrese el segundo número: "))
+resultado = num1 + num2
+
+print(f"El resultado es: {resultado}")
+'''
+
+num1 = input("Ingrese el primer número: ")
+num2 = input("Ingrese el segundo número: ")
+resultado = int(num1) + int(num2)
+
+print(f"El resultado es: {resultado}")
---
+++ 1_inputs.py
@@ -0,0 +1,9 @@
+# Boton de los 300 millones de dolares usuario
+"""
+nombre = input("Por favor, ingrese su nombre de usuario: ")
+print(nombre)
+"""
+
+print("Bienvenido al recorrido Alerce Puerto Montt")
+direccion = input("¿Hacia dónde quiere ir?\n")
+print(f"El usuario quiere ir a {direccion}.")
