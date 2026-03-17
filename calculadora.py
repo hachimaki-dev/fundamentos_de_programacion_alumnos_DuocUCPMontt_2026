4471be9580d2c2fa0950e9b1f61c8792bc8bc490
rominadanaee <rominacocio08@gmail.com>
2026-03-16 22:13:58 -0300

    calculadora en python

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+#calculadora basica
+numero1 = int(input("por favor ingrese el numero1:"))
+numero2 = int(input("por favor ingrese el numero2:"))
+suma = numero1 + numero2
+resta = numero1 - numero2 
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print("la suma es:",suma)
+print("la resta es:",resta)
+print("la multiplicacion es:",multiplicacion)
+print("la division es:",division)
