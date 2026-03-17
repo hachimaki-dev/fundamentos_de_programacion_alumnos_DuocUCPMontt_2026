5e1b50326647248afef56061e4d26cbd17f2445d
Shiro341 <tecnicocode@duck.com>
2026-03-16 21:31:45 -0300

    Hacer una calculadora en python para sumar

--- Semestre 1/1_tarea_suma.py
+++ Semestre 1/1_tarea_suma.py
@@ -1,11 +1,9 @@
+#Hacer una calculadora en python para sumar
+
 numero1 = input("Ingreses su primer numero: ")
-
 numero2 = input("Ingrese el segundo numero: ")
-
 
 #int fuerza a que lo interprete como numero
 resultado =int(numero1) + int(numero2)
 
 print(f"El resultado es {resultado}")
-
-#hacer una calculadora en python para sumar, restar, multiplicar y dividir
