c50c019bf1e33a6a95df53f689dbe7090f2651a4
RubiZero <abcmudkip@gmail.com>
2026-03-18 16:39:30 -0300

    Mayor de edad?

---
+++ 0_EsMayor.py
@@ -0,0 +1,6 @@
+edad_usuario=int(input("Introduzca su edad"))
+if edad_usuario>=18 : 
+    print ("Acceso concedido")
+else edad_usuario<18 : 
+    print ("Acceso denegado")
+#Identacion en los : de los "if"
