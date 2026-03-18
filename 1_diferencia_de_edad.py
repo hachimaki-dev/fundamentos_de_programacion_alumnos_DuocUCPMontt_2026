3a20116cf0025e207c3bb51b72f9c029c19d6b67
BlazingFunk2005 <mtkonar@gmail.com>
2026-03-18 16:02:16 -0300

    Diferencia de edad para determinar si el usuario es mayor de edad

---
+++ 1_diferencia_de_edad.py
@@ -0,0 +1,5 @@
+edad_del_usuario = int(input("Bienvenido, porfavor ingrese su edad: "))
+if edad_del_usuario >= 18:
+    print("Accesso Permitido ദ്ദി(｡•̀ ,<)~✩‧₊")
+else:
+    print("Accesso Denegado (•̀⤙•́)")
