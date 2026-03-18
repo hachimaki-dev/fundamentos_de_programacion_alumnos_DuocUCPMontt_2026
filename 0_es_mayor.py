8e0da57214202bdd5cfc00cdc055e9284c2ebbfa
Pepinho25 <josecampisto@gmail.com>
2026-03-18 16:16:05 -0300

    este es el test de los condicionales, if o else

---
+++ 0_es_mayor.py
@@ -0,0 +1,8 @@
+print("=====Bienvenido a la licoreria=====")
+edad_de_persona = int(input("por favor ingrese su edad "))
+if edad_de_persona >= 18:
+    print("Usted puede comprar, escoja lo que le guste")
+else:
+    print("Por favor retirese de mi licoreria")
+
+#la identacion es el espacio entre una linea y otra, se activa rapidamente con tab
