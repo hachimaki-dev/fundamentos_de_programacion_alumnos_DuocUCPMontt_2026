0c1b3164d202a6835f3b56d37f54d27cf1b73121
Evelin-vidal <evelynpatriciavidalbustamante@gmail.com>
2026-03-18 16:20:23 -0300

    Determinar si es mayor de edad

---
+++ 0_es_mayor.py
@@ -0,0 +1,10 @@
+#Determinar si es mayor de edad
+edad_usuario = int(input("Ingrese su edad"))
+if edad_usuario >= 18 : 
+    print("Acceso concedido")
+else : 
+    print("Acceso denegado")
+#if es si
+#else es de otro modo o no 
+#: significa entonces 
+
