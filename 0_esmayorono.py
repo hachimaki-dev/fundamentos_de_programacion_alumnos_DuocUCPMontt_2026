fbc196aec49fa0f4fce6c2a10c74a154cbea3dbb
Alvas-Cod3 <alvargonz507@gmail.com>
2026-03-18 16:17:16 -0300

    Hoy aprendimos if/else, puertas lógicas y mayor o igual que

---
+++ 0_esmayorono.py
@@ -0,0 +1,7 @@
+Edad_usuario = int(input("¿Cuál es su edad? "  ))
+#se compara la edad del usuario con 18
+#se usa el famoso if y else :)
+if (Edad_usuario) >= 18 :
+    print("Acceso concedido :)")
+else:
+    print("Acceso denegado")
