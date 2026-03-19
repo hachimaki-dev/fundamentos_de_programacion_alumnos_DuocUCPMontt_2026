df4feb559baac64e70f486ac5075f9105e732b7d
KAT-000 <sebastianignaciocorderospier@gmail.com>
2026-03-19 16:13:02 -0300

    juego

---
+++ Novela_no_visual.py
@@ -0,0 +1,25 @@
+print("5000 años antes del nacimiento de nuestros heroes aventureros, habia una elfa de cabellos blancos cual nieve llamada Dahlya...")
+
+print("Era la mas joven de una familia de 4 hermanos, este es el inicio de como se convirtio en uno de los maximos referentes de la magia en la historia...")
+
+print("Dhalia no era como el resto de sus hermanos, a ella le gustaba estudiar cada minimo detalle de los libros que tenia en su casa.")
+
+print("Una vez su hermano mayor, Dahrien, la confronto preguntandole por que le gustaban tanto leer si podia crear aventuras reales para poder contar en un futuro.")
+respuesta = int(input("Escoge: 1: responder mal // 2: responder bien"))
+
+if(respuesta) == 1 :
+
+    print("Dahrien, sabes que odio las aventuras, un bruto como tu no seria capaz de entender la belleza que hay en un libro de magia...")
+   
+else :
+    print("Oh... Dahrien, me he dado cuenta de que la magia es mas compleja de lomque creemos, los dragones nos han dado una virtud precciosa y hay que aprovecharla...")
+    print("Dahrien: Creo que nunca sere capaz de entenderte a ti pelos blancos, siempre seras una extraña. No te entiendo hermana mia, no creo que confiar ciegamente en el conocimiento de los dragones sea seguro.")
+    
+    print("Luego de un rato tu hermano te deja sola estudiando los grimorios, ellos no son capaces de entender que lo que tienes es grande, pero muy en el fondo de tu alma sabes que él tiene razón, no puedes estar encerrada tanto tiempo")
+
+respuesta_1 = int(input("Elección:"))
+
+if(respuesta_1) == 2 : 
+    print("Sigues leyendo")
+
+else: print("Sales")
