436d05d4aee4570fc7edb8eafcb4ad5a77157d54
00awakedead <abelcabero019@gmail.com>
2026-03-23 13:54:42 -0300

    Aprendi sobre el uso del while

---
+++ estructura_de_control_mientras.py
@@ -0,0 +1,10 @@
+print("*****INICIO DEL PROGRAMA*****")
+agua = 0
+
+while agua <= 5 :
+    print(f"Llevamos {agua} de agua")
+    agua = agua + 1
+    if agua == 4 :
+        break
+
+print("*****FIN DEL PROGRAMA*****")
