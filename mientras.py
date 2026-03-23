b0732ba9df3bfd042b8433acd9367388be8c5252
gabriela-pezo <gabrielapezo69@gmail.com>
2026-03-23 17:59:45 -0300

    hoy es el dia del platano

---
+++ mientras.py
@@ -0,0 +1,19 @@
+while true :
+    print("1. League of legends")
+    print("2. Valorant")
+    print("3. faker")
+    opcion = int(input("elija su opcion"))
+
+    if opcion == 1:
+        print("League of legends")
+    elif opcion == 2:
+        print("Valorant")
+    elif opcion == 3:
+        print("Faker")
+    else:
+        print("chaolin")            
+
+
+
+
+
