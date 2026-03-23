2718c229b4335a3016d32b24fdd04b9b042c65c5
HachiMaki-dev <hachimakidev@gmail.com>
2026-03-23 14:13:12 -0300

    aprendimos de while

---
+++ estructura_de_control_mientras.py
@@ -0,0 +1,16 @@
+opcion = int(input("ELuiasu opcion"))
+
+
+if opcion == 1:
+    print("Cosas de opcion 1")
+    
+elif opcion == 2:
+    print("Cosas de opcion 2")
+
+
+elif opcion == 3:
+    print("Cosas de opcion 3")
+
+else:
+    print("TODO LO DEMAS")
+    
