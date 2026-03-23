00e797f32f6cd49f859307d0a1f4de0cc457095e
flame6672 <gustavoa3498gmail.com>
2026-03-23 10:21:25 -0300

    Clase 4 en la que hemos realizado cosas con el While

---
+++ Ejemplos.py
@@ -0,0 +1,7 @@
+contador = 0
+while True:
+    contador = contador + 1
+    print(f"Hola {contador}")
+    if contador == 32:
+        break
+print("***** Fin del Programa c: *****")
---
+++ Mientras.py
@@ -0,0 +1,8 @@
+while True:
+    nota = float(input("Ingrese su nota: "))
+    if nota == 7:
+        print("Nota perfecta en Chile :D")
+        break
+    elif nota >= 4:
+        print("Aprobado")
+        break
