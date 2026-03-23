a3ce1459cb7f5b19e98cc10aa27d60528b971c89
Gtvq32 <gtvq1718@gmail.com>
2026-03-23 17:55:25 -0300

    hoy aprendi el uso del while

---
+++ mientras.py
@@ -0,0 +1,66 @@
+estoy_haciendo_un_pedido = True
+escogiendo_ingredientes = False
+
+while estoy_haciendo_un_pedido :
+    print("Que desea pedir")
+
+    print(".1 Pizza ")
+    print(".2 Hamburguesas ")
+    print(".3 Papitas")
+    print(".0 Terminar el pedido")
+
+    opcion_comida = int(input(""))
+    
+    if opcion_comida == 1 :
+        print("Escoja los ingredientes")
+        escogiendo_ingredientes = True
+
+        while escogiendo_ingredientes :
+            print(".1 Extra queso")
+            print(".2 Peperoni")
+            print(".3 picante")
+            print(".0 Terminar de escojer ingredientes")
+
+            opcion_ingredientes = int(input(""))
+
+            if opcion_ingredientes == 0 :
+                print("")
+                escogiendo_ingredientes = False
+    if opcion_comida == 2 :
+        print("Escoja los ingredientes")
+        escogiendo_ingredientes = True
+
+        while escogiendo_ingredientes :
+            print(".1 italiana")
+            print(".2 Con queso")
+            print(".3 Vegana")
+            print(".0 Terminar de escojer ingredientes")
+
+            opcion_ingredientes = int(input(""))
+
+            if opcion_ingredientes == 0 :
+                print("")
+                escogiendo_ingredientes = False
+    
+    if opcion_comida == 3 :
+        print("Escoja los ingredientes")
+        escogiendo_ingredientes = True
+
+        while escogiendo_ingredientes :
+            print(".1 Con salchcicha")
+            print(".2 Con queso")
+            print(".3 Con palta")
+            print(".0 Terminar de escojer ingredientes")
+
+            opcion_ingredientes = int(input(""))
+
+            if opcion_ingredientes == 0 :
+                print("")
+                escogiendo_ingredientes = False
+        
+    if opcion_comida == 0 :
+        estoy_haciendo_un_pedido = False
+
+print("************************")
+print("*****Fin del pedido*****")
+print("************************")
