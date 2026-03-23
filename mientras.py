5379dd21b2ce5c8b3a31ccc43ab8a0d453d2d4d9
Tomy30b <barriatomy2@gmail.com>
2026-03-23 17:55:20 -0300

    hoy aprendi el uso del while

---
+++ mientras.py
@@ -0,0 +1,9 @@
+contador = 0
+bandera = True
+while bandera == True :
+    print(f"Hola mundito vamos en el numero {contador}")
+    contador = contador + 1 
+    if contador == 101 :
+        
+
+print("sali del ciclo")
