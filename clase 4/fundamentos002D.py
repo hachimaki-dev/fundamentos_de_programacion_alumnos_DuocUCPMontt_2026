20c5768869eeee642a54803b0ab0888b67785ce4
roox25 <roxa.henriquez@duocuc.cl>
2026-03-23 14:14:08 -0300

    aprendimos while

---
+++ clase 4/fundamentos002D.py
@@ -0,0 +1,12 @@
+print("****INICIO DEL PROGRAMA****")
+agua = 0
+
+while agua <= 4:
+    print(f"llevamos {agua} de {agua}")
+    agua = agua + 1
+    if agua == 4:
+
+        break
+
+
+print("****FIN DEL PROGRAMA****")
