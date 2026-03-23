13e03c2cdff869a63d54be19a631fe536c052424
Shiro341 <tecnicocode@duck.com>
2026-03-23 14:13:12 -0300

    Clase 4 While

---
+++ Semestre 1/Unidad 1/estructura_de_control_mientras.py
@@ -0,0 +1,13 @@
+print("****Inicio del programa****")
+agua = 0
+
+#while es mientras
+#los : significa entonces
+while agua < 5 :
+    agua = agua + 1
+    print(f"Llevamos {agua} de agua")
+    if agua == 4 :
+        break
+
+print("****Fin del Programa*****")
+
