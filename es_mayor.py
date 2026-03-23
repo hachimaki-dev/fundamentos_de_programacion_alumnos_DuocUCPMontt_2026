01cb4e8e0ee5311f101dcbe27eb651b5d9a181d4
agcamila01 <agcamila003@gmail.com>
2026-03-23 03:40:47 -0300

    Add files via upload

---
+++ es_mayor.py
@@ -0,0 +1,14 @@
+print("***********BIENVENIDO AL PROGRAMA***********")
+edad_de_usuario = int(input("¿Cual es tu edad? "))
+
+if edad_de_usuario >= 18 and edad_de_usuario <= 50: #and -> conjuncion
+    print(":):):):):):):):):)")
+    print(" Acceso Concedido ")
+    print(":):):):):):):):):)")
+
+else:
+    print("XXXXXXXXXXXXXXXXX")
+    print(" Acceso Denegado ")
+    print("XXXXXXXXXXXXXXXXX")
+
+#TAREA: CREAR UN JUEGO DE NARRATIVAS
