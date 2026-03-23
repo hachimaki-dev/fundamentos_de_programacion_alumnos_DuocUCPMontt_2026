61e1c106797a69b2940a6025eaffea86a0e4900b
Evelin-Vidal <evelynpatriciavidalbustamante@gmail.com>
2026-03-23 17:54:18 -0300

    Clase 4_Mientras

---
+++ Tarea.py
@@ -0,0 +1,15 @@
+while True:
+    print("1.Netflix")
+    print("2.HBO")
+    print("3.Youtube")
+
+    opcion = int(input("Elija su opcion"))
+    if opcion == 1 :
+       print("Bienvenido a Netflix")
+    if opcion == 2 :
+       print("Bienvenido a HBO")
+    if opcion == 3 :
+       print("Bienvenido a Youtube")
+       break
+print("###FIN DEL PROGRAMA###")
+
---
+++ mientras.py
@@ -0,0 +1,15 @@
+while True: 
+    print("1. Contar chiste")
+    print("2. Contar adivinanza")
+    print("3. Salir")
+
+ opcion = int(input("Elija su opcion"))
+
+    if opcion == 1:
+       print("meh")
+    if opcion == 2: 
+       print("megf")
+    if opcion == 3:
+       print("megmg")
+    break
+print("FIN")
