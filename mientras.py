2f4b861df09abf52133c70e9ce1879494af3fd37
HachiMaki-dev <hachimakidev@gmail.com>
2026-03-23 17:55:19 -0300

    hoy aprendi el uso del While

---
+++ mientras.py
@@ -0,0 +1,19 @@
+while True:
+    print("1. Contar chiste")
+    print("2. Contar adivinanza")
+    print("3. Salir")
+
+    opcion = int(input("Elija su opcion")) 
+
+   
+    if opcion == 1:
+        print("Que dice MickeyMouse cuando se quema? - Mikemao")
+    if opcion == 2:
+        print("Que hay entre la arena y el mar? - Y")
+    if opcion == 3:
+            break
+
+   
+
+print("FIN DEL PROGRAMA")
+
