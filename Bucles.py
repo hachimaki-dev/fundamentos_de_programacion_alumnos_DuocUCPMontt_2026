eec5784772a1175b4efcc7d597db0937d9fb491b
Amaro2039 <ovandoamaro0@gmail.com>
2026-03-23 18:04:35 -0300

    Hoy aprendi While

---
+++ Bucles.py
@@ -0,0 +1,14 @@
+while True :
+    print("1. Lol ")
+    print("2. RL ")
+    print("3. Chao")
+    opcion = int(input("Elija su opcion"))
+
+    if opcion == 1:
+        print("Lol")
+    elif opcion == 2:
+        print("Rl")
+    elif opcion == 3:
+        print("Chao")
+    else:
+        print("xd")
