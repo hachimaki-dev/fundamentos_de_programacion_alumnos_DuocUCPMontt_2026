2ef895ca5b7b5e38e6ecda68c4e2caf7c1b0d33b
andrewfarias452 <an.fariast@duocuc.cl>
2026-03-23 10:30:12 -0300

    se agrego la tarea de hoy

--- 0_inputs.py
+++
@@ -1,3 +0,0 @@
-print("Adivinaré el número que piensas")
-numero = int(input("ingresa el número que estás pensando: "))
-print("tu número es: " + str(numero))
---
+++ mientras.py
@@ -0,0 +1,10 @@
+Saldo = 1500
+while True:
+    Saldo -= 500
+    print(f"Saldo: {Saldo}")
+    if Saldo <= 1:
+        break
+
+if Saldo <=500:
+    print("Saldo bajo")
+    
