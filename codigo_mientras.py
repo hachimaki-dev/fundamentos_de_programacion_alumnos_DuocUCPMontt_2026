b07d0d6b6360e7c23cd0d17df5b223b62b4bfa6b
Amaro-07 <amaro.loos2007@gmail.com>
2026-03-23 14:01:25 -0300

    WHILE code

---
+++ codigo_mientras.py
@@ -0,0 +1,10 @@
+print("****INICIO DEL PROGRAMA****")
+agua = 0
+
+while agua <= 4:
+    print(f"Llevamos {agua} de agua")
+    agua = agua + 1
+    if agua == 5:
+        break
+
+print("****FIN DEL PROGRAMA****")
--- diagrama.py
+++ diagrama.py
@@ -1,4 +1,4 @@
 nombre = input("Ingrese su nombre: ")
 print("Bienvenida/o al recorrido Puerto Montt - Alerce")
-destino = input ("Por favor indiqueme su destino: ")
-print("Entendido, su destino es: ", destino, "El bus hacia", destino, "Saldra enseguida")
+destino = input("Por favor indiqueme su destino: ")
+print("Entendido,su destino es:", destino, "El bus hacia", destino, "Saldra enseguida")
