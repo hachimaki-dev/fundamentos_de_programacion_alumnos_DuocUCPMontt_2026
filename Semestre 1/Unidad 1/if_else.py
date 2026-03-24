8df65805fcdb065bfa7a23e8d47da2b49aa91cc3
Shiro341 <tecnicocode@duck.com>
2026-03-24 19:49:53 -0300

    Clase 3

---
+++ Semestre 1/Unidad 1/if_else.py
@@ -0,0 +1,12 @@
+print ("=======Bienvenidos al programa=======")
+edad_de_usuario = int(input("¿Cuál es tu edad? "))
+#Todas las funciones tienen un parentesis por eso tiene doble "))"
+
+if edad_de_usuario >= 18 and edad_de_usuario <= 50:
+    print("Acceso permitido :D")
+    #Codigo del si puede continuar
+    #and es un confunción une las dos cosas 
+    #or con una de las dos cosas funciona
+else:
+    print("Acesso denegado .-.") 
+    #Codigo del no puede continuar  
