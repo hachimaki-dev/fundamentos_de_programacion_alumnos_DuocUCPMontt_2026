c62353a48dfbd1cd3b6c39730a867224b5c14f00
Pepinho25 <josecampisto@gmail.com>
2026-03-23 23:01:09 -0300

    agregue ina barra de vida para el usuario (falta ver como evitar que el usuario ponga una string como input para que no de error)

--- lucha_contra_el_monstruo.py
+++ lucha_contra_el_monstruo.py
@@ -1,7 +1,9 @@
-vida_monstruo=500
+vida_personaje = 300
+vida_monstruo = 500
 print("te perdiste en el bosque, sientes que una vil presencia te esta asechando desde hace un largo rato, vas a revisar el origen del ruido que te lleva acosando. Cuando decides acercarte la bestia se avalanza sobre ti, con un habilidoso movimiento logras esquivarla, ahora solo queda luchar")
-while vida_monstruo > 0:
-    print(f"vida del jefe {vida_monstruo}")
+while vida_monstruo > 0 and vida_personaje > 0:
+    print(f"==tu vida {vida_personaje}==")
+    print(f"==vida del jefe {vida_monstruo}==")
     print("===Combate===")
     print("1-Atacar con la espada")
     print("2-Usar magia de fuego")
@@ -9,28 +11,48 @@ while vida_monstruo > 0:
     print("4-Usar magia de electricidad")
     print("5-Usar magia de tierra")
     print("6-Usar magia de viento")
-    print("7-Huir")
-    opcion =int(input("Elije: "))    
+    print("7-Curarte")
+    print("8-Huir")
+    opcion = int(input("Elije: "))    
     if opcion == 1:
-        print("Atacas al monstruo con un tajo directo al pecho")
+        print("Atacas al monstruo con un tajo directo al pecho, pero la bestia te ataca con sus feroces garras")
         vida_monstruo = vida_monstruo - 25
+        vida_personaje = vida_personaje - 50
     if opcion == 2:
         print("Acatas a la bestia con una bola de fuego en la boca")
         vida_monstruo = vida_monstruo - 50
+        vida_personaje = vida_personaje - 25
     if opcion == 3:
         print("lanzasas agua a presion a la bestia en el pecho")
         vida_monstruo = vida_monstruo - 40
+        vida_personaje -= 20
     if opcion ==4:
         print("te fuerzas a lanzar punzantes lanzas de hielo al pecho de la bestia")
         vida_monstruo = vida_monstruo - 75
+        vida_personaje -= 10
     if opcion == 5:
         print("haces temblar el piso para desestabilizar al monstruo")
         vida_monstruo = vida_monstruo -25
+        vida_personaje -= 100
     if opcion == 6:
         print("lanzas una fuerte rafaga de aire afilado a la bestia")
         vida_monstruo= vida_monstruo-15
+        vida_personaje -= 15
     if opcion == 7:
-        print("No pudiste escapar la bestia te ha devorado")
+        if vida_personaje < 300:
+            print("te proteges por un momento escondientote de la bestia, aprovechas ese momento para curarte")
+            vida_personaje += 100
+        else:
+            print("Tu vida esta al maximo")
+    if opcion == 8:
+        print("No pudiste escapar, la bestia te ha devorado")
+        break
+    if opcion > 8:
+        print("Comando no valido")
+    if opcion < 1:
+        print("Comando no valido")
+    if vida_personaje <= 0:
+        print("Has dado todo en batalla pero el monstruo se mostro victorioso ante ti...")
         break
     if vida_monstruo <= 0:
         print("la vida del monstruo a caido a 0")
