b55858cbf32e336235a14479f2466f5245d7b677
Benjaminb80 <benjaminbusta45@gmail.com>
2026-03-25 16:34:15 -0300

    mi ejercicio con números

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+número_secreto = 7
+número_ingresado = 0
+número_ingresado = input("elige un número del 1 al 10")
+while número_secreto != número_ingresado :
+ número_ingresado = int(input("elige un número del 1 al 10"))
+ if número_secreto != número_ingresado :
+     print ("fallaste")
+     
+ 
+print("ganaste")
+
+
