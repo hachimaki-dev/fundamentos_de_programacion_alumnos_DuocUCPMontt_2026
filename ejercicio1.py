1093ac67665b56f81c4e61b5d004726442dcc915
gabriela-pezo <gabrielapezo69@gmail.com>
2026-03-25 16:29:21 -0300

    hoy estuvo pulento

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+numero_secreto = 7
+numero_adivinado = 0
+
+while numero_secreto != numero_adivinado :
+    numero_adivinado = int(input("Ingresar un numero del 1 al 10: "))
+    if numero_secreto != numero_adivinado :
+        print("fallastee")
+    
+print("ganaste")
+
