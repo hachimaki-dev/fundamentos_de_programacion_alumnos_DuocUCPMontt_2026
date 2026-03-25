1538ee1cfccf3a26e56c26ca56efd7bc7f97ac7a
AphidCiro <navarrogamboajulian@gmail.com>
2026-03-25 19:45:27 -0300

    tried to make a turn system seems useless

--- combate pokemon.py
+++ combate pokemon.py
@@ -1,37 +1,43 @@
+import time
 Pokemon1 = "Pikachu"
 PokemonRival = "Eevee"
 VidaPokemon1 = 20
 VidaPokemonRival = 20
-
+turno = True
 Combate = True
+
 while Combate == True:
-    
-
-    print (f"{Pokemon1} tiene {VidaPokemon1} puntos de vida \n {PokemonRival} tiene {VidaPokemonRival}")
-    Accion = input(f"""¿Que deberia hacer {Pokemon1}? Opciones: 
-               Atacar
-               Curar
-               Huir\n""" )
-    if Accion == "Atacar" or Accion == "atacar":
-        print(f"\n{Pokemon1} atacó a {PokemonRival}")
-        VidaPokemonRival = VidaPokemonRival-5
-    elif Accion == "Curar" or Accion == "curar":
-        print(f"\n{Pokemon1} se curo")
         
-        VidaPokemon1 = VidaPokemon1 + 2
+    while turno == True:
+        print (f"{Pokemon1} tiene {VidaPokemon1} puntos de vida \n {PokemonRival} tiene {VidaPokemonRival}")
+        Accion = input(f"""¿Que deberia hacer {Pokemon1}? Opciones: 
+                Atacar
+                Curar
+                Huir\n""" )
+        if Accion == "Atacar" or Accion == "atacar":
+            print(f"\n{Pokemon1} atacó a {PokemonRival}")
+            VidaPokemonRival = VidaPokemonRival-5
+            turno = False
+        elif Accion == "Curar" or Accion == "curar":
+            print(f"\n{Pokemon1} se curo")
+            VidaPokemon1 = VidaPokemon1 + 2
+            turno = False
+            if VidaPokemon1 >= 20:
+                    VidaPokemon1 = 20
         
-        if VidaPokemon1 >= 20:
-                VidaPokemon1 = 20
+        elif Accion == "Huir" or Accion == "huir":
+            print(f"\nNO PUEDES HUIR")
+            turno = False
         
-
-    elif Accion == "Huir" or Accion == "huir":
-        print(f"\nNO PUEDES HUIR")
-    else:
-        print("\nOpción invalida")
-    print(f"{PokemonRival} ataco a {Pokemon1}")
+        else:
+            print("\nOpción invalida")
+            turno = False
+    print(f"Turno de {PokemonRival}")
+    print(f"{PokemonRival} ataco a {Pokemon1} por {3} de daño")
+    time.sleep(1)
     VidaPokemon1 = VidaPokemon1-3
+    turno = True
+if VidaPokemon1 <= 0 or VidaPokemonRival <= 0:
+    Combate = False
 
-    if VidaPokemon1 <= 0 or VidaPokemonRival <= 0:
-        Combate = False
-
-print(f"\nCombate terminado")        
+print(f"\nCombate terminado")            
