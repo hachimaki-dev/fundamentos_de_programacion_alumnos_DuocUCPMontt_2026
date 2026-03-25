a3cca4588ddf3b763991211ecdfa35d8d3352158
RubiZero <abcmudkip@gmail.com>
2026-03-25 16:37:00 -0300

    Tarea frustrante

---
+++ ejercicio.py
@@ -0,0 +1,30 @@
+#Instrucciones
+#Crear diagrama de flujo del algoritmo solicitado
+#Solo cuando mi diagrama de flujos ha sido validado, entonces crear codigo
+
+#1Crear un algoritmo que obligue al usuario a adivinar un numero secreto previamente determinado por el sistemna. Si falla, indicarle que fallo y solicitarle que vuelva a intentarlo. Si acierta, lo felicitamos y termina el programa
+
+
+#Mi prueba
+numero_secreto=7
+numero_elegido=0
+print=("Ingrese un numero entre el 1 y el 10")
+input=numero_elegido
+while numero_secreto!=numero_elegido==True:
+    print("Fallaste, intentalo de nuevo")
+    print("Ingrese un numero entre el 1 y el 10")
+    input=numero_elegido
+while numero_secreto!=numero_secreto==False    
+print("Ganaste")
+#El profesor dijo que no puedo compararr
+
+
+numero_secreto=7
+numero_elegido=0
+while numero_secreto!=numero_elegido:
+    numero_elegido=int(input("Ingrese un numero entre 1 y el 10"))
+    if numero_secreto!=numero_elegido:
+        print("Fallaste, intentalo de nuevo")
+print("Ganaste")
+
+#Tarea crear un diagrama de flujo y luego programarlo, sobre un cachipun el juego de 2 player, termina cuando un jugador llega a 3 victorias
