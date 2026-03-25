e739553aeb84c8f49a3f83eef629357160377a58
BlazingFunk2005 <mtkonar@gmail.com>
2026-03-25 16:27:36 -0300

    Tarea y ejercicio de la clase

---
+++ ejercicio_1.py
@@ -0,0 +1,19 @@
+# Crear diagrama de flujos del algoritmo solicitado solo cuando mi diagrama de flujos ha sido validado, entonces, crear codigo
+#1 Crear un algoritmo que oblige al usuario a adivinar un numero secreto previamente determinado por el sistema. si falla indicarle que fallo y solicitarle que vuelva intertarlo. si acierta, lo felicitamos y termina el programa.
+
+import random
+
+numero_secreto = random.randint(1, 10)
+numero_adivinado = 0
+
+print(numero_secreto)
+
+while numero_secreto != numero_adivinado:
+    numero_adivinado = int(input("Ingrese un numero para adivinar (Entre el 1-10): "))
+    if numero_adivinado != 4:
+        print("Fallaste! Intente denuevo!")
+
+
+print("Has ganado!")
+
+            
---
+++ tarea.py
@@ -0,0 +1,3 @@
+# Crear un diagrama de flujos y luego programarlo
+# Sobre un cachipun 
+# El juego termina cuando uno de los dos jugadores llega a 3 victorias!
