2f75f8c128ff4f7e8637d02465603359ab54a7e4
LeCoward <sebabarri2710@gmail.com>
2026-03-26 11:19:18 -0300

    jasjas

---
+++ kachipum.py
@@ -0,0 +1,8 @@
+sel_jugador1 = ""
+sel_jugador2 = ""
+puntos_jugador1 = 0
+puntos_jugador2 = 0
+print("Están apunto de comenzar a jugar Kachipum uno contra el otro")
+print("Porfavor escriban su opcion(piedra, papel o tijeras)")
+sel_jugador1 = input("Jugador 1, Ingrese su elección")
+sel_jugador2 = input("Jugador 2, Ingrese su elección")
