d59bec6a33b435b33c947077de35ba64051957d5
MatiasValdivia327 <matias.felipe7777@gmail.com>
2026-03-26 10:10:44 -0300

    numero secreto

---
+++ adivina_el_numero.py
@@ -0,0 +1,10 @@
+numero_secreto = 7
+numero_elejido = 0
+while numero_secreto != numero_elejido:
+    numero_elejido = int(input("adivina el numero "))
+    if numero_secreto != numero_elejido:
+        print("te equivocaste")
+    
+
+
+print("adivinaste")
---
+++ primerwhile.py
@@ -0,0 +1,11 @@
+import time
+
+
+water = 0
+while water <= 4:
+    water = water + 1
+    print(f"llevamos {water}")
+    if water == 3:
+        break
+    
+    
