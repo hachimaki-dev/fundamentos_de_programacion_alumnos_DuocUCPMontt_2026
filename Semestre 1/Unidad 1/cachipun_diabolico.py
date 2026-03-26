504649db6884e94d4bda7c51f92c90d2631808de
Shiro341 <tecnicocode@duck.com>
2026-03-26 11:20:14 -0300

    cachipun_diabolico

---
+++ Semestre 1/Unidad 1/cachipun_diabolico.py
@@ -0,0 +1,12 @@
+opcion_usuario1 = " "
+opcion_usuario2 = " "
+
+puntaje_usuario1 = 0
+puntaje_usuario2 = 0
+
+print("****Bienvenidos al cachipun extremo devil****")
+print("****Por favor escriba la palabra piedra, papel o tijeras****")
+
+opcion_usuario1 = input("J1 - Por favor ingrese su elección: ")
+opcion_usuario2 = input("J2 - Por favor ingrese su elección: ")
+
