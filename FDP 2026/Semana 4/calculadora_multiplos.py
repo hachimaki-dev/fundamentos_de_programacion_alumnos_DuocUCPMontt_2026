22b5003d05bedae8c10fb61b9e0c9e14d3e261d9
Martin Muñoz <martin.andres.munoz.c@gmail.com>
2026-03-30 20:10:46 -0300

    calculadora de multiplos

--- FDP 2026/Semana 4/calculadora_multiplos.py
+++ FDP 2026/Semana 4/calculadora_multiplos.py
@@ -2,11 +2,12 @@ num_inicio = 0
 num_final = -1
 
 multiplo = 0
+lista_multiplos = []
 
 print("Te voy a decir que número son múltiplos de un número que me des, en un rango que me des")
 
 while multiplo < 1:
-    multiplo = int(input("Ingrese el número que quiere saber sus múltiplos"))
+    multiplo = int(input("\nIngrese el número que quiere saber sus múltiplos: "))
 
     if multiplo < 1:
         print("El número ingresado debe ser mayor a 0")
@@ -18,4 +19,9 @@ while(num_inicio > num_final):
     if num_inicio >= num_final:
         print("El número inicial debe ser menor al final")
 
+for i in range(num_inicio, num_final+1):
+    if i % multiplo == 0:
+        lista_multiplos.append(i)
+
+print(f"Los números multiplos de {multiplo} entre {num_inicio} y {num_final} son: {lista_multiplos}")
 
