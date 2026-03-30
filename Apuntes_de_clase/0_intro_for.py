7437cf652f909008c7dc740ca1fb5c79ff094606
flame6672 <gustavoa3498@gmail.com>
2026-03-30 10:39:08 -0300

    Mi nueva rama personal

---
+++ Apuntes_de_clase/0_intro_for.py
@@ -0,0 +1,8 @@
+Variable1 = "Blabla"
+Variable2 = "Blaivj"
+Variable3 = 22465
+
+# for variable3 <--- X , tiene que ser una variable limpia, sin valor asignado
+
+for cada_cosa_dentro in range(5):
+    print(f"Voy en la iteración N°: {cada_cosa_dentro}")
