7edbe5f9572ffc27f119da5e99c79ee8b654bc01
Shiro341 <tecnicocode@duck.com>
2026-03-30 14:12:07 -0300

    aprendimos de for

---
+++ Mis codigos/Apunto de la clase/tipo de pregunta.py
@@ -0,0 +1,25 @@
+#¿Qué tipo de dato devuelve la función input() por defecto?
+#Respuesta: str(texto)
+#concadenar es unir a menos que tenga un espacio que tambien es un texto
+#booleana buscar significado
+# contador = contador + 1 <---> contador += 1 es lo mismo 
+#the mas tarde o despues (según el contexto)
+#elif puede ser else if en otros tipos de programamción
+#prueba el 6 del abril leer codigo, hacer codigo, ver diagramas de flujos
+
+# si tubiera que enseñarle algo a un compañero con la lectura de recien como seria y porque?
+# lo explicaria con muebles unidos que almacenan datos por que seria lo mas facil de entender ya que el forma almacena informacion apartir
+# while se repite de manera infinita hasta que pasa algo
+#for puedes decir cuantas veces queres que se repita
+# a diferecia del while en el FOR si es necesario
+# determinar la cantidad de vueltas de alguna forma
+# como?? nose, eso depende de ti
+#for en español es para
+
+#for cada_elemento_que_se_encuentre_dentro_de_otro_conjunto_de_elementos 
+# in dentro (conjunto de elementos)
+
+#continue es como que continue con el siglo del for
+#continue es como un limite condicional
+# % es como division
+# % se dice mod 
---
+++ Mis codigos/for_basico_ejercicios.py
@@ -0,0 +1,4 @@
+1 = cristobal
+
+for i range(1, 11):
+    print(f"Estas en el top {1} felicidades")
