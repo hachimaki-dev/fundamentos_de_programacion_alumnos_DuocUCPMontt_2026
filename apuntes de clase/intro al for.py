2f62f07f7adc0fe8083511ff5e25459bf9f199ae
Roten-chan <henriquezzobeiba@gmail.com>
2026-03-30 10:37:02 -0300

    aaaaaaaaaaaa

---
+++ apuntes de clase/intro al for.py
@@ -0,0 +1,3 @@
+#la palabra que le sigue al for no puede ser una variable, tiene que se runa una que no está declarada jjwjw
+for por_cada_cosa_dentro_de in range (10):
+    pr"voy en la iteración:" +1)
