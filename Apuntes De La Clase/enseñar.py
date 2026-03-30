2f8226cf229f6ada3d2d297a03c17f4d465fecef
4l3xZzz <alexandervillalon54@gmail.com>
2026-03-30 15:12:32 -0300

    Hoy aprendimo de for

---
+++ Apuntes De La Clase/enseñar.py
@@ -0,0 +1,11 @@
+#Si tuviera que enseñarle algo a un compañero de la lectura. ¿Què seria y porque?
+    #Le enseñaria a saber que en for, el range se divide en tres partes (1, 8, 2) Inico siendo con el nùmero que parte (1) Fin, con el nùmero que termina (8) y paso que seria
+    #los numeros que va pasando
+    
+#A diferencia del while en FOR si es necesario determiar la cantidad de vuelta de alguna forma...
+#Comò? Depende de ti
+
+# for CADA_ELEMENTO_QUE_SE_ENCUENTRE_DENTRO_DE_OTRO_CONJUNTO_DE_ELEMENTOS
+
+for cada_elemento_dentro_de in range (2, 15, 3):
+    print(f"Hola vamos en el numero {cada_elemento_dentro_de}")
