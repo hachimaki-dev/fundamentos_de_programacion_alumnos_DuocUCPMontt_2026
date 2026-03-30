20bee06905edd640b0d6a48c69fd8e1c29d528a4
jp.sanchez6 <megajuanx@gmail.com>
2026-03-30 10:37:08 -0300

    el for

---
+++ apuntos de clase/0_intro_for.py
@@ -0,0 +1,7 @@
+variable1 = "hola"
+variable2 = "como estas"
+variable3 = "aqui estoy XD"
+variable4 = 12345
+
+for cada_cosa_dentro_de in range(10) :
+    print(f"voy en la iteracion numero: {cada_cosa_dentro_de}")
