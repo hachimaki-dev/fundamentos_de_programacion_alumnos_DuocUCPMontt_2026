8e85cab5759fde6a87d5a5d5a4e4827a5f484786
moenita <davidmoena2@gmail.com>
2026-03-30 14:16:32 -0300

    ciclo_for

--- ciclo_for.py
+++ ciclo_for.py
@@ -1,4 +1,4 @@
-for i in range (1,100,1):
+for i in range (1,101,1):
     print(f"El numero de ordenes diarias en una empresa de comida son: {i}")
     if i == 100:
         print("Tus ventas son buenas")
