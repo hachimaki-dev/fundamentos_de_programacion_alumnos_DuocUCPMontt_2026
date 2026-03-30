f4a858db58d34943062eac99a192f1b06dea4d73
Crstxn1 <cmancillat9@gmail.com>
2026-03-30 14:13:04 -0300

    Hoy vimos el For

---
+++ Mis codigos/EL_PROFE_EXPLICA_EL_FOR.py
@@ -0,0 +1,10 @@
+# Si tuviera que enseñarle algo de la lectura recien leida a un compañero que seria? y por que.
+#R: le enseñaria a usar los break y continue aplicados para for de esa manera puede reomper y continuar el codigo
+
+# Codigo, borrar #.
+#for cada_elemento_dentro_de in range(0, 15, 2):
+    #print(f"Hola, vamos en el numero {cada_elemento_dentro_de}")
+    
+    # este codigo 
+for i in range (1001):
+    print(i)
