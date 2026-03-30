1ad8fcc2786173a931bee989b5a9c8f2689791e3
axel-trujillo <axeltrujillo1110@gmail.com>
2026-03-30 10:36:23 -0300

    apuntes for

---
+++ Apuntes de clases/0_intro_for.py
@@ -0,0 +1,8 @@
+#esta variablese asignara por una iteracion con un valor
+
+variable1 = "blabva"
+variable2 = "kfsldj"
+variable3 = "sjadls"
+
+for cada_cosa_dentro_de in range(10) :
+    print(f"voy en la iteracion numero: {cada_cosa_dentro_de}") 
