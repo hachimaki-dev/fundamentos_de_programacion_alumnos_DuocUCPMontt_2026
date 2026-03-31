1af10007bf412547b9efd3149c1c2f95264f8f96
Albedo-AB <di.aravenab@duocuc.cl>
2026-03-30 21:03:41 -0300

    mis apuntes Fvcd

---
+++ ejemplo for.py/ejmplo for.py
@@ -0,0 +1,16 @@
+#for
+#por_cada_elemento_dentro_de_un_conjunto_de_elementos
+
+for por_cada_elemento in range(8):
+    print(f"hola voy en la vuelta n {por_cada_elemento}")
+
+for por_cada_elemento in ["manzana", "pera","manzana"]:
+    print(f"hola voy en la vuelta {por_cada_elemento}")
+
+#vaina bacana solo dale play y entenderas
+
+
+for por_cada_elemento in range(3,10,-2):
+    print(f"hola voy en la vuelta n {por_cada_elemento}")
+
+
---
+++ mis apuntes/prueba.py
@@ -0,0 +1,30 @@
+z = 10
+z = z + 5
+z = z * 2
+ #DA 30
+ #PORQUE Z ES LA VARIABLE INICIAL DE 10 AVANZANDO A Z ( IGUAL 10) + 5
+ #DESPUES SE MULTIPLICA X 2
+
+x = 8
+if x < 5:
+    print("Gato")
+else:
+    print("Perro")
+    # Como 8 NO es menor que 5, la condición es False, así que entra en bloque else e imprime "Perro".
+    
+    contador = 0
+while contador < 3:
+    print("Hola")
+    contador += 1
+    #Se ejecuta cuando contador vale 0, 1 y 2. Cuando llega a 3, la condición 3 < 3 es falsa y se detiene. Son 3 veces.
+    
+    #MIENTRAS LA CONDICION SEA VERDADERA ENTONCES MI CODIGO SE VA AL BUCLE 
+    #MIENTRAS CONTADOR SEA MENOR QUE 3 
+    
+print("¿Cuál es la palabra reservada en Python equivalente a Sino si")
+     
+     #En Python se usa elif para encadenar múltiples condiciones.
+     
+     #¿Qué tipo de dato devuelve la función input() por defecto?
+     
+     #input() SIEMPRE devuelve un string (texto). ¡Por eso necesitamos int() para convertirlo si queremos sumar números!
