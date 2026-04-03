d4f6e22f5d619a54ae3bb5382e7243a9a9441386
Roten-chan <henriquezzobeiba@gmail.com>
2026-04-03 17:50:13 -0300

    Banco de ejercicios

---
+++ Ejercicios para estudiar/Simulador de Daño de RPG.py
@@ -0,0 +1,15 @@
+#El jefe final tiene 1000 puntos de vida iniciales.
+#Usa un ciclo while que se repita mientras la vida del jefe sea mayor que 0.
+#Dentro del ciclo, pide al usuario ingresar el daño del ataque (entero).
+#Si el daño es menor a 0, muestra un mensaje "El ataque falló" y no restes vida.
+#Si el daño es mayor o igual a 0, réstalo de la vida del jefe y muestra la vida restante.
+#Si la vida es 0 o menos, rompe el ciclo o deja que termine naturalmente, y muestra "¡Jefe derrotado!".
+
+Jefe_final=1000
+while Jefe_final > 0:
+    Ataque = int(input("Ingrese ataque: "))
+    if Ataque < 0:
+        print("¡El ataque falló!")
+    else:
+        Jefe_final = Jefe_final - Ataque
+print("¡Jefe derrotado!")
---
+++ apuntes de clase/1.py
@@ -0,0 +1,7 @@
+n=0
+int(input("Ingresa número"))
+if n % 2 == 0:
+    print("PAR")
+else:
+    print("IMPAR")
+print("listo")
