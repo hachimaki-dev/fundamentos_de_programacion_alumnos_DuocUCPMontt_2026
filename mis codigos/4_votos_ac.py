7e4b04710881e1019b5fa48fa19b80746152496e
Alonso Munoz <alon.munozc@duocuc.cl>
2026-04-03 21:04:50 -0300

    Entregando mis codigos

--- mis codigos/4_votos_ac.py
+++ mis codigos/4_votos_ac.py
@@ -1,11 +1,11 @@
-n = int(input("Ingres el numero de los encuestados: "))
+n = int(input("Ingres el numero de los encuestados"))
 todos_votaron = False
 a_favor = 0
 en_contra = 0
 contador = 1
 
 while contador <= n :
-    voto = int(input("1. A favor, 2. En contra: "))
+    voto = int(input("1. A favor, 2. En contra"))
     if voto == 1:
         a_favor = a_favor + 1
         contador = contador + 1
@@ -13,7 +13,7 @@ while contador <= n :
         en_contra = en_contra + 1
         contador = contador + 1
     else:
-        print("Por favor elija entre 1 a favor o 2 en contra: ")
+        print("Por favor elija entre 1 a favor o 2 en contra")
 
 print("Votos a favor:", a_favor)
 print("Votos en contra:", en_contra)
