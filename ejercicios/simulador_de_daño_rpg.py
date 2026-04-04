044d18325bb087314b2b8c7eaebe656102ab2d37
seba1304256 <seba13email.com@gmail.com>
2026-04-04 00:23:04 -0300

    mis ejercicios:3

--- ejercicios/simulador_de_daño_rpg.py
+++ ejercicios/simulador_de_daño_rpg.py
@@ -4,11 +4,14 @@ while jefe_final > 0:
     daño = int(input("ingrese daño de ataque:"))
     if daño < 0 :
         print("el ataque fallo")
-        print(f"{jefe_final} vida del jefe")
+        print(f"la vida del jefe es {jefe_final}")
     elif daño >= 0:
-        print("le realizas un ataque")
         jefe_final = jefe_final - daño
-        print(f"{jefe_final} punto de vida del jefe")
-if jefe_final <= 0:
+        if jefe_final < 0:
+            jefe_final = 0
+        print(f"le realizas un ataque, la vida del jefe es {jefe_final}")
+        
+else:
+    jefe_final <= 0
     print("Felicidades derrotaste al jefe")
 
