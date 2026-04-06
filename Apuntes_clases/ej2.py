9b27ed103bcea6e4d5a256bd92dec57f076361df
axel-trujillo <axeltrujillo1110@gmail.com>
2026-04-06 11:33:11 -0400

    estudiar prueba

---
+++ Apuntes_clases/ej2.py
@@ -0,0 +1,18 @@
+#1. Utiliza un ciclo (while) para preguntar repetidamente al usuario el precio de un suplemento a agregar al carrito. Si ingresa "0" u otro número de salida que definas, el ciclo debe terminar.
+#2. Lleva la cuenta del total a pagar y de la cantidad de productos ingresados.
+#3. Si el usuario compró 3 o más productos en total, aplícale un descuento del 10% al total al finalizar.
+#4. Imprime el total final a pagar y la cantidad de productos comprados.
+
+contador = 0
+total = 0
+while contador == contador: 
+    contador = contador + 1
+    total = int(input("ingresar valor del producto: "))
+    print(f"{total} + {contador}")
+    total = total + 
+    if total == 0: 
+        break 
+if contador > 3 :
+    total = total * 0.10
+    print("se le ha aplicado un 10% de descuento ")
+
