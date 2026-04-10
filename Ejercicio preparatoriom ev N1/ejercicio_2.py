7221e52611d468fd635deb2bd684648e0d4e5c14
Albedo-AB <di.aravenab@duocuc.cl>
2026-04-09 21:57:05 -0400

    mis codigos

--- Ejercicio preparatoriom ev N1/ejercicio_2.py
+++ Ejercicio preparatoriom ev N1/ejercicio_2.py
@@ -4,4 +4,4 @@ grupo1 = "BTS"
 grupo2 = "blackpink"
 grupo3 = "stray kids"
 while grupo != grupo1 and grupo != grupo2 and grupo != grupo3:
-    print("no es parte del fandom!")
+    print("no es parte del fandom!!")
