41f28d42107ec6db89b9de45298c383a79543a48
Roten-chan <henriquezzobeiba@gmail.com>
2026-04-12 19:12:18 -0400

    Create KPOP.py

---
+++ apuntes de clase/Mecanis de ruptura/KPOP.py
@@ -0,0 +1,7 @@
+grupo = input("Ingresa tu grupo favorito: ")
+ 
+while grupo != "BLACKPINK" and grupo != "Stray Kids" and grupo != "BTS":
+     print("¡Acceso Denegado!.")
+     grupo = input("Ingresa tu grupo favorito: ")
+    
+print("¡Acceso Concedido! Bienvenido al Fandom.")
