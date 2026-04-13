c53a23d3af10efac04f28651549c539302dddcb1
cianeken <vic.soto@duocuc.cl>
2026-04-13 10:43:09 -0400

    Evaluacion1

---
+++ evaluacionn1_vicente_soto/eje1.py
@@ -0,0 +1,14 @@
+
+print("Registro de habilidades diarias")
+
+registrar_actividades = 1
+mostrar_analisis_del_tiempo = 2
+salir = 3
+
+print("menu de opciones")
+
+print("1) Registrar actividades")
+print("2) Mostrar analisis del tiempo")
+print("3) Salir")
+
+opcion = int(input("elige una opcion: ")) 
