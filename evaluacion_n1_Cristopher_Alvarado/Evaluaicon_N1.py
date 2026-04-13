2d5e607ee00edf72d2e960ae41952f52c14ab28a
22cristopher <cristhoferalvarado36@gmai.com>
2026-04-13 16:39:20 -0400

    Evaluacion numero 1

---
+++ evaluacion_n1_Cristopher_Alvarado/Evaluaicon_N1.py
@@ -0,0 +1,33 @@
+print("***BIENVENIDO AL REGISTRO DE ACTIVIDADES DIARIAS***")
+
+Registrar_actividades = 1
+Mostrar_analisis_de_tiempo = 2
+Salir = 3
+
+print("***MENU DE OPCIONES***")
+
+opcion_1 = print(" N1: Registrar_actividades")
+opcion_2 = print( "N2: Mostrar_analisis_de_tiempo")
+opcion_3 = print( "N3: Salir")
+
+
+print("***ELIJA UNA OPCION***")
+input()
+if opcion_1 ==1:
+
+ print("Escojiste la opcion numero 1")
+print("Registra tu numero de actividades diarias")
+input()
+
+print("Ingresa tu tiempo para analizarlo")
+input()
+
+print("Escojiste la opcion numero 3")
+            
+
+
+
+
+
+
+ 
