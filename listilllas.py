d191af7e5f26c2935002d2755774fe40087344c6
DarkDevBat <jo.banguera@duocuc.cl>
2026-04-20 18:07:06 -0400

    xd

---
+++ listilllas.py
@@ -0,0 +1,21 @@
+ataques = ["Impactrueno", "Ataque Rápido", "Rayo", "Cola Férrea"]
+
+for ataque in ataques:
+    print("pikachu puede usar", ataque)
+
+print("Pikachu tiene un total de", len(ataques), "ataques disponibles")
+
+##########
+
+alumnos = ["Harry", "Hermione", "Ron", "Draco", "Neville", "Luna"]
+gryffindor = []
+slytherin = []
+
+for alumno in alumnos:
+    if len(alumnos) > 5:
+        gryffindor.append
+    else:
+        slytherin.append
+print("lista", gryffindor)
+print("lista 2", slytherin)
+
