3f70fd1d9317aa92975e2fc4ef3a17ff55966ff5
HachiMaki-dev <hachimakidev@gmail.com>
2026-04-20 17:21:59 -0400

    listas

---
+++ 003D_listas.py
@@ -0,0 +1,56 @@
+#Mi biblioteca de juegos en steam
+#mi_juego1 = "Cs 1.6"
+#mi_juego2 = "Team Fortress"
+#mi_juego3 = "Left 4 Dead"
+#mi_juego4 = "Half-Life"
+#mi_juego5 = "Portal 2"
+#mi_juego6 = "DOTA 2"
+#mi_juego7 = "Monster Hunter Wilds"
+#mi_juego8 = "Golfin overin with Alva Majo"
+#
+#print(f"En mi biblioteca de Steam tengo el {mi_juego1 }")
+#print(f"En mi biblioteca de Steam tengo el {mi_juego2 }")
+#print(f"En mi biblioteca de Steam tengo el {mi_juego3 }")
+#print(f"En mi biblioteca de Steam tengo el {mi_juego4 }")
+#print(f"En mi biblioteca de Steam tengo el {mi_juego5 }")
+#print(f"En mi biblioteca de Steam tengo el {mi_juego6 }")
+#
+
+
+
+
+
+#Manera mas algortimica
+
+biblioteca_steam = [
+"Cs 1.6", "Team Fortress 2", "Left4Dead", "Half-Life", "Portal2", "Dota2", "Monster Hunter",
+"Counter-Strike 2", "Half-Life 2", "Portal", "Left 4 Dead 2", "Garry's Mod",
+"Rust", "ARK: Survival Evolved", "Terraria", "Stardew Valley", "Hades",
+"Dead Cells", "The Witcher 3", "Cyberpunk 2077", "Elden Ring", "Dark Souls III",
+"Sekiro: Shadows Die Twice", "Red Dead Redemption 2", "GTA V", "Fallout 4",
+"Skyrim", "DOOM Eternal", "Wolfenstein II", "BioShock Infinite",
+"The Forest", "Subnautica", "No Man's Sky", "Valheim", "Among Us",
+"Phasmophobia", "Payday 2", "Borderlands 2", "Borderlands 3",
+"Far Cry 5", "Far Cry 3", "Assassin's Creed Odyssey",
+"Assassin's Creed Valhalla", "Resident Evil 2", "Resident Evil 4",
+"Devil May Cry 5", "Metal Gear Solid V", "Hitman 3", "Control",
+"Alan Wake", "Outlast", "The Evil Within", "Cuphead",
+"Hollow Knight", "Celeste", "Ori and the Blind Forest",
+"Ori and the Will of the Wisps", "Slay the Spire", "Factorio",
+"Satisfactory", "Cities: Skylines", "Euro Truck Simulator 2",
+"American Truck Simulator"
+]
+
+
+
+print(biblioteca_steam[38])
+
+for elemento in biblioteca_steam:
+    print(f"Juego N {contador} {elemento}")
+    contador = contador + 1
+
+
+
+
+
+
