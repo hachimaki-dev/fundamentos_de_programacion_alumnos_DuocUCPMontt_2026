92f9a2e0a9a2bf87142ac2fb4034449cef55128a
nicoleasd <nicaguilar2006@gmail.com>
2026-04-20 10:39:20 -0400

    Inicios del for

---
+++ Ej_Ataques_Pikachu.py
@@ -0,0 +1,7 @@
+ataques = ['Impactrueno' , 'Ataque Rápido' , 'Rayo' , 'Cola Férrea'] 
+
+for i  in ataques: 
+    print(f"Pikachu puede usar: {i}") 
+
+total_ataques = len(ataques) 
+print(f"Pikachu tiene un total de {total_ataques} ataques disponibles.")
