261fd6b3742c1c14542776e06e7717141547dd3d
LeCoward <sebabarri2710@gmail.com>
2026-04-20 14:11:24 -0400

    avance en clase porfavor terminar casa

---
+++ for_clases/ataques_pikachu.py
@@ -0,0 +1,5 @@
+ataques = ["Impactrueno", "Ataque Rápido", "Rayo", "Cola Férrea"]
+
+for attack in ataques :
+    print(f"{attack}")
+print("Pikachu tiene un total de ", + len(ataques) " disponibles")
