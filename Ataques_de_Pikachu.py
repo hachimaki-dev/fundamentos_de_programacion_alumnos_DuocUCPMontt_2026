358979ce6382ed3d9a17c12b41c114be4abf730b
flame6672 <gustavoa3498@gmail.com>
2026-04-20 20:16:12 -0400

    Primer ejercicio de la EA2

--- Ataques_de_Pikachu.py
+++ Ataques_de_Pikachu.py
@@ -1,4 +1,4 @@
 ataques = ["Impactrueno", "Ataque Rápido", "Rayo", "Cola Férrea"]
-for golpe in ataques:
-    print(f"Pikachu puede usar: {ataques[golpe]}")
+for i in ataques:
+    print(f"Pikachu puede usar: {i}")
 print(f"Pikachu tiene un total de {len(ataques)} ataques disponibles.")
