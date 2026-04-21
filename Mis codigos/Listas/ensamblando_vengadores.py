f89fe576180a3ec86e116fca2ca21d7e1934e688
agcamila01 <agcamila003@gmail.com>
2026-04-20 23:31:37 -0400

    vengadores

--- Mis codigos/Listas/ensamblando_vengadores.py
+++ Mis codigos/Listas/ensamblando_vengadores.py
@@ -12,14 +12,33 @@
 # Opcional de Destrucción: Integra la opción de permitir hacer vengadores.pop() si el usuario escribe la palabra secreta "Sacrificar".
 
 vengadores = []
+palabra_secreta = "SACRIFICAR"
 
 while True:
-    print("Menú: \n1. Agregar Avenger\n2. Mostrar Base\n3. Sacrificar\n4. Salir")
+    print("\nMenú: \n1. Agregar Avenger\n2. Mostrar Base\n3. Destrucción\n4. Salir")
     eleccion = int(input("Ingrese su elección: "))
     if eleccion == 1:
-        nombre_heroe = input("Ingrese el nombre del héroe: ")
+        nombre_heroe = input("\nIngrese el nombre del héroe: ")
         vengadores.append(nombre_heroe)
-    if eleccion == 2:
+    elif eleccion == 2:
+        print("\n-------- BASE --------")
         for i in range(len(vengadores)):
-            print(i + "-" + vengadores[i])
+            print(f"{i}-{vengadores[i]}")
             vengadores[i] = vengadores[i].upper()
+        print("----------------------")
+    elif eleccion == 3:
+        print("\nHa ingresado al modo de DESTRUCCIÓN...")
+        palabra_ingresada = input("Ingrese la palabra secreta para eliminar a un héroe: ").upper()
+        if len(vengadores) > 0 and palabra_ingresada == palabra_secreta:
+            vengadores.pop()
+            print("----- Héroe eliminado -----")
+        elif palabra_ingresada != palabra_secreta:
+            print("\nNo has adivinado la palabra...")
+        else:
+            print("\n----- No hay héroes en la base -----")
+    elif eleccion == 4:
+        print("\nSaliendo del programa...")
+        break
+    else:
+        print("\nIngrese una opción correcta")
+
