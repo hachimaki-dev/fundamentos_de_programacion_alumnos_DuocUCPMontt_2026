9acbf5d24d66d522dbb830d56be9e4d651402933
YukiTheFunkiest <mtkonar@gmail.com>
2026-04-21 08:35:36 -0400

    Desafio de while loop

---
+++ lista_de_juegos_con_while.py
@@ -0,0 +1,49 @@
+# Never do this btw
+
+juego1 = "CS 1.6"
+juego2 = "Team Fortress 2"
+juego3 = "Portal"
+juego4 = "Portal 2"
+juego5 = "Left 4 Dead 2"
+juego6 = "Factorio"
+juego7 = "Kerbal Space Program"
+juego8 = "Terraria"
+
+libreria_de_juegos = ["CS 1.6", "Team Fortress 2", "Portal", "Portal 2", "Left 4 Dead 2", "Factorio", "Kerbal Space Program", "Terraria"]
+
+contador = 0
+juego = ""
+
+while contador < 8:
+    juego_n = "juego" + str(contador + 1)
+    if juego_n == "juego1":
+        print(juego1)
+        contador +=1
+    if juego_n == "juego2":
+        print(juego2)
+        contador +=1
+    if juego_n == "juego3":
+        print(juego3)
+        contador +=1
+    if juego_n == "juego4":
+        print(juego4)
+        contador +=1
+    if juego_n == "juego5":
+        print(juego5)
+        contador +=1
+    if juego_n == "juego6":
+        print(juego6)
+        contador +=1
+    if juego_n == "juego7":
+        print(juego7)
+        contador +=1
+    if juego_n == "juego8":
+        print(juego8)
+        contador +=1
+
+print("\n V V V Ahora utilizando una lista V V V \n")
+
+contador = 0
+while contador < 8:
+    print(libreria_de_juegos[contador])
+    contador +=1
---
+++ taquilla_del_cine_kawaii.py
@@ -0,0 +1,12 @@
+# Gestionar la venta de N entradas en una sala de cine.
+
+"""
+1. Pregunta cuántas entradas se van a comprar en total en esta transacción.
+2. Usa un ciclo while para procesar CADA entrada.
+3. Para cada entrada, pregunta la EDAD de la persona.
+- Si es menor de 12 años, su entrada cuesta $3.000.
+- Si tiene entre 12 y 64 años, la entrada cuesta $6.000.
+- Si es adulto mayor (65 o más), la entrada cuesta $4.000.
+4. Usa un acumulador para sumar el precio de todas las entradas.
+5. Imprime al final el gran total recaudado en esa transacción.
+"""
