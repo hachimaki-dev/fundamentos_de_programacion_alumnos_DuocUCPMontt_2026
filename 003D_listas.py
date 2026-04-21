19763db8a01c454dd9da18bffd5609fca67ecb2a
benjamin80 <benjaminbusta45@gmail.com>
2026-04-20 23:57:13 -0400

    commit de steam y pikachu

---
+++ 003D_listas.py
@@ -0,0 +1,55 @@
+#mi biblioteca de juegos de steam
+#mi_juego1 = "cs 1.6"
+#mi_juego2 = "Team Fortress 2"
+#mi_juego3 = "Half Life"
+#mi_juego4 = "Left 4 Dead"
+#mi_juego5 = "Portal"
+#mi_juego6 = "Gmod"
+#mi_juego7 = "Dota 2"
+#mi_juego8 = "Day of defeat"
+#mi_juego9 = "Hollow Knight"
+#mi_juego10 = "Sonic mania"
+
+
+#manera mas algortimica
+
+
+
+
+
+
+
+
+
+#arreglo_cualquiera = ["Carlo", "Josmer", "Conner", "Camilo", "Olaguier"]
+#arreglo_cualquiera_edad = [31, 19, 19, 19]
+#arreglo_cualquiera_estaturas = [1.71, 1.76, 1.70, 1.65]
+biblioteca_steam = ["cs 1.6", "Team Fortress 2", "Left4dead", "Half-Life", "Portal", "Dota2", "Monster Hunter", "Sonic Mania", "Doom 3","PUBG: Battlegrounds", "Payday 2"]
+
+#para obtener el tamaño o la longitud de algo
+biblioteca_steam.append("Among us")
+
+biblioteca_steam.append("Pokémon")
+
+biblioteca_steam.append("Garrys mod")
+
+biblioteca_steam.pop(4) #el 4 elemento del arreglo
+
+#cuando yo se como se llama lo que quiero
+biblioteca_steam.remove ("Team Fortress 2")
+#por cada elemento dentro de una coleccion de elementos
+
+
+print(biblioteca_steam {10})
+
+for rango in range(1,5):
+
+for elemento in biblioteca_steam:
+    print (f"Juego N {contador} {elemento}")
+    contador = contador + 1 
+
+
+
+
+
+#print(biblioteca_steam[1])
