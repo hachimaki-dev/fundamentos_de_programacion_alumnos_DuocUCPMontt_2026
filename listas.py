bc8f2fbdd1377435d698d34cfbb4141301c35dfb
flame6672 <gustavoa3498@gmail.com>
2026-04-20 20:23:41 -0400

    Ajuste en mis apuntes

--- listas.py
+++ listas.py
@@ -4,6 +4,6 @@ curso_edad = ['???', '???', '???', '???', '???']
 # opcion = int(input("¿Que datos quiere extraer? "))
 # print(f"Datos recibidos: \n{curso_nombres[opcion]} \n{curso_edad[opcion]}")
 
-for nombre in curso_nombres:
-    print(f"El nombre es: {nombre}, este dato es el N°{contador}")
+for nombre, edad in zip(curso_nombres, curso_edad):
     contador += 1
+    print(f"El nombre es: {nombre} \nSu edad es: {edad} \nEste dato es el N°{contador}\n")
