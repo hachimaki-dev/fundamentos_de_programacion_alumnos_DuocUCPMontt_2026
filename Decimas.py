bd205822a8edf7e3aa1533def56da8aa5e720c26
RelarSamsa <vhernandezp4@gmail.com>
2026-04-22 09:46:07 -0400

    Add files via upload

---
+++ Decimas.py
@@ -0,0 +1,28 @@
+#Vida de Cahara
+vida_headcahara = 20
+vida_torsocahara = 425
+vida_leftarmcahara = 20
+vida_rightarmcahara = 20
+vida_leftlegcahara = 20
+vida_rightlegcahara = 20
+#Vida de Crow Mauler
+vida_headcrowmauler = 500
+vida_torsocrowmauler = 1250
+vida_leftarmcrowmauler = 700
+vida_rightarmcrowmauler = 700
+vida_leftlegcrowmauler = 200
+vida_rightlegcrowmauler = 200
+
+#Skills de Cahara
+left_arm =
+right_arm =
+
+#Skills de Crow Mauler
+##Head
+peck = 
+flock_of_crows = 
+skill_head = [peck, flock_of_crows]
+###Left Arm
+maul = 
+
+#Inventario
