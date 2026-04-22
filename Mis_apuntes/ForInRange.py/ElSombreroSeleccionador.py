e067693b508093900f04e38532c8ec838ad9641b
bellium-xls <sanscraftinghd@gmail.com>
2026-04-21 21:42:24 -0400

    sisis

--- Mis_apuntes/ForInRange.py/ElSombreroSeleccionador.py
+++ Mis_apuntes/ForInRange.py/ElSombreroSeleccionador.py
@@ -1,10 +1,14 @@
 alumnos = ["Harry", "Hermonie", "Ron", "Draco", "Neville", "Luna"]
 gryffindor = []
 slytherin = []
-for letras in alumnos:
-    if len(letras) > 5:
-        alumnos.append(gryffindor)
-    elif len(letras) <= 5:
-        alumnos.append(slytherin)
+for alumno in alumnos:
+    if len(alumno) > 5:
+        gryffindor.append(alumno)
+    elif len(alumno) <= 5:
+        slytherin.append(alumno)
 print(f"La casa Gryffindor tiene : {gryffindor}.")
 print(f"La casa Slytherin tiene : {slytherin}.")
+for alumno_slytherin in slytherin:
+    print(alumno_slytherin)
+for alumno_gryffindor in gryffindor:
+    print(alumno_gryffindor)
