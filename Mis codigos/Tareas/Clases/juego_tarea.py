34214374d38739ce3c40e8c6b3b6e1250147b64d
vzmaedo-hub <vzmaedo@gmail.com>
2026-04-23 15:48:06 -0400

    mis codigos

--- Mis codigos/Tareas/Clases/juego_tarea.py
+++ Mis codigos/Tareas/Clases/juego_tarea.py
@@ -5,25 +5,39 @@ rasen_shuriken = 50
 chidogan = 200
 
 import time
+import random
+
+escupir_acido = 100
+garras = 200
+mordedura = 300
+
+dinero = 0
+
+ataque1 = ["Escupir Ácido", 100]
+ataque2 = ["Garras", 200]
+ataque3 = ["Mordedura", 300]
+
+opciones_jefe1 = [ataque1, ataque2, ataque3]
+
+pocion_curativa_menor = 300
+pocion_curativa_media = 500
+pocion_curativa_meyor = 700
 
 
 print("BIENVENIDO A LUMINARY")
 print("")
 time.sleep(1)
+
 nombre_jugador = input("Para empezar ¿Como te llamas?")
 print("")
 time.sleep(1)
 print(f"Perfecto. Bienvenido {nombre_jugador}")
 print("")
 time.sleep(2)
-print("Para comenzar, te explicarè el juego")
-time.sleep(3)
-comprension_juego = input("Te encontraràs con 3 secciones. En cada secciòn hay 1 jefe, tendràs que derrotarlo para avanzar hacia la siguiente secciòn. Comienzas con 2 poderes, tendràs que seleccionar uno de estos para atacar, esto se hace con las letras (1) y (2). Màs adelante se te darà el menu de selecciòn y veràs que poderes posees. ¿Comprendido? 1).si 2).no")
-while comprension_juego != "1":
-    print("")
-    comprension_juego = input("Te encontraràs con 3 secciones. En cada secciòn hay 1 jefe, tendràs que derrotarlo para avanzar hacia la siguiente secciòn. Comienzas con 2 poderes, tendràs que seleccionar uno de estos para atacar, esto se hace con las letras (1) y (2). Màs adelante se te darà el menu de selecciòn y veràs que poderes posees. ¿Comprendido? 1).si 2).no")
 print("")
-
+print("Comencemos el juego")
+print("")
+time.sleep(1)
 print("Despiertas a oscuras. No sabes donde te encuentras ni el por que. Miras a tu alrededor y de pronto aparece una luz que te ciega completamente.")
 print("")
 time.sleep(3)
@@ -32,26 +46,41 @@ print("")
 time.sleep(3)
 print(f"{nombre_jugador}: ...")
 time.sleep(2)
+print("")
 print(f"{nombre_jugador}: ¿Còmo sabes mi nombre?.")
 print("")
 time.sleep(3)
 print("???: Eso ahora mismo no importa. Te explicarè como funcionan las cosas aquì.")
 time.sleep(2)
-print(f"???: Cuentas con {vida_jugador}HP. Tienes que derrotar al jefe de esta secciòn. El jefe tendra UNICAMENTE en esta seccion {vida_jefe1}HP, ya que, a medida que avances te encontraràs con enemigos mucho màs poderosos. Tus poderes son los siguientes:")
+print("")
+print(f"???: Cuentas con {vida_jugador}HP. Tienes que derrotar al jefe de esta secciòn. El jefe tendra UNICAMENTE en esta seccion {vida_jefe1}HP, ya que, a medida que avances te encontraràs con enemigos mucho màs poderosos. Tambièn tu poder irà incrementando a medida que avances. Te otorgarè algunas habilidades para que comiences con tu aventura.")
 time.sleep(5)
-print("Poderes:")
 print("")
-print(f"1).Chidogan {chidogan}Ptos de daño")
-print("")
-print(f"2).Rasen Shuriken {rasen_shuriken}Ptos de daño")
-print("")
-time.sleep(3)
 print("*Has adquirido Chidogan y Rasen Shuriken*")
 print("")
 time.sleep(3)
+respuesta1 = int(input("1).Muchas gracias 2)...."))
+if respuesta1 == 1:
+    print("")
+    print(f"{nombre_jugador}: Muchas gracias")
+    print("")
+    time.sleep(2)
+    print("???: No hay de que.")
+    print("")
+    time.sleep(2)
+elif respuesta1 == 2:
+    print("")
+    print(f"{nombre_jugador}: ...")
+    time.sleep(2)
+print("???: Ah... se me olvidaba algo.")
+print("")
+time.sleep(2)
+print("*Has adquirido pociòn curativa (3)*")
+print("")
+time.sleep(2)
 print("???: Buena suerte... la necesitaràs.")
 print("")
-time.sleep(1)
+time.sleep(2)
 print(f"{nombre_jugador}: ¡¡¡ESPERA!!!")
 print("")
 time.sleep(3)
@@ -59,6 +88,7 @@ print("Ves como lo que habia ahì desaparece por completo. A los extremos de las
 print("")
 time.sleep(10)
 print("Comienza La Batalla")
+time.sleep(2)
 print("")
 print(f"---Darkorus---HP:{vida_jefe1}---")
 print("")
@@ -66,15 +96,31 @@ print(f"---{nombre_jugador}---HP:{vida_jugador}")
 print("")
 print("Habilidades: ")
 print("")
-print("1).rasen shuriken")
+print("1).rasen shuriken - 50")
 print("")
-print("2).chidogan")
+print("2).chidogan - 200")
 print("")
-print("3).Esquivar")
+print("3).Pociòn curativa - +300")
 print("")
+
+contador_curativa = 0
+
 while vida_jefe1 > 0:
-    ataque = input("Escoge una habilidad")
+    print("Habilidades: ")
     print("")
+    print("1).rasen shuriken - 50")
+    print("")
+    print("2).chidogan - 200")
+    print("")
+    print("3).Pociòn curativa - +300")
+    print("")
+    ataque = input("")
+    print("")
+    
+    while ataque == "3" and contador_curativa >= 3:
+        ataque = input("No te quedan pociones, escoja otra opciòn")
+        print("")
+
     if ataque == "1":
         vida_jefe1 = vida_jefe1 - rasen_shuriken
         print(f"Has infligido {rasen_shuriken} Ptos de daño.")
@@ -92,27 +138,35 @@ while vida_jefe1 > 0:
         print(f"---Darkorus---HP:{vida_jefe1}---")
         print("")
         time.sleep(1)
-
-    else:
+    
+    elif ataque == "3" and contador_curativa < 3:
+        vida_jugador = vida_jugador + pocion_curativa_menor
+        print(f"Tu salud ha incrementado +300. Tu salud actual es de {vida_jugador}")
+        contador_curativa = contador_curativa + 1
+        print("")
+    
+    elif ataque != "1" and ataque != "2" and ataque != "3":
         print("Ataque nulo. Pierdes un turno")
         print("")
         time.sleep(1)
         print(f"HP:{vida_jefe1}")
         print("")
         time.sleep(1)
-    
+
 
     if vida_jefe1 > 0:
         
-         vida_jugador = vida_jugador - 100
-         print("")
-         time.sleep(1)
-         print(f"Te han inflingido 100 Ptos de daño.")
-         print("")
-         time.sleep(1)
-         print(f"---{nombre_jugador}---HP:{vida_jugador}")
-         print("")
-         time.sleep(1)
+        ataque_del_jefe = random.choice(opciones_jefe1)
+        nombre_ataque_jefe1 = ataque_del_jefe[0]
+        daño_ataque_jefe1 = ataque_del_jefe[1]
+        vida_jugador = vida_jugador - daño_ataque_jefe1
+        print(f"Darkous ha utilisado  {nombre_ataque_jefe1}. Te ha infligido {daño_ataque_jefe1}Ptos de daño")
+        print("")
+        time.sleep(1)
+        print(f"---{nombre_jugador}---HP:{vida_jugador}")
+        print("")
+        time.sleep(1)
+
     while vida_jugador <= 0:
         print("Game Over")
         print("")
@@ -130,21 +184,50 @@ while vida_jefe1 > 0:
         while continuar_1 != 1 and continuar_1 != 2:
             continuar_1 = input("Ingrese una respuesta valida")
 
-        
 
 print("")
 print("Un destello te ciega la vista")
 print("")
-time.sleep(1)
+time.sleep(2)
 print(f"???: Veo que lograste pasar la primera etapa. Muy bien hecho {nombre_jugador}")
+time.sleep(2)
 print("")
-time.sleep(3)
+print("???: Ten tu recompensa")
+time.sleep(2)
+print("")
+print("*Has conseguido 200 Monedas de plata*")
+dinero = dinero + 200
+time.sleep(2)
+print("")
+print("???: Se me olvidò explicarte.")
+time.sleep(2)
+print("")
+print("???: Cada vez que pases de secciòn te darè una recompansa. Podràs comprar pociones con estas monedas, nada mal ¿no?.")
+print("")
+time.sleep(2)
 print(f"{nombre_jugador}: Ya dime que es lo que pasa")
 print("")
-time.sleep(3)
+time.sleep(2)
 print("???: Aun no es el momento... Ya lo sabras")
 print("")
 time.sleep(2)
 print(f"{nombre_jugador}: ¿Entonces... ahora que es lo siguiente?")
-time.sleep(3)
+time.sleep(2)
+print("")
+print("???: Lo siguiente serà tu primera compra en la tienda.")
+time.sleep(2)
+print("")
+print("TIENDA DE ALQUIMIA")
+print("")
+print("1).Pociòn de vida - $100")
+print("")
+print("2).Pociòn de vida media - $300")
+print("")
+print("3).Pociòn de vida mayor - $500")
+print("")
+print("4).Salir")
+print("")
+eleccion_compra = input("¿Què deseas comprar?")
+if eleccion_compra == "1":
     
+
