0e27b71ac991b6e4a9015e25da2762ef3dae6fb9
agcamila01 <agcamila003@gmail.com>
2026-04-22 23:24:59 -0400

    menu dificultad y pista

--- Mis codigos/Tarea Juego/juego.py
+++ Mis codigos/Tarea Juego/juego.py
@@ -1,26 +1,94 @@
-
 palabras_correctas = ["PYTHON", "CODIGO", "LOGICA", "BUCLE", "LISTA", "VARIABLE", "CADENA", "ENTERO", "FUNCION", "CONSOLA", "OBJETO", "RECURSO", "PROCESO", "TECLADO", "PANTALLA"]
 palabras_desordenadas = ["YPHTNO", "GIDOCO", "CIALGO", "ELUBC", "STILA", "BAVALRIE", "NEDACA", "REETNO", "COINFUN", "OLSACON", "TEJOBO", "SRUOCRE", "SCEOPRO", "DALOTEC", "TALLAPAN"]
 
-print("Bienvenido al juego")
-print("Instrucciones: ")
+print("Bienvenido al juego") #
+print("Instrucciones: ") #
 
 nombre_jugador = input("Ingrese su nombre: ")
-palabra_elegida = ""
+palabra_seleccionada = ""
 palabra_correcta = ""
-intentos = 5
+opcion_dificultad = 0
+vidas = 0
 
 while True:
-    eleccion = int(input("Ingrese un numero del 1 al 15: "))
-    palabra_elegida = palabras_desordenadas[eleccion-1]
-    palabra_correcta = palabras_correctas[eleccion-1]
-    print(f"Ordena la palabra: {palabra_elegida}")
-    print(palabra_correcta)
-    while intentos > 0:
-        if palabra_elegida == palabra_correcta:
-            print("ganaste")
-            break
-            
+    modo_de_juego = int(input("Elija su modo de juego:\n1. Modo Libre (Una palabra)\n2. Modo Clásico (Todas las palabras)\n3. Salir del juego\n"))
+    if modo_de_juego == 1 or modo_de_juego == 2:
+        while True:
+            opcion_dificultad = int(input("Dificultad: \n1. Fácil\n2. Medio\n3. Difícil\n4. Extremo\n"))
+            if opcion_dificultad == 1:
+                dificultad = "Fácil"
+                vidas = 10
+                break
+            elif opcion_dificultad == 2:
+                dificultad = "Medio"
+                vidas = 5
+                break
+            elif opcion_dificultad == 3:
+                dificultad = "Difícil"
+                vidas = 3
+                break
+            elif opcion_dificultad == 4:
+                dificultad = "Extremo"
+                vidas = 1
+                break
+            else:
+                print("Por favor ingrese una opción válida")
+        if modo_de_juego == 1:
+            print("----------------------------------------")
+            print("Modo de Juego: Modo Libre (Una palabra)")
+            print(f"Dificultad: {dificultad}")
+            print(f"Vidas: {vidas}")
+            print("----------------------------------------")
+            print("Presione ENTER para comenzar")
+            input()
 
-    break
+            # incompleto
+            indice_palabra = int(input(f"Ingrese un numero del 1 al {len(palabras_desordenadas)}: "))
+            palabra_seleccionada = palabras_desordenadas[indice_palabra-1] # Restamos 1 porque la lista comienza desde el 0
+            palabra_correcta = palabras_correctas[indice_palabra-1]
+            while vidas > 0:
+                respuesta_usuario = input(f"Ordena la palabra {palabra_seleccionada}: ").upper()
+                if respuesta_usuario == palabra_correcta:
+                    print(f"¡Correcto! ¡Felicidades {nombre_jugador}!")
+                    # seguir jugando?
+                    break
+                else:
+                    vidas -= 1
+                    print("¡Incorrecto!")
+                    print(f"Vidas restantes: {vidas}")
+                    pista = "" # Inicializado aqui para que no se concatene cada vez que se pierde una vida
+                    for letra in range(len(palabra_correcta)):
+                        if letra % 2 == 0: # Para mostrar solo letra por medio
+                            pista = pista + palabra_correcta[letra]
+                        else:
+                            pista = pista + "_"
+                    if dificultad == "Fácil": # Si elige dificultad fácil, se ve la pista cada vez que el usuario se equivoque
+                        print(f"Pista: {pista}")
+                    elif dificultad == "Medio": # Se ve una pista fome cuando queden 2 vidas y pista buena cuando quede 1 vida
+                        if vidas == 2:
+                            print(f"Pista: La palabra comienza con {palabra_correcta[0]}")
+                        elif vidas == 1:
+                            print(f"Pista: {pista}")
+                    elif dificultad == "Difícil" and vidas == 1: # Solo se ve la pista fome cuando queda 1 vida
+                        print(f"Pista: La palabra comienza con {palabra_correcta[0]}")
+                    
+                    # vidas == 0??
+            break # jugar otra vez o salir
+
+        elif modo_de_juego == 2:
+            print("----------------------------------------")
+            print("Modo de Juego: Modo Clásico (Todas las palabras)")
+            print(f"Dificultad: {dificultad}")
+            print(f"Vidas: {vidas}")
+            # puntaje
+            print("----------------------------------------")
+            print("Presione ENTER para comenzar")
+            input()
+
+            # incompleto
+
+    elif modo_de_juego == 3:
+        break
+    else:
+        print("Ingrese una opcion válida")
 
