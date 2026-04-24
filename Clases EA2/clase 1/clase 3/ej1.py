d8279ca5ecc13d355ab5e2fc33b37398df01331d
Roten-chan <henriquezzobeiba@gmail.com>
2026-04-24 10:45:53 -0400

    jeje

---
+++ Clases EA2/clase 1/clase 3/ej1.py
@@ -0,0 +1,11 @@
+libro = {
+    "Titulo" : "The Storm Whale",
+    "Autor" : "Benji Davies",
+    "Año" : "2013"
+}
+print(libro['Titulo'])
+print(libro['Autor'])
+print(libro['Año'])
+
+libro["Editorial"] = "Simon & Schuster Childrens Books"
+print(libro["Editorial"])
---
+++ Clases EA2/clase 1/clase 3/ejemplo.py
@@ -0,0 +1,11 @@
+personaje = {
+    "Nombre" : "Aurora",
+    "Posicion" : "Medio", 
+    "Rol" : "Mago",
+    "Tipo de daño" : "AP / Daño mágico",
+    "Región" : "El Freljord"
+}
+print(f"{personaje["Nombre"]}")
+
+personaje["Nombre"] = "Ashe"
+print(f"{personaje["Nombre"]}")
