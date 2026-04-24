fa61a2a0589fa13b22206adea93f270f4ec4a5b0
nicoleasd <nicaguilar2006@gmail.com0>
2026-04-24 09:36:02 -0400

    ejercicios_diccionarios

---
+++ Ejercicio2_diccionarios.py
@@ -0,0 +1,20 @@
+lacteos = {
+    "nombre" : "Leche",
+    "precio" : 1000,
+    "stock" : 15
+
+}
+
+legumbres = {
+    "nombre" : "Lentejas",
+    "precio" : 1500,
+    "stock" : 0
+
+}
+
+verduras = {
+    "nombre" : "Zapallo",
+    "precio" : 900,
+    "stock" : 20
+
+}
