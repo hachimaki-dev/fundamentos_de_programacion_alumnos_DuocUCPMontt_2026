f09f6b368a677772fe4e55970cdf94ae604f063c
CheelaMS <golhabesoto@proton.me>
2026-04-26 16:41:40 -0400

    agregadas cositas(seleccion,y diccionarios v1)

--- Apuntes_clases/Tabajo_Decimas.py
+++ Apuntes_clases/Tabajo_Decimas.py
@@ -1,9 +1,64 @@
 import random
-Pokemones_1 = ["Pikachu",  "Charizard",  "Gengar",  "Ninetales-Alola", " Tyranitar",  "Gardevoir"]
-Pokemones_2 = ["Sandslash", "Snorlax", "Flygon", "Mimikyu", "Swampert"," Weavile"]
-while True:
-    Pokemon_Yo_Te_Elijo = input(f"¿Con cual pokemon quieres iniciar?{Pokemones_1}")
-    print(f"Has comenzado con {Pokemon_Yo_Te_Elijo}")
-    rival = random.choice(Pokemones_2)
-    print(f"Tu rival va a comenzar con {rival}")
+
+#Variables
+Pokemones_jugador = ["Pikachu",  "Charizard",  "Venasaur", "Blastoise"]
+Pokemones_rival = ["Sandslash", "Gyarados", "Exeggutor", "Arcanine"]
+Pokemones_Debilitados = []
+Pokemon_activo_jugador = []
+Pokemon_activo_rival = []
+Combate = True
+Error= "Opcion Invalida"
+SeleccionJugador= True
+Tipo_pokemon = ""
+vida_pokemon_activo =
+SelecionRival = True
+
+#Dicionarios
+dpikachu = {
+    "tipo":"Electrico"
+    "vida":20
+}
+dcharizard = {}
+
+while Combate == True:
+    
+
+    while SeleccionJugador == True:
+        print(f"¿Con cual pokemon quieres iniciar?")
+        print("Opciones:")
+        
+        for i in Pokemones_jugador:
+            print (i)
+        Pokemon_Yo_Te_Elijo = (input("Elija un pokemon "))
+
+        if Pokemon_Yo_Te_Elijo in Pokemones_jugador:
+            Pokemon_activo_jugador.append(Pokemon_Yo_Te_Elijo)
+            Pokemones_jugador.remove(Pokemon_Yo_Te_Elijo)
+            SeleccionJugador= False
+        else:
+            print (Error)
+
+
+    #print(f"DEBUG {Pokemones_jugador} y {Pokemon_activo_jugador}")  
+           
+            
+    #Selecion Rival  
+    while SelecionRival == True:
+    
+        rival = random.choice(Pokemones_rival)
+        print(f"Tu rival va a comenzar con {rival}")
+        Pokemon_activo_rival.append(rival)
+        Pokemones_rival.remove(rival)
+        SelecionRival = False
+    
+    #print(f"DEBUG {Pokemones_rival} y {Pokemon_activo_rival}")  
+    
+    Turno Jugador
+    
+    
+    Combate=False    
+
+
+
+
 
