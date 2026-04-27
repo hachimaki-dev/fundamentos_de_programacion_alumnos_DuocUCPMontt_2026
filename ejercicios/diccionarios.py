b8d55f313878007cbb5a919f531fc18b3fbbcd91
AphidCiro <navarrogamboajulian@gmail.com>
2026-04-27 08:33:54 -0400

    wiwowi

---
+++ ejercicios/diccionarios.py
@@ -0,0 +1,14 @@
+libro = {
+    "titulo": "wowa",
+    "autor": "wiwa",
+    "año": 20
+}
+
+print(libro["titulo"])
+print(libro["autor"])
+print(libro["año"])
+
+libro["año"] = 30
+print(libro["año"])
+libro["webos"] = True
+print(libro["webos"])
---
+++ ejercicios/wiwiwa.py
@@ -0,0 +1,8 @@
+productos = [
+    {"nombre": ["banana, ono"]},
+    {"precio": 2000},
+    {"stock": 20}
+]
+
+for objetos in productos[0]["nombre"]:
+    print(objetos)
