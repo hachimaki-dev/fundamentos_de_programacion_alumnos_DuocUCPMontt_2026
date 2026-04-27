f7e6c0c2ef3622c3c206d06aef63395ff8bf359f
BlazingFunk2005 <mtkonar@gmail.com>
2026-04-27 17:26:26 -0400

    oal

---
+++ foxhole_test.py
@@ -0,0 +1,61 @@
+regions = {
+    {
+        "Name" : "Deadlands",
+        "Control" : "Neutral",
+        "Warden Control Percentage" : 0.0,
+        "Colonial Control Percentage" : 0.0,
+        "Losses" : 0,
+        "Warden Units" : [],
+        "Colonial Units" : [],
+        "Warden Distance" : 1,
+        "Colonial Distance" : 1
+    },
+
+    {
+        "Name" : "Callahan's Passage",
+        "Control" : "Warden Empire",
+        "Warden Control Percentage" : 100.0,
+        "Colonial Control Percentage" : 0.0,
+        "Losses" : 0,
+        "Warden Units" : [],
+        "Colonial Units" : [],
+        "Warden Distance" : 0,
+        "Colonial Distance" : 2
+    },
+
+    {
+        "Name" : "Umbral Wildwood",
+        "Control" : "Colonial Legion",
+        "Warden Control Percentage" : 0.0,
+        "Colonial Control Percentage" : 100.0,
+        "Losses" : 0,
+        "Warden Units" : [],
+        "Colonial Units" : [],
+        "Warden Distance" : 2,
+        "Colonial Distance" : 0
+    },
+
+    {
+        "Name" : "The Linn of Mercy",
+        "Control" : "Warden Empire",
+        "Warden Control Percentage" : 0.0,
+        "Colonial Control Percentage" : 100.0,
+        "Losses" : 0,
+        "Warden Units" : [],
+        "Colonial Units" : [],
+        "Warden Distance" : 1,
+        "Colonial Distance" : 2
+    },
+
+    {
+        "Name" : "Marban Hollow",
+        "Control" : "Colonial Legion",
+        "Warden Control Percentage" : 100.0,
+        "Colonial Control Percentage" : 0.0,
+        "Losses" : 0,
+        "Warden Units" : [],
+        "Colonial Units" : [],
+        "Warden Distance" : 2,
+        "Colonial Distance" : 1
+    },
+}
