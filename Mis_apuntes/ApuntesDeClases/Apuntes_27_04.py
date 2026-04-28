5e0bc9efa7236169b666e58de0d5ad8538d1f1e1
bellium-xls <sanscraftinghd@gmail.com>
2026-04-27 20:47:12 -0400

    Trabajo Colaborativo

--- Mis_apuntes/ApuntesDeClases/Apuntes_27_04.py
+++ Mis_apuntes/ApuntesDeClases/Apuntes_27_04.py
@@ -1,1 +1,4 @@
-#Aprendimos a conectar nuestra cuenta para iniciar sesión más rápido.
+#Primero : Aprendimos a conectar nuestra cuenta para iniciar sesión más rápido.
+#Search : Podemos hacer replace de algunas palabras en todos los archivos existentes mientras estén presentes en estas.
+#Run & Debug : Encuentra el problema del programa iterando línea por línea.
+#Apredimos a usar el git a traves de la interfaz gráfca.
