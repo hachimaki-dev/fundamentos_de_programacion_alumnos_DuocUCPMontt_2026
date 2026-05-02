8d5ccb2cc8067be50bda99eff3c643ded59d72a8
diego0954-alt <diego0954@icloud.com>
2026-05-02 00:06:14 -0400

    diccionarios python

---
+++ Ejercicio/diccionarios.py
@@ -0,0 +1,7 @@
+casa = {'oliver': "desordenado", 'moana': "hordenada"} #oliver=clave desordenado=valor
+print(casa['oliver'])
+print(casa['moana']) 
+dict[casa]
+casa.keys()
+casa.values()
+casa.items()
