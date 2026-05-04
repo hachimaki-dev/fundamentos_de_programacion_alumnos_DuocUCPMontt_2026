1eaa6eed357356322f0a102a0e9ed7eeef00bfb8
Roten-chan <henriquezzobeiba@gmail.com>
2026-05-04 13:22:54 -0400

    Resuelto ejercicio 9: Costos de Producción (Emprendimiento)

---
+++ Desafio 55/ej9.py
@@ -0,0 +1,19 @@
+#Ejercicio 9: Costos de Producción (Emprendimiento)
+#Vas a vender poleras estampadas y necesitas calcular tus ganancias mensuales.
+
+#Datos Iniciales: Vendes cada polera a 15000. Hacer una polera te cuesta: 4000 la tela, 2500 el estampado y 500 el empaque. Vendiste 50 poleras en el mes.
+
+#Reglas de Negocio: La ganancia por cada polera es su precio de venta menos todo lo que te costó hacerla. La ganancia del mes es esa ganancia por unidad multiplicada por las poleras que vendiste.
+
+#Restricciones: No escribas el resultado a mano, deja que Python haga las sumas y multiplicaciones. Imprime tu ganancia mensual final.
+
+polera = 15000 #Producto final
+
+#Materiales
+tela = 4000
+estampado = 2500
+empaque = 500
+
+ganancia = (polera - (tela + estampado + empaque)) * 50
+
+print(f"La ganancia del mes es ${ganancia}")
