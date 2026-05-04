00eba4ab5fafcbedb6b3764645836530854dc701
AG26535 <alangodoy235@gmail.com>
2026-05-04 19:58:31 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ desafio55/3.py
@@ -0,0 +1,8 @@
+personas_total = 5
+pichangas = 15000*2
+schops = 3500*5
+
+consumo_total = (pichangas + schops)
+propina = (consumo_total *0.10)
+monto_total = (consumo_total + propina) / 5
+print("Cada uno debe pagar: ",monto_total)
