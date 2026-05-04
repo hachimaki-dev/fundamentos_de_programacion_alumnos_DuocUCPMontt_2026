6f00a7596b8e61a43d9392d661f66df2a24590e7
nanyasg <nany.asg10@gmail.com>
2026-05-04 16:55:05 -0400

    ejercicio 2

---
+++ 55 desafios/2.py
@@ -0,0 +1,9 @@
+costo_auto = 5000000
+ahorros = 1500000
+
+dinero_faltante = costo_auto - ahorros
+print(f"para comprar el auto faltan ${dinero_faltante}")
+
+meses_por_ahorrar = dinero_faltante // 150000
+
+print(f"Podre comprar el auto en {meses_por_ahorrar} meses ahorrando")
