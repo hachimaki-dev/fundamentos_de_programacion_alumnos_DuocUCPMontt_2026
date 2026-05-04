3079f5a02e811c9caeea2bab1c91ea96f7761c35
Martin Muñoz <martin.andres.munoz.c@gmail.com>
2026-05-04 12:56:52 -0400

    Resuelto Ejercicio 7: Armando el PC Gamer

---
+++ Desafio55/7.py
@@ -0,0 +1,9 @@
+procesador = 250000
+placa = 120000
+tarjeta_video = 450000
+
+descuento_tarjeta_video = 0.85
+
+total_pagar = procesador + placa + (tarjeta_video * descuento_tarjeta_video)
+
+print(total_pagar)
