bbaf0a4f1158e562b72333a9ecfd166450b0f5ac
Maxiidxss <levmaximiliano@gmail.comm>
2026-05-04 09:39:44 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un auto

---
+++ Ejercicio 2-55.py
@@ -0,0 +1,14 @@
+#Quieres calcular cuántos meses te tomará ahorrar para comprarte un auto
+#auto cuesta 5000000
+#ahorrado 1500000
+#ahorrar 150000 cada mes
+
+auto_total = 5000000
+ahorrado = 1500000
+ahorrar_mes =150000
+
+
+print("dinero faltante") 
+print( auto_total - ahorrado)
+print("Meses necesarios para ahorrar para juntar lo restante")
+print(auto_total //  ahorrar_mes  - 10)
