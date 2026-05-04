850a751527e3b4ba73ce4fd8b05d08206fe7710a
Happy210906 <olaquierpro@gmail.com>
2026-05-04 16:43:03 -0400

    Segundo vamoooooo

---
+++ desafio55/2.py
@@ -0,0 +1,7 @@
+precio_auto = 5000000
+dinero_ahorrado = 1500000
+dinero_mensual = 150000
+dinero_faltante = precio_auto - dinero_ahorrado
+print(f"La cantidad de dinero que te falta son {dinero_faltante}")
+meses_faltantes = dinero_faltante // dinero_mensual
+print(f"Te faltan {meses_faltantes} en total, para el dinero necesario")
