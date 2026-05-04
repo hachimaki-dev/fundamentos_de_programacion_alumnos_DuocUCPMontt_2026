ecf98d3bc3de417e2722cfd048eaf2ab36d1323b
bisente777 <vic.aponte@duocuc.cl>
2026-05-04 17:11:04 -0400

    Resuelto Ejercicio 10: Estacionamiento Aeropuerto

---
+++ desafio55/10.py
@@ -0,0 +1,7 @@
+costo_1er_hora = 1200
+costo_mediahora = 500
+bloques_mediahora = 4
+tiempo_estacionado = 3
+costo_despues1erhora = costo_mediahora*4
+total = costo_1er_hora + costo_despues1erhora
+print(total)
