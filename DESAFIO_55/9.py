a157983e8d74ac5468a7f237c6922e44aa3b24c2
Neiram374 <neiramatias553@gmail.com>
2026-05-04 17:38:18 -0400

    resuelto ejercicio 9: costo de produccion (emprendimiento)

---
+++ DESAFIO_55/9.py
@@ -0,0 +1,9 @@
+ventas = 15000
+tela = 4000
+estampado = 2500
+empaque = 500 
+costo_unitario = tela + estampado + empaque
+ganancia_unitaria = ventas - costo_unitario
+poleras = 50 
+ganancia_mensual = ganancia_unitaria * poleras
+print(ganancia_mensual)
