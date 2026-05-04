f621acad31aa914849124979e8eef36c6fa3f1d0
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-04 19:50:00 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ desafio55/3.py
@@ -0,0 +1,10 @@
+amigos = 5
+pichanga = 15000
+schop = 3500
+
+total_comsumido = (pichanga * 2) + (schop * 5) 
+propina = total_comsumido // 10
+monto_final = total_comsumido + propina 
+monto_persona = monto_final // 5
+
+print(monto_persona)
