d8a33aea685f2467504886e877d961dbfd6fa440
andrewfarias452 <an.fariast@duocuc.cl>
2026-05-04 18:19:10 -0400

    Resuelto Ejercicio 25: Filtro de Palabras (Discord)

---
+++ EA2/desafio55/25.py
@@ -0,0 +1,17 @@
+'''Ejercicio 25: Filtro de Palabras (Discord)
+Crea un robot moderador de chat que censure insultos.
+
+Datos Iniciales: Tienes una lista de mensajes en el chat: `['hola', 'noob', 'genial', 'manco']`.
+
+Reglas de Negocio: El bot revisa las palabras una por una. Las palabras 'noob' y 'manco' están prohibidas. Si el bot ve una de esas palabras, debe cambiarla por el texto '[CENSURADO]'. Si la palabra es buena, la deja igual.
+
+Restricciones: Recorre la lista con un `for`. Adentro, usa un `if` para revisar si la palabra es mala e imprimir la censura, sino, imprime la palabra original.'''
+
+chat = ['hola', 'noob', 'genial', 'manco']
+
+for mensaje in chat:
+    if mensaje == 'noob' or mensaje == 'manco':
+        print('[CENSURADO]')
+    else:
+        print(mensaje)
+        
