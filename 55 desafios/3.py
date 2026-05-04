8564fcf454b23867a6d2d588227fd06890351002
nanyasg <nany.asg10@gmail.com>
2026-05-04 17:09:37 -0400

    ejercicio 3

---
+++ 55 desafios/3.py
@@ -0,0 +1,12 @@
+
+personas = 5
+pichangas = 2*15000
+schops = 5*3500
+
+monto_comida = (pichangas + schops)
+propina = monto_comida * 0.10
+monto_total = monto_comida + propina
+
+
+monto_persona = monto_total / personas
+print(monto_persona)
