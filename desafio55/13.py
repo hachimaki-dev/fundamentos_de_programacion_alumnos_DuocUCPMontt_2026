93af2d591589002bd251b69c25634e35cca18d92
bisente777 <vic.aponte@duocuc.cl>
2026-05-04 17:27:48 -0400

    Resuelto Ejercicio 13: Promoción VIP BancoEstado (OR y AND)

---
+++ desafio55/13.py
@@ -0,0 +1,8 @@
+sueldo_cliente = 800000
+deudas_cliente = 0
+años_en_el_banco = 6
+if sueldo_cliente > 1000000 or (años_en_el_banco > 5 and deudas_cliente == 0):
+    print("Cliente VIP")
+else:
+    print("Cliente normal")
+
