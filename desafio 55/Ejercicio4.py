f424c4d11bf99dd22581eef447c390347d62aef2
4l3xZzz <alexandervillalon54@gmail.com>
2026-05-04 12:53:33 -0400

    Resuelto Ejercicio 4: Calculadora de Cuotas (MercadoLibre)

---
+++ desafio 55/Ejercicio4.py
@@ -0,0 +1,5 @@
+precio_notebook = 1200000
+cuotas = 12
+envio = 15000
+print(f"Compre un notebook que cuesta ${precio_notebook}CLP, lo pague en {cuotas} sin interes, pero el envio se paga si o si en el primer mes")
+print(f"El total lo que tengo que pagar el primer mes es de ${precio_notebook / 12 + envio}CLP")
