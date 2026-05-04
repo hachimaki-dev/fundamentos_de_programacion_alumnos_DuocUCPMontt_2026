447a90d10e07965952bdd9597d7eb3d76a8e78f1
gabriela-pezo <gabrielapezo69@gmail.com>
2026-05-04 16:30:36 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ gitflow_gabu/Desafio55.py/2.py
@@ -0,0 +1,6 @@
+costo_auto = 5000000
+ahorro_previo = 1500000
+meta_mensual = 150000
+plata_faltante = costo_auto - ahorro_previo
+meses_necesarios = plata_faltante // meta_mensual
+print("Estos son los meses que te faltan para alcanzar la meta: ", meses_necesarios)
