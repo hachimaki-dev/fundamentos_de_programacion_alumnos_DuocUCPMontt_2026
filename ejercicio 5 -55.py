796645e7baf221f5a88431bbb7087fa899fada43
Maxiidxss <levmaximiliano@gmail.comm>
2026-05-04 17:40:38 -0400

    Resuelto Ejercicio 5: Administrador de Carga Bip! Avanzado

---
+++ ejercicio 5 -55.py
@@ -0,0 +1,11 @@
+#tarjeta empieza con 10000
+#El pasaje de micro cuesta 730
+# metro en punta cuesta 850. Además, harás una recarga de 3000.
+
+tarjeta_inicio = 10000
+pasaje_micro = 730
+pasaje_metro = 850
+recarga = 3000
+saldo_total = 13000
+
+print(tarjeta_inicio - pasaje_micro - (pasaje_metro * 2) + recarga)
