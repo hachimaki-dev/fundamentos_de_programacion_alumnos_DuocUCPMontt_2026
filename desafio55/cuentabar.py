0615a2d6c5d0b7c653bcecff1b6b9a240b7bbb54
MatiasValdivia327 <matias.felipe7777>
2026-05-04 13:59:50 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ desafio55/cuentabar.py
@@ -0,0 +1,12 @@
+consumo = 47500
+
+print("la cuenta por 2 pichanga(15000 cada una) y 5 schops(3500 cada una) es un total de")
+propina = consumo * 0.10
+consumo_total = consumo + propina
+print(f" el total de la cuenta es de {consumo_total} ya que se suma un 10% de propina")
+pago_por_separado = consumo_total / 5
+print(f"cada uno deberia pagar {pago_por_separado}")
+
+
+
+
