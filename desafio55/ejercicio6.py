471bbbc4c61acbd2a6c00fc1c7c32ff05c268c39
TokaiBenja <mirandabenjamin678@gmail.com>
2026-05-04 13:13:26 -0400

    Ejercicio 6

---
+++ desafio55/ejercicio6.py
@@ -0,0 +1,12 @@
+print("Ejercicio 6: Ganancia en criptomonedas (Binance)")
+cripto = "estafa"
+precio_bitcoin = 50000
+dolar_en_pesos = 900
+print(f"Hoy el bitcoin vale {precio_bitcoin}, decidiste comprar 0.05 BTC")
+bitcoin_comprados = 0.05
+precio_bitcoin_actual = 62000
+ganancia_total = precio_bitcoin_actual - precio_bitcoin
+ganancia_real = ganancia_total * bitcoin_comprados
+print(f"La ganancia de BTC ante la subida es de {ganancia_real}")
+print(f"Ganaste ${ganancia_real * dolar_en_pesos} en pesos chilenos")
+
