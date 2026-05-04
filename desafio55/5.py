9e9c109e0cebfdb2f0f97e6e8b8509dffbc6f5d1
darkdevbat <batmanjohan12@gmail.com>
2026-05-04 17:08:30 -0400

    Resuelto Ejercicio 5: Administrador de Carga Bip! Avanzado

---
+++ desafio55/5.py
@@ -0,0 +1,9 @@
+dinero_tarjeta = 10000
+pasaje_micro = 730
+metro = 850
+recarga = 3000
+
+costo_dia = dinero_tarjeta - pasaje_micro - (metro + metro)
+dinero_tarjeta = costo_dia + recarga
+
+print(dinero_tarjeta)
