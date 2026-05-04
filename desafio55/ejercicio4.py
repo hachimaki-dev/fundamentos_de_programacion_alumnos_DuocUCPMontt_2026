b06a756569a831cc607c07dd0f7f31bfc5d1e543
TokaiBenja <mirandabenjamin678@gmail.com>
2026-05-04 12:53:29 -0400

    Ejercicio 4

---
+++ desafio55/ejercicio4.py
@@ -0,0 +1,6 @@
+print("Ejercicio 4: Calculadora de Notas (Mercado Libre)")
+precio_notebook = 1200000
+coste_envio = 15000
+
+monto_primer_mes = precio_notebook // 12 + coste_envio
+print(f"Compraste una laptop en 12 cuotas sin interes, el envio te sale ${coste_envio}, por lo tanto el primer mes pagas ${monto_primer_mes}")
