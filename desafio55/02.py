13792ab84603571ea28a6f9455fb619f5cb0bd26
figueroawa <selena.ale.figueroa@gmail.com>
2026-05-04 12:27:50 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ desafio55/02.py
@@ -0,0 +1,9 @@
+precio_auto = 5000000
+dinero_ahorrado = 1500000
+monto_a_ahorrar_mensual = 150000
+
+dinero_restante = precio_auto - dinero_ahorrado
+
+meses_restantes = dinero_restante // monto_a_ahorrar_mensual
+
+print(meses_restantes)
