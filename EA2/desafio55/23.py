f9baaea324cdc4d956a712922a7f8908d97bc6da
andrewfarias452 <an.fariast@duocuc.cl>
2026-05-04 18:14:17 -0400

    Resuelto Ejercicio 23: Bloqueo de Seguridad (Cajero)

---
+++ EA2/desafio55/23.py
@@ -0,0 +1,22 @@
+'''Ejercicio 23: Bloqueo de Seguridad (Cajero)
+Haz que el cajero se bloquee si alguien intenta adivinar tu clave muchas veces.
+
+Datos Iniciales: Tienes un contador de intentos fallidos que parte en 0.
+
+Reglas de Negocio: Imagina que el sistema está siempre prendido esperando una clave (un ciclo infinito). Si alguien pone mal la clave, sumas 1 al contador. Cuando el contador llegue a 3, el sistema debe imprimir 'Bloqueo de Tarjeta' y apagarse de inmediato para evitar un robo.
+
+Restricciones: Inicia tu ciclo con `while True:`. La única forma de apagar el programa es poner un `if` adentro que use la palabra mágica `break`.'''
+
+intentos_fallidos = 0
+clave = "1234"
+
+while True:
+    clave_ingresada = input("Ingrese su clave: ")
+    if clave_ingresada != clave:
+        intentos_fallidos += 1
+        if intentos_fallidos == 3:
+            print("Bloqueo de tarjeta")
+            break
+    else:
+        print("Ingresando...")
+        
