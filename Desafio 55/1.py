fd4e8eb8e36449678bd22386c26bb8e4b8863963
jjeanfranco <jjeanfrancom@gmail.com>
2026-05-04 19:31:43 -0400

    Resuelto Ejercicio 1: Liquidación de Sueldo Básico

--- Desafio 55/1.py
+++ Desafio 55/1.py
@@ -2,6 +2,6 @@ sueldo_base=500000
 bono_colacion=50000
 movilizacion=30000
 
-sueldo_con_descuento=sueldo_base-(sueldo_base*19/100)
+sueldo_con_descuento=sueldo_base-(sueldo_base*17/100)
 
 print(sueldo_con_descuento+bono_colacion+movilizacion)
