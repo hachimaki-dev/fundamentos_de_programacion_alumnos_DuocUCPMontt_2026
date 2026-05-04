c7bcf221ef65419b02aeb29bf172366c480f8724
CheelaMS <golhabesoto@proton.me>
2026-05-04 09:24:56 -0400

    Resuelto Ejercicio 6: Ganancia en Criptomonedas (Binance)

---
+++ DESAFIOS 55/Ejercicio6_ganaciabinance.py
@@ -0,0 +1,8 @@
+valorantiguo= 50000
+valoractual=62000
+valordolartoclp=900
+
+diferencia= valoractual- valorantiguo
+gananciadolar=diferencia*0.05
+gananciaclp= gananciadolar*valordolartoclp
+print(gananciaclp)
