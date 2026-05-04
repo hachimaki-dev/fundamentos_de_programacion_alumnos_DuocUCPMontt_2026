84e1f2e550685d729a9b04be591a95cfa5323e41
Her-cloud <carcamoh78@gmail.com>
2026-05-04 09:39:56 -0400

    Resuelto Ejercicio 4: Calculadora de Cuotas (MercadoLibre)

---
+++ Desafio 55/ejercicio_4.py
@@ -0,0 +1,6 @@
+# Calculadora de Cuotas (MercadoLibre)
+
+costo_notebook = 1200000 / 12
+consto_envío = 15000
+primera_cuota = costo_notebook + consto_envío
+print('Total a pagar el primer mes: ', primera_cuota)
