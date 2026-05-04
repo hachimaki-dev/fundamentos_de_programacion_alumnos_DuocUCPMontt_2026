3d1567364d58a9a87bf3fa1f3b0bdfb5f7db9d2c
Zizager <zinedineargel12@gmail.com>
2026-05-04 16:46:37 -0400

    Resuelto Ejercicio 6: Ganancia en Criptomonedas (Binance)

---
+++ Desafio55/6.py
@@ -0,0 +1,8 @@
+compra_BTC=0.05
+btc_comprado1=50000
+btc_ahora=62000
+dolar=900
+btc_comprado1=(compra_BTC*btc_comprado1)*dolar
+btc_ahora=(compra_BTC*btc_ahora)*dolar
+ganancia=btc_ahora-btc_comprado1
+print(ganancia)
