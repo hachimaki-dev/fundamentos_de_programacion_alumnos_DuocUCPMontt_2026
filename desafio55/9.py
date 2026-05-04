0369b390dd8848bb9fe79a3bc6837d025d1f0c30
Happy210906 <olaquierpro@gmail.com>
2026-05-04 17:23:34 -0400

    Novena vamooooooo

---
+++ desafio55/9.py
@@ -0,0 +1,8 @@
+precio_polera = 15000
+tela = 4000
+estampado = 2500
+empaque = 500
+vendidas_mes = 50
+ganancia = precio_polera - tela - estampado - empaque
+dinero = ganancia * vendidas_mes
+print(dinero)
