29a912ce0041c217fc3c4358360353fb1cffac69
CamiloDiaz2 <cf.diazs@duocuc.cl>
2026-05-04 15:30:59 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ Desafio 55/2.py
@@ -0,0 +1,9 @@
+auto_costo = 5000000
+ahorro_actual = 1500000
+ahorro_por_mes = 150000 
+
+dinero_faltante = auto_costo - ahorro_actual
+
+meses_necesarios_para_auto = dinero_faltante // ahorro_por_mes
+
+print(meses_necesarios_para_auto)
