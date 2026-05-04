cb726312b9d66794c3f65a885557928cdb9bb3f6
Martin Muñoz <martin.andres.munoz.c@gmail.com>
2026-05-04 13:02:08 -0400

    Resuelto Ejercicio 9: Costos de Producción (Emprendimiento)

---
+++ Desafio55/9.py
@@ -0,0 +1,11 @@
+precio_polera = 15000
+
+tela = 4000
+estampado = 2500
+empaque = 500
+
+cant_poleras_vendidas = 50
+
+ganacias = (precio_polera - (tela + estampado + empaque)) * cant_poleras_vendidas
+
+print(ganacias)
