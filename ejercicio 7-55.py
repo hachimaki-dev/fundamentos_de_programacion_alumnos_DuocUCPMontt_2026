a84fa270aceb8071eecba512fd701b0ab11cdb98
Maxiidxss <levmaximiliano@gmail.comm>
2026-05-04 17:58:53 -0400

    Resuelto Ejercicio 7: Armando el PC Gamer

---
+++ ejercicio 7-55.py
@@ -0,0 +1,11 @@
+#un procesador 250000
+#placa madre 120000
+#tarjeta de video 450000
+#descuento del 15% SÓLO en la tarjeta de video
+
+proce = 250000
+mother = 120000
+placa = 450000
+
+placa_con_descuento = placa * (1 - 0.15)
+print(proce + mother + placa_con_descuento)
