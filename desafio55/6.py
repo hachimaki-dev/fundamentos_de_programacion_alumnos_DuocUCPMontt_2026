6138cb238ee775357bfb0471fca037a124d188f8
axel-trujillo <axeltrujillo1110@gmail.com>
2026-05-04 08:48:06 -0400

    Resuelto ejercicio 6: Ganancias en criptomonedas

---
+++ desafio55/6.py
@@ -0,0 +1,8 @@
+BTC_Adquirido = 0.05 
+btc_1 = 50000
+btc_2 = 62000
+dolar = 900 
+diferencia = btc_2 - btc_1
+mis_ganancias = diferencia * BTC_Adquirido
+mis_ganancias_chilenas = mis_ganancias * 900
+print(f"Genere un total de ${mis_ganancias_chilenas} ganancias en BTC")
