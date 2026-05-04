d155900c0e092c2aa4983cb9cdfdf960ce1f4591
Zobeiba Henriquez <henriquezzobeiba@gmail.com>
2026-05-04 15:08:24 -0400

    Resuelto Ejercicio 12: Aprobación de Crédito (AND simple)

---
+++ Desafio 55/ej12.py
@@ -0,0 +1,16 @@
+#Ejercicio 12: Aprobación de Crédito (AND simple)
+#Revisa si un cliente cumple los requisitos del banco para pedir un crédito.
+
+#Datos Iniciales: El cliente tiene un sueldo de 550000 y tiene 1 deuda activa.
+
+#Reglas de Negocio: El banco exige DOS cosas a la vez: que el sueldo sea mayor a 500000 Y que la cantidad de deudas sea exactamente 0.
+
+#Restricciones: Usa la palabra reservada `and` dentro de un `if` para revisar ambas cosas en la misma línea. Imprime `'Aprobado'` o `'Rechazado'`.
+
+sueldo = 550000
+deuda_activa = 1
+
+if sueldo > 500000 and deuda_activa == 0:
+    print("APROBADO")
+else:
+    print("RECHAZADO")
