953b6e09e6abfa11bc29982999156261dfa07b3e
Alvas-cod3 <alvargonz507@gmail.com>
2026-05-04 17:12:47 -0400

    Resuelto Ejercicio 7: Armando el PC Gamer

---
+++ desafio55/7.py
@@ -0,0 +1,7 @@
+valor_procesador = 250000
+valor_motherboard = 120000
+valor_GPU = 450000
+porcentaje_GPU = 0.85
+valor_GPU = 450000 * porcentaje_GPU
+total_PC = valor_motherboard + valor_GPU + valor_procesador
+print(total_PC)
