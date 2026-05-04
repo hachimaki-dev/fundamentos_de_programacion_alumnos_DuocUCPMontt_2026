44fb50ebf5e98c4ddfb5aedbd87c4fcd3a6031a2
Gallegos-115 <hectorgallegos544@gmail..com>
2026-05-04 14:00:51 -0400

    DESAFIOS

--- Desafío_5/Armando el PC Gamer.py
+++ Desafío_5/Armando el PC Gamer.py
@@ -1,12 +1,14 @@
 Valor_de_Procesador = 250000
 Valor_de_Placa_Madre = 120000
 Valor_de_tajerta_de_video = 450000
+precio_original = 450000
 Valor_Final = 0
 descuento= 0.15
 
-Valor_de_tajerta_de_video = (Valor_de_tajerta_de_video * descuento) 
+Valor_de_tajerta_de_video = Valor_de_tajerta_de_video * descuento
+precio_original = precio_original - Valor_de_tajerta_de_video
 
-
+Valor_Final = precio_original + Valor_de_Procesador + Valor_de_Placa_Madre
 
 print (Valor_Final)
 
---
+++ Desafío_5/Ganancia en Criptomonedas (Binance).py
@@ -0,0 +1,11 @@
+btc = 0.05 
+btc_1 = 50000 
+valor_del_nuevo_btc = 62000 
+total_de_btc = 0
+dolar = 900 
+
+total_de_btc = btc * valor_del_nuevo_btc
+ganancia = total_de_btc - (btc * btc_1)
+ganancia_en_dolar = ganancia * dolar
+print (ganancia_en_dolar)
+
