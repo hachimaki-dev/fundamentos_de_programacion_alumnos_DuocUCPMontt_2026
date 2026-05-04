79be9b3f7eb7b539ecf043dd54940c0e56521c55
nanyasg <nany.asg10@gmail.com>
2026-05-04 17:58:55 -0400

    ejercicio7

---
+++ 55 desafios/7.py
@@ -0,0 +1,8 @@
+procesador = 250000
+placa_madre = 120000
+tarjeta_video = 450000
+
+tarjeta_video = tarjeta_video * 0.85
+
+precio_final = procesador + placa_madre + tarjeta_video
+print(precio_final)
