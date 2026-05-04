cba6389bfdb6efc7359de30d2c176a2a1caaa380
bisente777 <vic.aponte@duocuc.cl>
2026-05-04 17:04:20 -0400

    Resuelto Ejercicio 8: Rendimiento de Servidor (AWS EC2)

---
+++ desafio55/8.py
@@ -0,0 +1,8 @@
+ram_servidor = 16
+sistema_operativo = 2.5
+programas_4 = 1.2*4
+megabyte = 1024
+usado = sistema_operativo + programas_4
+libre = ram_servidor - usado 
+final = libre*megabyte
+print(final)
