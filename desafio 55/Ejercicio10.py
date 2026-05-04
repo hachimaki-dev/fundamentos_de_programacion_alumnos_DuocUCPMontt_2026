6fc51d830018e8e75541440905170a72a98cfea2
4l3xZzz <alexandervillalon54@gmail.com>
2026-05-04 13:42:15 -0400

    Resuelto Ejercicio 10: Estacionamiento Aeropuerto

---
+++ desafio 55/Ejercicio10.py
@@ -0,0 +1,6 @@
+hora_1 = 1200
+media_hora = 500
+
+print(f"En este estacionamiento la primera hora cuesta ${hora_1}CLP, y despues la media hora cuesta ${media_hora}CLP")
+print(f"En total te queda 3 horas")
+print(f"El total a pagar seria de ${hora_1 + media_hora * 4}CLP")
