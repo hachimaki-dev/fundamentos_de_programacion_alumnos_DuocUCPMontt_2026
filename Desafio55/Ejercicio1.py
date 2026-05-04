f893736b7345a70a7402d6a5e72c6554eeda3ca7
AlanRuizg6589 <ala.ruizg@duocuc.cl>
2026-05-04 16:42:19 -0400

    Resuelto Ejercicio 1: Liquidación de Sueldo Básico

---
+++ Desafio55/Ejercicio1.py
@@ -0,0 +1,27 @@
+"""Ejercicio 1: Liquidación de Sueldo Básico
+Eres el contador de la empresa y debes calcular el sueldo de un empleado.
+
+Datos Iniciales: El sueldo base es 500000. El bono de colación es 50000 y el de movilización es 30000.
+
+Reglas de Negocio: Al empleado se le descuenta un 7% por salud y un 10% por AFP. Ojo: estos descuentos se calculan SÓLO sobre el sueldo base (los bonos no sufren descuentos). 
+
+El sueldo líquido es el sueldo base más los bonos, menos los descuentos.
+
+Restricciones: Asigna todos los valores directamente en el código (no uses inputs). Imprime sólo el número final del sueldo líquido.
+
+Resultado esperado en consola:"""
+
+
+Sueldo_Base = 500000
+
+Bono_Colacion = 50000
+
+Movilizacion = 30000
+
+
+Sueldo_Base_Descuentos = Sueldo_Base * 0.83
+
+Sueldo_Liquido = Sueldo_Base_Descuentos + Bono_Colacion + Movilizacion
+
+
+print(Sueldo_Liquido)
