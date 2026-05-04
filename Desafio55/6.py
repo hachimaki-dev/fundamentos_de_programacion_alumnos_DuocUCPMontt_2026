875cbfc544f3826266f87c34295f92680fff897e
AphidCiro <navarrogamboajulian@gmail.com>
2026-05-04 09:34:54 -0400

    Resuelto Ejercicio 6: Ganancia en Criptomonedas (Binance)

---
+++ Desafio55/6.py
@@ -0,0 +1,12 @@
+#Quieres saber cuánta plata ganaste invirtiendo en Bitcoin.
+#Datos Iniciales: Compraste 0.05 BTC. Cuando compraste, 1 BTC valía 50000 dólares. Hoy, 1 BTC vale 62000 dólares. El dólar está a 900 pesos chilenos.
+#Reglas de Negocio: Calcula la diferencia entre el precio actual y el antiguo para saber cuánto ganaste por cada BTC. Multiplica eso por la cantidad de BTC que tienes. Ese será tu total ganado en dólares. Finalmente, conviértelo a pesos chilenos.
+#Restricciones: Haz los cálculos paso a paso usando variables. Imprime sólo la ganancia total en pesos.
+bitcoin = 0.05
+precio_viejo = 50000
+precio_nuevo = 62000
+precio_dolar = 900
+diferencia = precio_nuevo - precio_viejo
+ganancia = diferencia * bitcoin
+ganancia_pesos = ganancia * precio_dolar
+print(f"{ganancia_pesos}$ pesos")
