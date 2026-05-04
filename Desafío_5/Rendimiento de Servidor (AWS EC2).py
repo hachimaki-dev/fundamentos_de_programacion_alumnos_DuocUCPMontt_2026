5ecb7966613c95ccd6d73b755d5fc2f858267fad
Gallegos-115 <hectorgallegos544@gmail..com>
2026-05-04 14:09:19 -0400

    DESAFIOS_8

---
+++ Desafío_5/Rendimiento de Servidor (AWS EC2).py
@@ -0,0 +1,10 @@
+Ram = 16
+Sistema_Operativo = 2.5
+Programas_Abiertos = 1.2
+total_de_espacio = 0
+
+total_de_espacio = Ram - (Sistema_Operativo + (Programas_Abiertos * 4)) 
+
+total_de_espacio = total_de_espacio * 1024
+
+print (total_de_espacio)
