e75672ac41d4ac208a9143ee80064020d11a0b09
KAT-000 <sebastianignaciocorderospier@gmail.com>
2026-05-04 17:08:04 -0400

    Ejercicio2_Seva

---
+++ Desafio_55/2.py
@@ -0,0 +1,20 @@
+Precio_auto = 5000000
+Ahorros = 1500000
+ahorro_Mensual = 150000
+
+print(f"El auto sale: ${Precio_auto}")
+print(f"Tu tienes: ${Ahorros}")
+
+dinero_faltante = Precio_auto - Ahorros
+
+print(f"Faltan: ${dinero_faltante}")
+
+print(f"Para obtener el dinero deberas ahorrar todos los meses")
+
+Faltante = Precio_auto - Ahorros 
+
+print(f"Faltan: ${Faltante}")
+
+Tiempo = Faltante // ahorro_Mensual
+
+print(f"Necesitas ahorrar durante {Tiempo} meses para poder comprar el auto")
