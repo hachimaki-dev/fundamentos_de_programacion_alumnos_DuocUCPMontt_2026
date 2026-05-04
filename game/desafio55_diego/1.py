3099a65eb5fb37efd402369a1ff947bc4119b09b
Nacho27 <ignacionacho2000x@gmail.com>
2026-05-04 16:34:11 -0400

    resultado ejercicio 1

---
+++ game/desafio55_diego/1.py
@@ -0,0 +1,10 @@
+sueldo_base = 500000
+bono_colacion = 50000
+bono_moviliz = 30000
+
+descuento_salud = sueldo_base * 0.07
+descuento_afp = sueldo_base * 0.10
+
+sueldo_liquido = sueldo_base + bono_colacion + bono_moviliz - (descuento_salud + descuento_afp)
+
+print(int(sueldo_liquido))
