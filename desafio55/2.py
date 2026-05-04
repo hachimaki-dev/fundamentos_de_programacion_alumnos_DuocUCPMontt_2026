9fb01109ae77914cfd6ac9f4ed811f60af37fc44
nicoleasd <nicaguilar2006@gmail.com>
2026-05-04 09:19:28 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ desafio55/2.py
@@ -0,0 +1,24 @@
+precio_auto = 5000000
+
+ahorrado = 1500000
+
+ahorro_por_mnes = 150000
+
+
+faltante_auto = precio_auto - ahorrado
+
+
+
+
+
+
+
+contador_meses = faltante_auto // ahorro_por_mnes
+
+
+
+
+print(contador_meses)
+
+
+
