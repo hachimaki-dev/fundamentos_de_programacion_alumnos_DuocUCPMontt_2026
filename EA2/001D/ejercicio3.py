24f13c33837537324b484e65c71ea6aa8234cc0c
Ptrcim <pmiramore@gmail.com>
2026-05-04 10:09:22 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ EA2/001D/ejercicio3.py
@@ -0,0 +1,7 @@
+cantidad_de_personas=5
+valor_pichanga =15000
+valor_shop=3500
+valor_cuenta= valor_pichanga*2 + valor_shop*5
+valor_cuenta= valor_cuenta + valor_cuenta*(10/100)
+valor_de_cada_uno = valor_cuenta/5
+print(valor_de_cada_uno)
