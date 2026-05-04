460c7980a33f25aa2acba0e30bb8081bfa9d56de
Maxiidxss <levmaximiliano@gmail.comm>
2026-05-04 17:49:33 -0400

    Resuelto Ejercicio 6: Ganancia en Criptomonedas (Binance)

---
+++ ejercicio 6-55.py
@@ -0,0 +1,11 @@
+#Compraste 0.05 BTC
+# 1 BTC valía 50000 dólares
+#Hoy, 1 BTC vale 62000 dólares
+# El dólar está a 900 pesos chilenos.
+
+btc_comprado = 0.05
+precio_anterior = 50000
+precio_actual = 62000
+dolar = 900
+
+print((precio_actual - precio_anterior) * btc_comprado * dolar)
