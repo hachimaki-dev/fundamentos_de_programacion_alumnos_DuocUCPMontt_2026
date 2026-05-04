ec2eb40e41221f96f3b5c699f52713c52f81e127
diego0954-alt <diego0954@icloud.com>
2026-05-04 10:36:40 -0400

    Proyección de Ahorro para un Auto

--- desafio 55/2.py
+++ desafio 55/2.py
@@ -1,3 +1,8 @@
 auto = 5000000
 ahorrado = 1500000
 ahorrar_cada_mes = 150000
+
+total_ahorrar = auto - ahorrado
+total_cuotas = total_ahorrar // ahorrar_cada_mes
+
+print(f"el total de cuotas es de:{total_cuotas}")
