7eb7746f219d633522cb35f39c79850bb6716a62
axel-trujillo <axeltrujillo1110@gmail.com>
2026-05-04 09:29:03 -0400

    Resuelto ejercicio 8: Rendimiento servidor(AWS EC2)

---
+++ desafio55/8.py
@@ -0,0 +1,6 @@
+gb_servidor = 16
+sistema_operativo = 2.5
+programas = 4 * 1.2
+gasto_total = sistema_operativo + programas
+gb_utilizadas = gb_servidor - gasto_total
+print(f"Los MB utilizados son {gb_utilizadas * 1024} MB")
