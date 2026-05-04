3dc919dbe67b50e283b61bd65ff5ffcb9d17977d
Gtvq32 <gtvq1718@gmail.com>
2026-05-04 16:38:37 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

--- desafio55_Gustavo/3.py
+++ desafio55_Gustavo/3.py
@@ -5,6 +5,6 @@ cuenta_sin_propina = pichanga + schops
 
 cuenta_con_propina = cuenta_sin_propina * 1.1
 
-cuenta_por_persona = cuenta_con_propina / 5
+cuenta_por_persona = cuenta_con_propina // 5
 
 print(cuenta_por_persona)
