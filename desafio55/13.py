de65ade6c72aacd67b9b898c8940452bb8a79028
Happy210906 <olaquierpro@gmail.com>
2026-05-04 17:48:27 -0400

    Decimo Tecero vamooooooo

---
+++ desafio55/13.py
@@ -0,0 +1,7 @@
+ganancia_cliente = 800000
+años_banco_cliente = 6
+deudas_cliente = 0
+if ganancia_cliente > 1000000 or años_banco_cliente > 5 and deudas_cliente == 0:
+    print("Sos Cliente VIP.")
+else:
+    print("Sos un pobre normalucho.")
