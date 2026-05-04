08b13f49a735b3f40ffe7cc515ebf1bbd622bcaa
LeCoward <sebabarri2710@gmail.com>
2026-05-04 13:07:52 -0400

    Ej5

---
+++ desafio55/Ej5.py
@@ -0,0 +1,16 @@
+#🟢 Rango: Inofensivo
+#Ejercicio 5: Administrador de Carga Bip! Avanzado
+#Vas a hacer un seguimiento del saldo de tu tarjeta Bip! durante el día.
+
+#Datos Iniciales: Tu tarjeta empieza con 10000. El pasaje de micro cuesta 730 y el de metro en punta cuesta 850. Además, harás una recarga de 3000.
+
+#Reglas de Negocio: En el día tomas 1 micro y 2 metros. Después de esos viajes, recargas tu tarjeta.
+
+#Restricciones: Usa una sola variable para el saldo y ve restándole o sumándole el dinero paso a paso. Imprime tu saldo al final del día.
+
+saldo = 10000
+pasaje_micro = 730
+pasaje_metro = 850
+recarga_bip = 3000
+
+print("Si cumples con todos estos pasos previamente mencionados el saldo de tu tarjeta al final del día sería: $", saldo - pasaje_micro - pasaje_metro * 2 + recarga_bip ," pesos.")
