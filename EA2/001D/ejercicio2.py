df3fbe1df5a3e9b5b42117d26e4fd5d38dc46528
Ptrcim <pmiramore@gmail.com>
2026-05-04 09:47:24 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro

---
+++ EA2/001D/ejercicio2.py
@@ -0,0 +1,6 @@
+valor_auto= 5000000
+cantidad_ahorrada = 1500000
+ahorro_mensual= 150000
+cantidad_a_ahorrar = valor_auto - cantidad_ahorrada
+meses_a_ahorrar = cantidad_a_ahorrar//ahorro_mensual
+print(meses_a_ahorrar)
