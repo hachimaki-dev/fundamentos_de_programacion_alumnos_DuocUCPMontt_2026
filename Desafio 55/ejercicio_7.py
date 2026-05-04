831dcbd38623cf8fee70a5aece1e97411779fcbf
Her-cloud <carcamoh78@gmail.com>
2026-05-04 10:29:54 -0400

    Resuelto Ejercicio 7: Armando el PC Gamer

---
+++ Desafio 55/ejercicio_7.py
@@ -0,0 +1,8 @@
+# Armando el PC Gamer
+
+procesador = 250000
+placa_madre = 120000
+tarjeta_de_video = 450000
+descuento = tarjeta_de_video * 0.85 
+suma_del_costo = procesador + placa_madre + descuento
+print('Total a comprar es: ', suma_del_costo)
