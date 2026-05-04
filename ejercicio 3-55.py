67c4914b565ac32477fa5658980e09c92206d4a5
Maxiidxss <levmaximiliano@gmail.comm>
2026-05-04 17:18:48 -0400

    resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ ejercicio 3-55.py
@@ -0,0 +1,9 @@
+#dividir la cuenta por igual.
+#5 personas en total
+#2 pichangas a 15000 cada una
+# 5 schops (a 3500 cada uno).
+
+pichanga = 15000
+schops = 3500
+
+print((pichanga * 2 + schops* 5) *1.1 //  5)
