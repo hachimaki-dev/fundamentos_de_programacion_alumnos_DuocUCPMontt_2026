310c7cd94b7cb8b49ae29fb2c0e51f0ebd2cbdb0
flame6672 <gustavoa3498@gmail.com>
2026-05-04 08:42:13 -0400

    55 desafios

---
+++ 55_ejercicios/calculadora_cuotas.py
@@ -0,0 +1,4 @@
+precio_laptop = 1200000
+cuotas = 12
+envio = 15000
+print(f"El precio a pagar el primer mes será de: {(precio_laptop / cuotas) + envio} \nLos proximos meses será de: {(precio_laptop / cuotas)}")
