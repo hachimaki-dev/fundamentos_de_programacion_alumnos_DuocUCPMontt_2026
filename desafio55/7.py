af1fcc86faddd3e337b3eef99de6eb8070596b8e
axel-trujillo <axeltrujillo1110@gmail.com>
2026-05-04 08:59:54 -0400

    Resuelto ejercicio 7: Armando el PC gamer

---
+++ desafio55/7.py
@@ -0,0 +1,6 @@
+Procesador = 250000
+placa_madre = 120000
+tarjeta_video = 450000 
+descuento = tarjeta_video * 0.15
+tarjeta_video_descontada = tarjeta_video - descuento
+print(f"{ Procesador + placa_madre + tarjeta_video_descontada}")
