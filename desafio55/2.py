a6375c1bf69ad45e06adb60ccdef0a75899cb891
Impox0 <benjaminmaldonadojeldes@gmail.com>
2026-05-04 19:50:18 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ desafio55/2.py
@@ -0,0 +1,14 @@
+auto = 5000000
+ahorrado = 1500000
+vas_ahorrar = 150000
+
+dinero_faltante = auto - ahorrado
+
+print(f"Te falta {dinero_faltante} para comprar el auto")
+
+meses_compra_auto = dinero_faltante // vas_ahorrar
+
+print(f"Te faltan {meses_compra_auto} meses")
+
+
+
