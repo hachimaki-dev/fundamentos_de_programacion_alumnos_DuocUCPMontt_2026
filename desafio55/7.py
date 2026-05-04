7de8329918d9caca0c458a29a01d04958ee716ea
moenita <davidmoena2@gmail.com>
2026-05-04 13:19:16 -0400

    7Mo desafio resuelto!

---
+++ desafio55/7.py
@@ -0,0 +1,8 @@
+precio_procesador= 250000
+precio_placamadre= 120000
+
+descuento_tarjetavideo= 15
+precio_tarjetavideo= 450000 - (450000 * 15 /100)
+
+precio_final= precio_procesador + precio_placamadre + precio_tarjetavideo
+print(f"Tu total es de {precio_final}")
