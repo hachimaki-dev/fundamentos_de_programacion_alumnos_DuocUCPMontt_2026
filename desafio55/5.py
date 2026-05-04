ef939eca3a3dc60bc3f9fa8475cf78165ff50dfe
Happy210906 <olaquierpro@gmail.com>
2026-05-04 16:59:44 -0400

    Quinto vamooooooooo

---
+++ desafio55/5.py
@@ -0,0 +1,6 @@
+dinero_inicial = 10000
+pasaje_micro = 730
+pasaje_metro = 850
+recarga = 3000
+saldo = dinero_inicial - pasaje_micro * 1 - pasaje_metro * 2 + recarga
+print(f"Tu Bip! le quedan {saldo} pesos de saldo.")
