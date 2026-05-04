9b2f6cb890e847d14a81516d8f538f8cb09b04e0
betobullsio19 <pueloalto1@gmail.com>
2026-05-04 13:47:06 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

--- desafio55/2.py
+++ desafio55/2.py
@@ -1,7 +1,6 @@
 costo_del_auto = 5000000
 ahorrado = 1500000
 ahorro_x_mes = 150000
-
 dinero_faltante = costo_del_auto - ahorrado
 
 print(f"el total de meses{dinero_faltante // ahorro_x_mes}")
---
+++ desafio55/3.py
@@ -0,0 +1,12 @@
+personas = 5
+pichangas = 2* 15000
+schops =5* 3500
+
+total_consumo = pichangas + schops
+
+propinas = total_consumo *0.10
+
+total_consumo = total_consumo + propinas
+
+personas = total_consumo / personas
+print(f"total a pagar por personas es {personas}")
