5fdbd12828ded9dfd6c27448db55cc50b5b59590
Amaro-07 <amaro.loos2007@gmail.com>
2026-05-04 14:00:27 -0400

    Amaro-Respuesta EJ8: Rendimiento de Servidor

---
+++ amaro_ej_8.py
@@ -0,0 +1,8 @@
+servidor = 16
+sistema_operativo = 2.5
+programas = 1.2 * 4
+
+gastos = servidor - sistema_operativo - programas
+resultado_mb = gastos * 1024
+
+print(f"GB restantes: {resultado_mb}")
