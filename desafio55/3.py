41b689dbe642ed0b22117e0f2fc405756a9838a8
Shiekd-L <ordonezbenja22@gmail.com>
2026-05-04 16:45:22 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ desafio55/3.py
@@ -0,0 +1,8 @@
+personas = 5
+pichangas = 15000
+schops = 3500
+consumo = pichangas * 2 + schops * 5
+propina =100/110 
+monto_final = consumo/propina
+paga_por_persona = monto_final/personas
+print(paga_por_persona)
