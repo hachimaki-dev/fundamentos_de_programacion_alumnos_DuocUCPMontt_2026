f545213e62b6739193ea4e8ee461ef626fa01fba
benjaminb80 <benjaminbusta45@gmail.com>
2026-05-04 21:15:03 +0000

    Resuelto Ejercicio 4: Calculadora de Cuotas (MercadoLibre)

---
+++ desafio 55/4.py
@@ -0,0 +1,9 @@
+precio_notebook = 1200000
+cuotas = 12
+envio_a_casa = 15000
+
+valor_cuotas = precio_notebook / cuotas
+total_primer_mes = valor_cuotas + envio_a_casa
+
+print("el valor de cada cuota es:", valor_cuotas)
+print("el total al pagar al mes es:", total_primer_mes)
