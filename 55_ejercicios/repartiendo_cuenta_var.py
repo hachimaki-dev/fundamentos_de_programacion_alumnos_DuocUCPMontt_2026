a84d3b149ddc27f4d6b328e156bfeb8fe2e1c8b3
flame6672 <gustavoa3498@gmail.com>
2026-05-04 08:33:34 -0400

    55 desafios

--- 55_ejercicios/repartiendo_cuenta_var.py
+++ 55_ejercicios/repartiendo_cuenta_var.py
@@ -1,8 +1,8 @@
 amigos = 5
 pichangas = 15000
 schops = 3500
-print(f"Fuiste a comer con tus {amigos} amigos \nEl total de lo que comieron fue de: {pichangas * 2 + schops * 5}")
 total = pichangas * 2 + schops * 5
+print(f"Fuiste a comer con tus {amigos} amigos \nEl total de lo que comieron fue de: {total}")
 total = total * 1.10
 total_repartido = total // amigos
 print(f"Cada uno debería pagar: ${total_repartido}")
