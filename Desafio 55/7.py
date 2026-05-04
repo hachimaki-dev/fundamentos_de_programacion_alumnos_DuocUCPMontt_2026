0389fe0dd86723c07a8d5bb3479da1f7a59058dc
CamiloDiaz2 <cf.diazs@duocuc.cl>
2026-05-04 15:49:14 -0400

    Resuelto Ejercicio 7: Armando el PC Gamer

---
+++ Desafio 55/7.py
@@ -0,0 +1,7 @@
+costo_procesador = 250000
+costo_placa_madre = 120000
+costo_tarjeta_de_video = 450000
+
+costo_total = costo_procesador + costo_placa_madre + (costo_tarjeta_de_video * 0.85)
+
+print(costo_total)
