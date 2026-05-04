0fea2dfd0c80b5c94cc16a6e88a1cbf91281ff5c
bisente777 <vic.aponte@duocuc.cl>
2026-05-04 16:59:38 -0400

    Resuelto Ejercicio 7: Armando el PC Gamer

---
+++ desafio55/7.py
@@ -0,0 +1,7 @@
+procesador = 250000
+placa_madre = 120000
+targeta_grafica = 450000
+dsct_targeta_grafica = targeta_grafica/100*15
+final_targeta = targeta_grafica - dsct_targeta_grafica
+total_compra = procesador + placa_madre + final_targeta
+print(total_compra)
