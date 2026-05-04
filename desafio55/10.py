f78da80f562c2c2c84dbb5a4c3bb7678b1ea63a2
figueroawa <selena.ale.figueroa@gmail.com>
2026-05-04 12:59:46 -0400

    Resuelto Ejercicio 10: Estacionamiento Aeropuerto

---
+++ desafio55/10.py
@@ -0,0 +1,5 @@
+precio_estacionamiento_primera_hora = 1200
+precio_estacionamiento_media_hora_extra = 500
+
+precio_total = precio_estacionamiento_primera_hora + (precio_estacionamiento_media_hora_extra * 4) # 3 horas en total
+print(precio_total)
