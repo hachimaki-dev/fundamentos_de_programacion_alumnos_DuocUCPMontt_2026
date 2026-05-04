adae37043069e55b0eb954b296e6f6a4bdb3ef28
bisente777 <vic.aponte@duocuc.cl>
2026-05-04 17:07:38 -0400

    Resuelto Ejercicio 9: Costos de Producción (Emprendimiento)

---
+++ desafio55/9.py
@@ -0,0 +1,8 @@
+valor_polera = 15000
+costo_tela = 4000
+costo_Espampado = 2500
+costo_empaque = 500
+venta = 50
+ganancia_polera = valor_polera - costo_tela - costo_empaque - costo_Espampado
+ganancia_total = ganancia_polera*venta
+print(ganancia_total)
