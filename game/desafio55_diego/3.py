10a716e6f0569a00b51d3f93994e75654320414b
Nacho27 <ignacionacho2000x@gmail.com>
2026-05-04 16:45:55 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ game/desafio55_diego/3.py
@@ -0,0 +1,15 @@
+precio_pichanga = 15000
+cantidad_pichangas = 2
+
+precio_schop = 3500
+cantidad_schops = 5
+
+personas = 5
+
+consumo = (precio_pichanga * cantidad_pichangas) + (precio_schop * cantidad_schops)
+
+propina = consumo * 0.10
+monto_final = consumo + propina
+pago_por_persona = monto_final / personas
+
+print("Cada persona debe pagar:", pago_por_persona)
