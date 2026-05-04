cbe66ec5df11434a24a187c5b28cc5642f575ed3
AphidCiro <navarrogamboajulian@gmail.com>
2026-05-04 09:25:09 -0400

    Resuelto Ejercicio 5: Administrador de Carga Bip! Avanzado

---
+++ Desafio55/6.py
@@ -0,0 +1,13 @@
+#Vas a hacer un seguimiento del saldo de tu tarjeta Bip! durante el día.
+#Datos Iniciales: Tu tarjeta empieza con 10000. El pasaje de micro cuesta 730 y el de metro en punta cuesta 850. Además, harás una recarga de 3000.
+#Reglas de Negocio: En el día tomas 1 micro y 2 metros. Después de esos viajes, recargas tu tarjeta.
+#Restricciones: Usa una sola variable para el saldo y ve restándole o sumándole el dinero paso a paso. Imprime tu saldo al final del día.
+saldo_tarjeta = 10000
+costo_pasaje_micro = 730
+costo_pasaje_metro = 850
+recarga = 3000
+saldo_tarjeta -= costo_pasaje_micro
+saldo_tarjeta -= costo_pasaje_metro
+saldo_tarjeta -= costo_pasaje_metro
+saldo_tarjeta += recarga
+print(saldo_tarjeta)
