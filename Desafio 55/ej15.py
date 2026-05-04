7bb7cbb724e0854febaca4daebac8eaa2cfc9f0e
Zobeiba Henriquez <henriquezzobeiba@gmail.com>
2026-05-04 16:09:38 -0400

    Resuelto Ejercicio 15: Envíos MercadoLibre (Zonas Extremas)

---
+++ Desafio 55/ej15.py
@@ -0,0 +1,23 @@
+#Ejercicio 15: Envíos MercadoLibre (Zonas Extremas)
+#Calcula cuánto debe pagar un cliente por el envío de su compra.
+
+#Datos Iniciales: El cliente compró 25000 en productos y vive en la región de 'Magallanes'.
+
+#Reglas de Negocio: Si la compra es mayor a 20000, el envío normal es gratis (0). Si es menor, cuesta 3000. Pero OJO: si el destino es 'Magallanes' o 'Aysen', siempre se suman 2000 extra al envío por ser zona extrema (incluso si era gratis).
+
+#Restricciones: Haz un `if/else` para el costo base, y luego un `if` SEPARADO (no anidado) para sumar el recargo de zona extrema. Imprime solo el costo total del envío.
+
+compra = 25000
+region = 'Magallanes'
+envio = ''
+
+if compra >= 20000:
+    envio = 0
+else: 
+    envio = 3000
+    
+if region == 'Magallanes' or region == 'Aysen':
+    envio += 2000
+    print(f"Tu coste de envío es: ${envio}")
+else:
+    print(f'Tu coste de envío es: ${envio}')
