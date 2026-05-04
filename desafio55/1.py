4e9f6e58cad213a0873fd09cebbf7bc4f6b1bc68
nicoleasd <nicaguilar2006@gmail.com>
2026-05-04 09:01:25 -0400

    Resuelto Ejercicio 1: Liquidación de Sueldo Básico

---
+++ desafio55/1.py
@@ -0,0 +1,18 @@
+sueldo_base = 500000 
+
+bono_colacion = 50000
+
+bono_movilizacon = 30000
+
+sueldo_liquido = 0
+
+sueldo_base = sueldo_base * 0.93
+
+sueldo_base = sueldo_base * 0.90
+
+sueldo_liquido = sueldo_base + bono_colacion + bono_movilizacon
+
+print(sueldo_liquido)
+
+
+
