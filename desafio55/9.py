789b4324dccc5b9f81e3e5b1baa0afbf8106d735
darkdevbat <batmanjohan12@gmail.com>
2026-05-04 17:49:34 -0400

    Resuelto Ejercicio 9: Costos de Producción (Emprendimiento)

---
+++ desafio55/9.py
@@ -0,0 +1,10 @@
+polera = 15000
+tela = 4000
+estampado = 2500
+empaque = 500
+ventas = 50
+
+ganancia_x_polera = polera - (tela + estampado + empaque)
+ganancia_mensual = ganancia_x_polera * 50
+
+print(ganancia_mensual)
