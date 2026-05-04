a2afb9cdf9623e0404e243960086a497ea8cbcd3
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-04 19:59:27 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ desafio55/2.py
@@ -0,0 +1,25 @@
+#Ejercicio 2: Proyección de Ahorro para un Auto
+#Quieres calcular cuántos meses te tomará ahorrar para comprarte un auto.
+
+#Datos Iniciales: El auto cuesta 5000000. Tú ya tienes ahorrado 1500000. Vas a ahorrar 150000 cada mes.
+
+#Reglas de Negocio: Primero descubre cuánto dinero te falta. Luego, averigua cuántos meses completos te tomará juntar ese dinero faltante con tu ahorro mensual.
+
+#Restricciones: Usa división entera (`//`) para asegurarte de que el resultado sea un número de meses exactos, sin decimales. Imprime solo el número de meses.
+
+#Resultado esperado en consola:
+#23
+
+
+
+precio_auto = 5000000
+monto_ahorrado = 1500000
+ahorro_mes = 150000
+
+
+cuanto_dinero_falta = precio_auto - monto_ahorrado
+
+cuantos_meses = cuanto_dinero_falta // ahorro_mes
+
+print(cuantos_meses)
+
