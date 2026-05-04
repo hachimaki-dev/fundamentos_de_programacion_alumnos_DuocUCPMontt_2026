60f63d9fd242b319d159febea2bd8cab7c03258c
benjamin476 <alexander.10benjamin@gmail.com>
2026-05-04 10:32:33 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ ejerciciosdesafio55/ej2.py
@@ -0,0 +1,6 @@
+costo_auto = 5000000
+ahorrado = 1500000
+ahorro_mensual = 150000
+total_a_ahorrar = costo_auto - ahorrado
+total_cuotas = total_a_ahorrar // ahorro_mensual
+print(f"total de cuotas a pagares: {total_cuotas}")
