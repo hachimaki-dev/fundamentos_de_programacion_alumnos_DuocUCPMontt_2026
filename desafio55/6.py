272dd991db271b888ddf20dd10831d8c63ee91a6
maximobv <barrigamaximo845@gmail.com>
2026-05-04 17:07:57 -0400

    Resuelto Ejercicio 6: Ganancia en Criptomonedas (Binance)

---
+++ desafio55/6.py
@@ -0,0 +1,10 @@
+
+BTC = 50000
+saldo_BTC = BTC * (0.05/50000)
+BTC_actual = 62000
+dolar = 900
+diferencia = BTC_actual - BTC
+diferencia = diferencia * saldo_BTC
+saldo_es_CLP = diferencia * dolar
+print(saldo_es_CLP)
+
