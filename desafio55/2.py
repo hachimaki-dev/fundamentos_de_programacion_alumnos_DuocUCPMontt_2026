527a6396623ef84ed7a85910afe499da640b8842
moenita <davidmoena2@gmail.com>
2026-05-04 12:40:44 -0400

    2do desafio resuelto!

---
+++ desafio55/2.py
@@ -0,0 +1,7 @@
+auto= 5000000
+dinero_ahorrado= 1500000
+dinero_faltante= auto - dinero_ahorrado
+print(f"Te falta {dinero_faltante} para comprar tu auto")
+
+meses_faltante= dinero_faltante // 150000
+print(f"Te faltan {meses_faltante} para conseguir el dinero")
