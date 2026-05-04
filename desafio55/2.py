30b0bc23d41953be8aedd8de70e2196ca7b6eb40
AG26535 <alangodoy235@gmail.com>
2026-05-04 19:49:58 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ desafio55/2.py
@@ -0,0 +1,7 @@
+precio_auto = 5000000
+ahorro_inicial = 1500000
+ahorro_mensual = 150000
+dinero_faltante = (precio_auto - ahorro_inicial)
+print ("dinero faltante: ",dinero_faltante)
+meses_faltantes = (precio_auto - ahorro_inicial) // ahorro_mensual
+print ("meses faltantes: ",meses_faltantes)
