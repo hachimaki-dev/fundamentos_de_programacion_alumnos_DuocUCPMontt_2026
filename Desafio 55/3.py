768b9b05ccdf5af3de20fdc83c50d50de6c787fe
jjeanfranco <jjeanfrancom@gmail.com>
2026-05-04 19:40:30 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ Desafio 55/3.py
@@ -0,0 +1,12 @@
+persona_totales=5
+costo_pichanga=15000
+costo_schops=3500
+
+consumo_total=(2*costo_pichanga)+(5*costo_schops)
+
+monto_final=(consumo_total*10/100)+consumo_total
+
+costo_por_persona=monto_final/persona_totales
+
+print(costo_por_persona)
+
