d8ff860d0be862452ac0df9d203e249956b659aa
maximobv <barrigamaximo845@gmail.com>
2026-05-04 16:50:20 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ desafio55/3.py
@@ -0,0 +1,9 @@
+shops = 3500
+personas = 5
+pichanga = 15000
+gasto_total = pichanga * 2 + shops * personas
+propina = gasto_total * (10/100)
+gasto_final = gasto_total
+gasto_final = gasto_final + propina
+gasto_final = gasto_final * (1/5)
+print(gasto_final)
