2942c55d58cb9ec721711ba90bbd8386797331e9
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-04 13:48:28 -0400

    Ejericio 3: División en el Bar

---
+++ 55ejercicios/ej3.py
@@ -0,0 +1,17 @@
+# Ejercicio 3: División de Bar 🍹🍷🍾🍸
+
+print("----------------------------")
+print("División de cuenta en el Bar")
+print("----------------------------")
+
+persona = 5
+
+precio_pichanga = 15000 * 2
+precio_schops = 3500 * 5
+
+consumo = precio_pichanga + precio_schops
+propina = consumo * 0.1
+
+monto_final = (consumo + propina) / 5
+
+print(monto_final)
