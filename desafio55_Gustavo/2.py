a5162a99e2bcac9d123d6746abf3d36f856f855d
Gtvq32 <gtvq1718@gmail.com>
2026-05-04 16:37:41 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ desafio55_Gustavo/2.py
@@ -0,0 +1,9 @@
+auto = 5000000 
+ahorro_actual = 1500000
+ahorro_por_mes = 150000
+
+cuanto_falta = auto - ahorro_actual
+
+meses_de_ahorro = cuanto_falta // ahorro_por_mes
+
+print(meses_de_ahorro)
---
+++ desafio55_Gustavo/3.py
@@ -0,0 +1,10 @@
+pichanga = 15000 * 2
+schops = 3500 * 5
+
+cuenta_sin_propina = pichanga + schops
+
+cuenta_con_propina = cuenta_sin_propina * 1.1
+
+cuenta_por_persona = cuenta_con_propina / 5
+
+print(cuenta_por_persona)
