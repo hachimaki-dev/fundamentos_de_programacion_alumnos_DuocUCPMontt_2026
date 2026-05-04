30e45bb84063c3bf8ff798dad7a44139d379b606
Gtvq32 <gtvq1718@gmail.com>
2026-05-04 16:55:12 -0400

    Resuelto Ejercicio 6: Ganancia en Criptomonedas (Binance)

---
+++ desafio55_Gustavo/6.py
@@ -0,0 +1,10 @@
+comprados = 0.05
+valor_antiguo = 50000
+valor_actual = 62000
+dolar = 900
+
+diferencia = valor_actual - valor_antiguo
+ganancia = diferencia * comprados
+transformacion = ganancia * dolar
+
+print(transformacion)
