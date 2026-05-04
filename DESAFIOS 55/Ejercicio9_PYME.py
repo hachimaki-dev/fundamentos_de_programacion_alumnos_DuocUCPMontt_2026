98825cb19f6096b953faaa7c83c2429fbb889b63
CheelaMS <golhabesoto@proton.me>
2026-05-04 09:45:03 -0400

    Resuelto Ejercicio 9: Costos de Producción (Emprendimiento)

---
+++ DESAFIOS 55/Ejercicio9_PYME.py
@@ -0,0 +1,10 @@
+valor_polera=15000
+valor_tela=4000
+valor_estampado=2500
+valor_empaque=500
+costopolera=valor_tela+valor_estampado+valor_empaque
+
+gananciapolera= valor_polera-costopolera
+
+gananciames=gananciapolera*50
+print(gananciames)
