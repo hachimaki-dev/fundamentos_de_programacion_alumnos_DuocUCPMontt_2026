66679c3c126032669b39dca0e37f48ad82436307
TomandoSoylent <therealdiegoxd@gmail.com>
2026-05-04 13:16:56 -0400

    Resuelto Ejercicio 6: Ganancia en Criptomonedas (Binance)

---
+++ desafio55/6.py
@@ -0,0 +1,19 @@
+bitcoins_comprados = 0.05
+pesos_chilenos = 900
+costo_del_bitcoin_actual = 62000
+costo_del_bitcoin_pasado = 50000 
+
+
+
+diferencia = (costo_del_bitcoin_actual - costo_del_bitcoin_pasado)
+
+diferencia = diferencia*bitcoins_comprados
+diferencia = diferencia*pesos_chilenos
+print(diferencia)
+
+
+
+
+
+
+
