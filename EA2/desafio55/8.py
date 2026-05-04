52b9dd4f2ddd60caee6798d50e4eac6e7cdf0432
andrewfarias452 <an.fariast@duocuc.cl>
2026-05-04 17:46:52 -0400

    Resuelto Ejercicio 8: Rendimiento de Servidor (AWS EC2)

---
+++ EA2/desafio55/8.py
@@ -0,0 +1,15 @@
+'''Ejercicio 8: Rendimiento de Servidor (AWS EC2)
+Estás revisando cuánta memoria RAM le queda libre a tu servidor.
+
+Datos Iniciales: El servidor tiene 16 GB en total. El sistema operativo siempre usa 2.5 GB. Tienes 4 programas corriendo y cada uno usa 1.2 GB. (Recuerda que 1 GB son 1024 MB).
+
+Reglas de Negocio: Suma lo que gasta el sistema y lo que gastan los programas. Réstale eso al total para saber cuántos GB quedan libres. Pero ojo, el resultado final debe estar en Megabytes (MB).
+
+Restricciones: Convierte la RAM restante multiplicándola por 1024 antes de imprimirla. Imprime solo el número.'''
+
+ram = 1024 * 16
+SO = 1024 * 2.5
+programas = (1024 * 1.2) * 4
+gasto_total = SO + programas
+ram_libre = ram - gasto_total
+print(ram_libre)
