b79a787b398c627c873ab2d4b0bb59072f7f10f0
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-04 19:29:00 -0400

    Resuelto Ejercicio 1: Liquidación de Sueldo Básico

---
+++ desafio55/1.py
@@ -0,0 +1,8 @@
+sueldo = 500000
+Bono_Comida = 50000
+Bono_Transporte = 30000
+fonasa = 35000
+afp = 50000
+
+sueldo_liquido = sueldo - (fonasa + afp) + Bono_Comida + Bono_Transporte
+print(sueldo_liquido)
