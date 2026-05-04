4cc61be1c28e93c38d482d34073cc5dbd36ff5a9
diego0954-alt <diego0954@icloud.com>
2026-05-04 18:27:47 -0400

    Repartiendo la Cuenta en el Bar

---
+++ desafio 55/3.py
@@ -0,0 +1,9 @@
+personas = 5
+pichangas = 2 * 15000
+schops = 5 * 3500
+
+total = pichangas + schops
+total_propina = total * 1.10
+pago_por_persona = total_propina / personas
+
+print(f"el monto por persona es de: {pago_por_persona}")
