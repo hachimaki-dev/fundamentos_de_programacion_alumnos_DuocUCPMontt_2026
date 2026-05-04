3ae7e3ac40dbfd1fa388236de9813e67453ab0a2
Zobeiba Henriquez <henriquezzobeiba@gmail.com>
2026-05-04 15:23:22 -0400

    Resuelto Ejercicio 13: Promoción VIP BancoEstado (OR y AND)

---
+++ Desafio 55/ej13.py
@@ -0,0 +1,17 @@
+#Ejercicio 13: Promoción VIP BancoEstado (OR y AND)
+#Revisa si un cliente puede subir a categoría VIP en su banco.
+
+#Datos Iniciales: El cliente gana 800000, lleva 6 años en el banco y tiene 0 deudas.
+
+#Reglas de Negocio: Eres VIP si cumples UNA de estas dos opciones: Opción A) Ganas más de 1000000. Opción B) Llevas 5 o más años en el banco Y tienes 0 deudas.
+
+#Restricciones: Combina `or` y `and` en un mismo `if`. Usa paréntesis para agrupar la Opción B y que Python no se confunda. Imprime `'Cliente VIP'` o `'Cliente Normal'`.
+
+sueldo_cliente = 800000
+antiguedad = 6 #años
+deudas = 0
+
+if (sueldo_cliente >= 1000000) or (antiguedad >= 5 and deudas == 0): #entre parentesis para separar condición A de la condición B
+    print('Cliente VIP')
+else:
+    print('Cliente normal')
