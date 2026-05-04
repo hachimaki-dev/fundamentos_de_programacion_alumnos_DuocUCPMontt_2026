6ec99d301c926c1d17fb54282cdc6af0351a883f
maximobv <barrigamaximo845@gmail.com>
2026-05-04 17:48:00 -0400

    Resuelto Ejercicio 13: Promoción VIP BancoEstado (OR y AND)

---
+++ desafio55/13.py
@@ -0,0 +1,7 @@
+sueldo_clienteb = 800000
+años_en_el_banco = 6
+deudad_cliente = 0
+if sueldo_clienteb >= 1000000 or (años_en_el_banco >= 5 and deudad_cliente == 0):
+    print("--Cliente VIP--")
+else:
+    print("-cliente normal-")
