bc302ad4ad93f4dee34554173de8f1545755bd2a
yvnnng28 <Benjaandres29sanchezzz@gmail.com>
2026-05-04 19:46:37 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ Desafio55/3.py
@@ -0,0 +1,6 @@
+personas = 5
+costos_comida = (2 * 15000) + (5 * 3500)
+propina = (costos_comida * 10) / 100
+costo_final = costos_comida + propina
+monto_cada_uno = costo_final / 5
+print(monto_cada_uno)
