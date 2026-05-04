56885c3f874949445ff61a650526f1d28d714367
axel-trujillo <axeltrujillo1110@gmail.com>
2026-05-04 08:33:19 -0400

    Resuelto ejercicio 5: Administrador de carga Bip! Avanzado

---
+++ desafio55/5.py
@@ -0,0 +1,7 @@
+saldo_tarjeta = 10000
+pasaje_micro = 730 
+pasaje_metro = 850 * 2
+recarga = 3000
+
+saldo_final = saldo_tarjeta - pasaje_metro - pasaje_micro + recarga
+print(f"El saldo final de tu tarjeta es de ${saldo_final}")
