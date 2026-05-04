56932750fdf2f4875d5a34be8e935ff62bd63771
fe-macias <fernando.macias19@icloud.comgit config --global user.name fernando.macias19@icloud.com>
2026-05-04 13:54:42 -0400

    ejercicio 6

---
+++ desafio55/criptomonedas.py
@@ -0,0 +1,16 @@
+#Quieres saber cuánta plata ganaste invirtiendo en Bitcoin.
+
+#Datos Iniciales: Compraste 0.05 BTC. Cuando compraste, 1 BTC valía 50000 dólares. Hoy, 1 BTC vale 62000 dólares. El dólar está a 900 pesos chilenos.
+
+#Reglas de Negocio: Calcula la diferencia entre el precio actual y el antiguo para saber cuánto ganaste por cada BTC. Multiplica eso por la cantidad de BTC que tienes. Ese será tu total ganado en dólares. Finalmente, conviértelo a pesos chilenos.
+
+#Restricciones: Haz los cálculos paso a paso usando variables. Imprime sólo la ganancia total en pesos.
+btc_05 = 25000
+btc_1_antiguo = 50000
+btc_1_actual = 62000
+dolar_clp = 900
+diferencia1 = (btc_05 - btc_1_antiguo) * 900
+diferencia2 = btc_05 - btc_1_actual * 900
+print(f"usted compro 0.05 btc , antiguamente valia {btc_1_antiguo} y actualmente vale {btc_1_actual} todo esto mientras el dolar vale {dolar_clp}"
+print(antiguamente usted gano {diferencia1} y ahora usted gana {diferencia2})
+ 
