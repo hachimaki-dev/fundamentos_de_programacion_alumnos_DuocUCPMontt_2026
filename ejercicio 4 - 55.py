c71ef3f577da300c95b69d55fcbc41ab2033b445
Maxiidxss <levmaximiliano@gmail.comm>
2026-05-04 17:27:19 -0400

    Resuelto Ejercicio 4: Calculadora de Cuotas (MercadoLibre)

---
+++ ejercicio 4 - 55.py
@@ -0,0 +1,9 @@
+#notebook cuesta 1.200.000
+#en 12 cuotas sin interes
+#el envio 15000 y se paga completo junto a la primera cuota
+
+notebook = 1200000
+envio = 15000
+
+print("Valor de la primera cuota")
+print(notebook / 12 + 15000)
