e167c5b219b4da7b039626f38d45d01034707133
bisente777 <vic.aponte@duocuc.cl>
2026-05-04 16:48:53 -0400

    Resuelto Ejercicio 4: Calculadora de Cuotas (MercadoLibre)

---
+++ desafio55/5.py
@@ -0,0 +1,7 @@
+inicio_targeta =  10000
+targeta_actual = 0
+pasaje_micro = 730
+metro = 850
+recarga = 3000
+targeta_actual = inicio_targeta + recarga - metro*2 - pasaje_micro
+print(targeta_actual)
