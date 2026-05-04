f2ac61fd54cbb9ce5f5c711ef670965cb24aca2e
maximobv <barrigamaximo845@gmail.com>
2026-05-04 16:43:48 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ desafio55/2.py
@@ -0,0 +1,12 @@
+
+auto = 5000000
+dinero_ahorra = 1500000
+dinero_mes = 150000
+dinero_faltante = auto - dinero_ahorra
+print("--el dinero faltante es--")
+print(dinero_faltante)
+meses_fal = dinero_faltante // dinero_mes
+print("--Los meses para alcanzar la meta de dinero son---")
+print(meses_fal)
+
+
