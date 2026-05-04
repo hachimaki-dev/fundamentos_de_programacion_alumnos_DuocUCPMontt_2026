4ac62591a804fe1d486de718c0cf96457d73ed5d
darkdevbat <batmanjohan12@gmail.com>
2026-05-04 16:55:03 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ desafio55/3.py
@@ -0,0 +1,9 @@
+persona_total = 5
+pichangas = 2 * 15000
+schops = 5 * 3500
+
+total_consumo = pichangas + schops
+total_propina = total_consumo * 1.1
+monto_final =total_propina // persona_total
+print(monto_final)
+
