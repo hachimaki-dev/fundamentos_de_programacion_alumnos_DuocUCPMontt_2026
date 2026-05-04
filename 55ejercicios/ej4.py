a6d2578234596d1d97029ef3bafb180ff1d36789
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-04 14:09:02 -0400

    Ejercicio 4: Calculadora de Cuotas (MercadoLibre)

---
+++ 55ejercicios/ej4.py
@@ -0,0 +1,14 @@
+# Ejercicio 4: Calculadora de Cuotas (MercadoLibre) 
+
+print("============================================================")
+print("Mercado Libre, la mejor App de compra y Envios de todo Chile")
+print("============================================================")
+
+precio_notebook = 1200000
+cuotas = 12
+envio = 15000
+
+valor_cuotas = precio_notebook / cuotas
+primer_pago = valor_cuotas + envio
+
+print("El valor es", primer_pago)
