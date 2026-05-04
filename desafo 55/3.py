438342b8bf5da3db7e6e34688e07716f80500edb
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-04 19:58:25 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

--- desafo 55/3.py
+++ desafo 55/3.py
@@ -1,7 +1,16 @@
 personas = 5 
 valor_pichanga = 15000
 schops = 3500
+propina = 10 
 
-total_pichanga = (valor_pichanga * 2) + (schops * 5)
-print(total_pichanga)
+valor_total = (valor_pichanga * 2) + (schops * 5)
+
+total_propina = (valor_total * propina) / 100
+
+total_total = valor_total + total_propina 
+
+total_personal = total_total / 5
+
+print(total_personal)
+
 
