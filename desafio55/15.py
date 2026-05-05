9e604db26db73ca8cc632c78cb9a9bf9d5a03496
Impox0 <benjaminmaldonadojeldes@gmail.com>
2026-05-04 21:56:04 -0400

    Resuelto Ejercicio 15: Envíos MercadoLibre (Zonas Extremas)

---
+++ desafio55/15.py
@@ -0,0 +1,15 @@
+compro = 25000 
+region = "Magallanes"
+zona_extrema = 2000
+envio = 0
+
+if compro > 20000:
+    envio == 0
+else:
+    compro < 20000
+    envio += 3000
+
+if region == "Magallanes" or "Aysen":
+    envio = envio + zona_extrema
+
+print(f"{envio}")
