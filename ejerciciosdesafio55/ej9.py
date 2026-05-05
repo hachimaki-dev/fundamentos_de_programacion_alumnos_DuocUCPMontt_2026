3848fd28f2a926a3a4bdfff0994ce84e1663c1f2
benjamin476 <alexander.10benjamin@gmail.com>
2026-05-04 21:41:32 -0400

    Resuelto Ejercicio 9: Costos de Producción (Emprendimiento)

---
+++ ejerciciosdesafio55/ej9.py
@@ -0,0 +1,12 @@
+polera = 15000
+tela = 4000
+estanpado = 2500
+empaque = 500
+
+#50 unidades a vender
+costo_polera = 4000 + 2500 + 500
+polera = polera - costo_polera
+costo_total = polera * 50
+ganancia = costo_total
+
+print(f"ganastes este mes: {ganancia}")
