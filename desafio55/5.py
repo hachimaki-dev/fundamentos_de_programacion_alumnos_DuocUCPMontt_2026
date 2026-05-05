ef13b94d8e639da0f3c41c3ca504743be8b9d1f6
amarooig <ama.gonzaleza@duocuc.cl>
2026-05-04 22:52:46 -0400

    Resuelto Ejercicio 5: Administrador de Carga Bip! Avanzado

---
+++ desafio55/5.py
@@ -0,0 +1,10 @@
+saldo = 10000
+pasaje_micro = 730
+pasaje_metro = 850
+recargar = 3000
+
+dia1 = saldo - 1 * pasaje_micro - 2 * pasaje_metro
+saldo_dia1 = dia1 + recargar
+print("Saldo después del día 1: ", saldo_dia1)
+
+
