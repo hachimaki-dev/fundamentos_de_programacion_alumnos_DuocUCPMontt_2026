1985465d6f558b2b64d0ad63088bd177804cbd37
amarooig <ama.gonzaleza@duocuc.cl>
2026-05-04 20:49:57 -0400

    Resuelto Ejercicio 4: Calculadora de Cuotas (MercadoLibre)

---
+++ desafio55/4.py
@@ -0,0 +1,9 @@
+valor_computador = 1200000
+cuotas = 12
+valor_cuota = valor_computador / cuotas
+print("Valor de cada cuota: ", valor_cuota)
+envio = 15000
+valor_cuota_con_envio = valor_cuota + envio
+print("Valor de cada cuota con envío: ", valor_cuota_con_envio)
+
+
