ef31ab3fbd6c239c79dd8d4e58f88a4b69011434
Stello17 <sptellocaceres@gmail.com>
2026-05-04 20:20:16 -0400

    Resuelto Ejercicio 5: Administrador de Carga Bip! Avanzado

---
+++ desafio55/4.py
@@ -0,0 +1,8 @@
+notebook = 1200000
+cuotas = 12
+valor_envio = 15000
+#valor cuota
+valor_cuota = notebook // cuotas
+valor_cuota_con_envio = valor_cuota + valor_envio
+
+print(f"El vlaor de lap rimera cuota con envio es de {valor_cuota_con_envio}")
