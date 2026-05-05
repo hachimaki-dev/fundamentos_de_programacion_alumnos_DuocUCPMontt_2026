bcd6866d1892f66305b726d1763b28bd1e5a74dd
Maxiidxss <levmaximiliano@gmail.comm>
2026-05-05 17:22:07 -0400

    Resuelto Ejercicio 9: Costos de Producción (Emprendimiento)

---
+++ ejercicio9-55.py
@@ -0,0 +1,11 @@
+#Vendes cada polera a 15000.
+#Hacer una polera te cuesta: 4000 
+# la tela, 2500 el estampado y 500 el empaque. Vendiste 50 poleras en el mes.
+
+polera = 15000
+tela = 4000
+estampado = 2500
+empaque = 500
+ventas_poleras_mes = 50
+
+print ((polera - (tela + estampado + empaque ))* ventas_poleras_mes)
