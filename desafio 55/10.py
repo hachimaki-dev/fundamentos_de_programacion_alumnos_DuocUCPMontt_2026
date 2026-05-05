60def00d08fcdd4a9aef356b32e921e4a696482f
diego0954-alt <diego0954@icloud.com>
2026-05-04 20:34:08 -0400

    Estacionamiento Aeropuerto

---
+++ desafio 55/10.py
@@ -0,0 +1,7 @@
+primera_hora = 1200
+bloques_media_hora_extra = 500
+bloques_extras = 4
+
+precio_total = primera_hora + (bloques_media_hora_extra * 4)
+
+print(f"total a cobrar es de: {precio_total}")
