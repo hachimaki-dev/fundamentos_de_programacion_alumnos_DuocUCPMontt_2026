0dc01d17c5d3f97e5054e401230fce50e4f0d1f6
Markito33 <markitoaguilar080@gmail.com>
2026-05-04 20:06:22 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ desafio55/3.py
@@ -0,0 +1,14 @@
+valor_pichanga = 30000
+valor_schops = 3500 * 5
+
+consumo = valor_schops + valor_pichanga
+
+propina = consumo * 10
+
+total = propina // 100
+
+total_final = consumo + total
+
+total_individual = total_final / 5
+
+print(total_individual)
