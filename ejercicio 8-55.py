e1f6eafe88baa5c01089cba50b284330f7fd41af
Maxiidxss <levmaximiliano@gmail.comm>
2026-05-05 17:15:37 -0400

    Resuelto Ejercicio 8: Rendimiento de Servidor (AWS EC2)

---
+++ ejercicio 8-55.py
@@ -0,0 +1,10 @@
+#servidor tiene 16 GB en total
+#El sistema operativo siempre usa 2.5 GB.
+#Tienes 4 programas corriendo y cada uno usa 1.2 GB. (Recuerda que 1 GB son 1024 MB).
+
+ram_total = 16
+sistema_operativo = 2.5
+programas_corriendo = 4
+consumo_programa = 1.2
+
+print((ram_total - (sistema_operativo + (programas_corriendo * consumo_programa))) * 1024)
