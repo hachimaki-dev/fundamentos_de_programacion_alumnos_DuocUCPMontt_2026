6794a661bbce041e907df5543dadb58add15475c
fe-macias <fernando.macias19@icloud.comgit config --global user.name fernando.macias19@icloud.com>
2026-05-04 22:56:15 -0400

    ejercicio 10

---
+++ desafio55/estacionamiento.py
@@ -0,0 +1,11 @@
+#Vas a calcular cuánto te van a cobrar por dejar el auto en el aeropuerto.
+
+#Datos Iniciales: Estacionar la primera hora cuesta 1200. Después de eso, cada media hora extra cuesta 500. Estuviste estacionado 3 horas en total.
+
+#Reglas de Negocio: La primera hora se cobra a 1200. Las 2 horas que sobran equivalen a 4 bloques de media hora extra.
+
+#Restricciones: Calcula el total sumando el precio de la primera hora más los bloques extra multiplicados por su valor. Imprime el costo total.
+estacionamiento_por_hora = 1200
+estacionamiento_despues_hora = 500
+total_estacionamiento = (500*2) + 1200
+print(total_estacionamiento)
