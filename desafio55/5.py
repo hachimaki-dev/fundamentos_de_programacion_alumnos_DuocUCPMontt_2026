9785d0a29f419f82745e9f0d3c69fb36e7c61ac0
consumo23 <consuelomb15@gmail.com>
2026-05-05 01:19:44 -0400

    Resuelto Ejercicio 5: Administrador de Carga Bip! Avanzado

---
+++ desafio55/5.py
@@ -0,0 +1,12 @@
+#Ejercicio 5: Administrador de Carga Bip! Avanzado
+
+saldo_tarjeta = 10000
+pasaje_micro = 730
+pasaje_metro = 850
+recarga = 3000
+
+
+saldo_tarjeta = saldo_tarjeta - pasaje_micro -  pasaje_metro * 2
+saldo_tarjeta = saldo_tarjeta + 3000
+
+print(saldo_tarjeta)
