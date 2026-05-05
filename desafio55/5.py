dbc67a885e3433a9dc3a1a387a210b0a5bd6a29b
BenjaminV-Ak <benjamin.vargas.catrilef@gmail.com>
2026-05-04 20:24:18 -0400

    Resuelto Ejercicio 5: Administrador de Carga Bip! Avanzado

---
+++ desafio55/5.py
@@ -0,0 +1,7 @@
+plata_en_tarjeta = 10000
+pasaje_micro = 730
+pasaje_metro = 850
+recarga = 3000
+
+gastado_por_dia = plata_en_tarjeta - pasaje_micro - pasaje_metro - pasaje_metro + recarga
+print(gastado_por_dia)
