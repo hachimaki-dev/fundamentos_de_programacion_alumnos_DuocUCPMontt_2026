d57edf3ec21e68e4fa38d953181656472c4ee585
SIA <o98290039@gmail.com>
2026-05-04 20:55:49 -0400

    Resuelto Ejercicio 9: Costos de Producción (Emprendimiento)

---
+++ desafio55/9.py
@@ -0,0 +1,8 @@
+valor_venta_polera = 15000
+tela = 4000
+estampado = 2500
+empaque = 500
+venta_mensual = 50
+costo_polera = tela + estampado + empaque
+ganancia = valor_venta_polera - costo_polera
+print(ganancia * venta_mensual)
