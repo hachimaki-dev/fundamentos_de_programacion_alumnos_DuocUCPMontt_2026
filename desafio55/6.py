d1ce6105d04824e0bbc379d0b004220de512aa4c
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-04 20:14:25 -0400

    Resuelto Ejercicio 6: Ganancia en Criptomonedas (Binance)

---
+++ desafio55/6.py
@@ -0,0 +1,9 @@
+bitcoin = 0.05
+valor_compra = 50000
+valor_actual = 62000
+dolar = 900
+
+diferencia = valor_actual - valor_compra
+ganancia = diferencia * bitcoin
+ganancia_pesochileno = ganancia * dolar
+print(ganancia_pesochileno)
