170f86572b0c27cd6dcb1ddd83e788c8c9c48393
Impox0 <benjaminmaldonadojeldes@gmail.com>
2026-05-04 20:26:58 -0400

    Resuelto Ejercicio 7: Armando el PC Gamer

---
+++ desafio55/7.py
@@ -0,0 +1,12 @@
+procesador = 250000
+placa = 120000
+tajeta_video = 450000
+descuento = 15
+menos = (tajeta_video * descuento)/100
+descuento_total = tajeta_video - menos
+
+total = procesador + placa + descuento_total
+
+print(total)
+
+
