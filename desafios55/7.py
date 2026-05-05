c8fded75dfc44c16501cec0bd548bf40d86dec5b
jp-sanchez6 <megajuanx@gmail.com>
2026-05-05 18:45:54 -0400

    ejercicio 7 completado

---
+++ desafios55/7.py
@@ -0,0 +1,9 @@
+procesador = 250000
+placa_madre = 120000
+gpu_precio_base = 450000
+
+gpu_con_descuento = gpu_precio_base * 0.85
+
+total_compra = procesador + placa_madre + gpu_con_descuento
+
+print(total_compra)
