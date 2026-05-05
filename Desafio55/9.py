4e607a7b2f7b2f9333b4376973d3052b248371bf
yvnnng28 <Benjaandres29sanchezzz@gmail.com>
2026-05-04 20:14:36 -0400

    Resuelto Ejercicio 9: Costos de Producción (Emprendimiento)

---
+++ Desafio55/9.py
@@ -0,0 +1,6 @@
+polera = 15000
+costo_polera = 4000 + 2500 + 500
+ventas_mes = polera * 50
+ganancias_polera = polera - costo_polera
+ganancias_totales = ganancias_polera * 50
+print(ganancias_totales)
