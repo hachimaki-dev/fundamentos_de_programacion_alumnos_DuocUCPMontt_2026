021749374869490daf3e58996b4c6843b0116da0
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-04 20:43:59 -0400

    Resuelto Ejercicio 4: Calculadora de Cuotas (MercadoLibre)

---
+++ desafio_55/4.py
@@ -0,0 +1,10 @@
+#Calculadora de cuotas
+
+notebook = 1200000
+cantidad_cuotas = 12
+envio_casa = 15000
+cuota_mensual = notebook / cantidad_cuotas
+valor_cuota = envio_casa + cuota_mensual
+
+print(valor_cuota)
+
