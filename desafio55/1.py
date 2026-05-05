bd580d7e808ae11945052a703d0c488a5f655e7a
Impox0 <benjaminmaldonadojeldes@gmail.com>
2026-05-05 01:49:10 -0400

    Resuelto Ejercicio 1: Liquidación de Sueldo Básico

--- desafio55/1.py
+++ desafio55/1.py
@@ -4,8 +4,8 @@ movilizacion = 30000
 salud = 7
 afp = 10
 
-descuento_afp = (sueldo_base * salud)/100
-descuento_salud = (sueldo_base * afp)/100
+descuento_afp = (sueldo_base * afp)/100
+descuento_salud = (sueldo_base * salud)/100
 sueldo_liquido = sueldo_base - descuento_afp - descuento_salud + bono_colacion + movilizacion
 
 print(f"Tu sueldo liquido es de: {sueldo_liquido}")   
