0db1331ade740461ceadaa66f0331c516b129a13
amarooig <ama.gonzaleza@duocuc.cl>
2026-05-05 13:44:06 -0400

    Resuelto Ejercicio 11: Clave Única Básico

--- desafio55/11.py
+++ desafio55/11.py
@@ -1,6 +1,8 @@
 intentos_fallidos = 0
-clave = "admin 1234"
-
-print("---bienvenido a su banco seguro---\n--para continuar:--\n")
-if clave == contraseña:
-    print(int(input("ingrese su contraseña:")))
+clave_correcta = "secreto"
+clave_escrita = "admin123"
+if clave_escrita == clave_correcta:
+    print("Acceso concedido")
+else:
+    intentos_fallidos += 1
+    print("Intentos fallidos: ", intentos_fallidos)
