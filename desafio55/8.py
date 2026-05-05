f3c164e5dfbfa3bb29c03de7985855757be9efab
yosmer1106 <yos.carrillo@duocuc.cl>
2026-05-05 00:52:40 -0400

    resuelto ejercicio 8: rendimiento de servidor (AWS EC2)

---
+++ desafio55/8.py
@@ -0,0 +1,17 @@
+capacidad_servidor = 16
+sistema_operativo = 2.5 
+programas = 4 * 1.2
+gb = 1024
+
+uso =  sistema_operativo + programas
+gigas_libres = capacidad_servidor - uso
+
+gigas_libres = gigas_libres * gb
+
+
+print(gigas_libres)
+
+
+
+
+
