15cf13e201f5c8697d499f3111b5b39a2b5b65ec
fe-macias <fernando.macias19@icloud.comgit config --global user.name fernando.macias19@icloud.com>
2026-05-04 22:51:26 -0400

    ejerccio 9

---
+++ desafio55/produccion.py
@@ -0,0 +1,11 @@
+#Vas a vender poleras estampadas y necesitas calcular tus ganancias mensuales.
+
+#Datos Iniciales: Vendes cada polera a 15000. Hacer una polera te cuesta: 4000 la tela, 2500 el estampado y 500 el empaque. Vendiste 50 poleras en el mes.
+
+#Reglas de Negocio: La ganancia por cada polera es su precio de venta menos todo lo que te costó hacerla. La ganancia del mes es esa ganancia por unidad multiplicada por las poleras que vendiste.
+
+#Restricciones: No escribas el resultado a mano, deja que Python haga las sumas y multiplicaciones. Imprime tu ganancia mensual final
+venta_polera = 15000
+costo_por_polera = (4000 + 2500 + 500)
+ganancia_polera = (venta_polera - costo_por_polera) * 50
+print(ganancia_polera) 
