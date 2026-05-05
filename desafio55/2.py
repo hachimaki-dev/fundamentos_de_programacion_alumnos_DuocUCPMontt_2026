2b811742e7ec46df27f80a09d3f35d3c25f3eec6
carla1.nicole <ca.nahuelquins@duocuc.cl>
2026-05-04 20:29:20 -0400

    Resuelto Ejercicio 4: Calculadora de Cuotas (MercadoLibre)

--- desafio55/2.py
+++ desafio55/2.py
@@ -3,5 +3,6 @@ ahorro=1500000
 ahorro_cada_mes= 150000
 
 dinero_faltante= (total - ahorro)
-dinero_futuro= ( ahorro_cada_mes * 24 + ahorro )
-print(dinero_futuro)
+
+total_cuotas = dinero_faltante // ahorro_cada_mes
+print(total_cuotas)
---
+++ desafio55/4.py
@@ -0,0 +1,5 @@
+valor_notebook= 1200000
+envio= 15000 
+
+valor_cuota_primer_mes = (1200000 / 12 + 15000)
+print(valor_cuota_primer_mes )
