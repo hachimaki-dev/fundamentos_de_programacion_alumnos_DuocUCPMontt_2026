6ffb4637f68cc3f9997d228e106adda50d6b19f1
AphidCiro <navarrogamboajulian@gmail.com>
2026-05-05 09:17:08 -0400

    agregados ejercicios 13-14

---
+++ Desafio55/13.py
@@ -0,0 +1,12 @@
+#Revisa si un cliente puede subir a categoría VIP en su banco.
+#Datos Iniciales: El cliente gana 800000, lleva 6 años en el banco y tiene 0 deudas.
+#Reglas de Negocio: Eres VIP si cumples UNA de estas dos opciones: Opción A) Ganas más de 1000000. Opción B) Llevas 5 o más años en el banco Y tienes 0 deudas.
+#Restricciones: Combina `or` y `and` en un mismo `if`. Usa paréntesis para agrupar la Opción B y que Python no se confunda. Imprime `'Cliente VIP'` o `'Cliente Normal'`
+ganancia_cliente = 800000
+años_en_banco = 5
+deudas = 0
+if ganancia_cliente > 1000000 or (años_en_banco >= 5 and deudas == 0):
+    print("Cliente VIP")
+else:
+    print("Cliente Normal")
+    
---
+++ Desafio55/14.py
@@ -0,0 +1,15 @@
+#Calcula si un alumno aprueba su ramo sumando un bono por asistencia.
+#Datos Iniciales: Tienes un 3.6 de nota y un 85% de asistencia.
+#Reglas de Negocio: Si tienes 80% o más de asistencia, tu nota sube 0.5 puntos. Luego de aplicar el bono (o no), revisa tu estado: si tu nota final es 4.0 o más, estás 'Aprobado'. Si estás entre 3.5 y 3.9, vas a 'Examen'. Si tienes menos, estás 'Reprobado'.
+#Restricciones: Usa un `if` para el bono y luego un bloque `if/elif/else` para definir el estado. Imprime solo el estado ('Aprobado', 'Examen' o 'Reprobado')
+nota = 3.6
+asistencia = 0.85
+
+if asistencia > 0.80:
+    nota = nota + 0.5
+if nota >= 4.0:
+    print("Aprobado")
+elif nota >= 3.5 and nota <= 3.9:
+    print("Examen")
+else:
+    print("Reprobado")
