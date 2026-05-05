8ce130f1b68282a70458359922e4a1b59840bafc
consumo23 <consuelomb15@gmail.com>
2026-05-05 14:36:59 -0400

    Resuelto Ejercicio 20: Simulador de Cajero: Retiro Complejo

---
+++ desafio55/20.py
@@ -0,0 +1,14 @@
+#Ejercicio 20: Simulador de Cajero: Retiro Complejo
+
+saldo = 50000
+limite_retiro = 200000
+retiro = 60000
+
+if retiro > limite_retiro :
+    print("Excede el limite")
+elif retiro > saldo:
+    print("Saldo insuficiente")
+elif retiro % 5000:
+    print("Monto invalido")
+else:
+    print("Retiro exitoso")
