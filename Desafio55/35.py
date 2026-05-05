a3dd5c26e0be7b74dadc498475adbb009882b2a5
yvnnng28 <Benjaandres29sanchezzz@gmail.com>
2026-05-05 03:36:27 -0400

    Resuelto Ejercicio 35: Filtrar por Umbral (Banco)

---
+++ Desafio55/35.py
@@ -0,0 +1,9 @@
+transf_dia = [15000, 80000, 2000, 150000]
+importante = []
+for tranfe in transf_dia:
+    if tranfe > 50000:
+        importante.append(tranfe)
+    else:
+        continue
+
+print(importante)
