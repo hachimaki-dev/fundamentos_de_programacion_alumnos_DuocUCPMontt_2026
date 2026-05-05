3c1a9a7404637cc2cd891a299a85c19b1c6091f1
AG26535 <alangodoy235@gmail.com>
2026-05-04 20:53:34 -0400

    Resuelto Ejercicio 7: Armando el PC Gamer

---
+++ desafio55/7.py
@@ -0,0 +1,7 @@
+procesador = 250000
+placa_madre = 120000
+tarjeta_video = 450000
+descuento = tarjeta_video * 0.15
+tarjeta_video = tarjeta_video - descuento 
+total = procesador + placa_madre + tarjeta_video 
+print (total)
