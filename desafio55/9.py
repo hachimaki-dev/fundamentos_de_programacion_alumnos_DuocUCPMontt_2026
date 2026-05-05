21746551109c488438859d6a2c0a69c0e11bf990
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-04 20:47:04 -0400

    Resuelto Ejercicio 9: Costos de Producción (Emprendimiento)

---
+++ desafio55/9.py
@@ -0,0 +1,9 @@
+precio_venta = 15000
+tela = 4000
+estampado = 2500
+empaquetado = 500
+ventas_mes = 50
+
+ganacia_polera = precio_venta - (tela + estampado + empaquetado)
+ganancia_mes = ganacia_polera * ventas_mes
+print(ganancia_mes)
