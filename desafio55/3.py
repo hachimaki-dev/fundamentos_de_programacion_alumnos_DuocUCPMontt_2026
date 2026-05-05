f611110287a38d85659120b5ecd15722251b3211
amarooig <ama.gonzaleza@duocuc.cl>
2026-05-04 20:08:43 -0400

    desafío 55 de Amaro

---
+++ desafio55/3.py
@@ -0,0 +1,6 @@
+pichanga = 15000 * 2
+schops = 3500 * 5
+total_gastos = pichanga + schops
+print("Total de gastos: ", total_gastos)
+propina = total_gastos * 0.1
+print("Propina a pagar: ", propina)
