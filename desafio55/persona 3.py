cbaf41a00ab1d1606aa450e784980535e1d935fb
Xtroler <xionx25@gmail.com>
2026-05-04 20:38:29 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ desafio55/persona 3.py
@@ -0,0 +1,9 @@
+personas = 5 
+pinchanga = 2 * 15000
+schops  = 5 * 3500
+
+total_con_propina = (pinchanga + schops ) * 1.1
+
+pago_por_persona = total_con_propina / personas
+
+print (pago_por_persona)
