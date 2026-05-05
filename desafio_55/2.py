873f8544694442f42b65c4b701ccdd7abba4d7ec
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-04 20:07:13 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ desafio_55/2.py
@@ -0,0 +1,15 @@
+#Proyeccion de ahorro para un auto
+
+auto = 5000000
+ahorrado = 1500000
+cada_mes_ahorrado = 150000
+
+
+cuanto_dinero_falta = (auto - ahorrado)
+
+cuantos_meses_tardara = (cuanto_dinero_falta // cada_mes_ahorrado)
+
+print(cuantos_meses_tardara)
+
+
+
