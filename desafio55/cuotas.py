660d24fb262b7ec026d5925d51460f1091f96c78
MatiasValdivia327 <matias.felipe7777>
2026-05-05 15:27:29 -0400

    Resuelto Ejercicio 4: Calculadora de Cuotas (MercadoLibre)

---
+++ desafio55/cuotas.py
@@ -0,0 +1,14 @@
+# a 12 cuoatas , el envio cuesta 15000
+notebook = 1200000
+envio = 15000
+
+print(f"compraste tu notebook a {notebook} en 12 cuotas y con un envio de {envio} que se paga el total en el primer mes con su debida cuota")
+primer_mes = notebook // 12 + envio
+print(f"el total a pagar en tu primera cuota es de {primer_mes}")
+
+
+
+
+
+
+
