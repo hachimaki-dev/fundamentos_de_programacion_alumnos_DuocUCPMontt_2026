bbdecd012df33a8611deeda03c10af58685fa998
micky260 <micky280405@gmail.com>
2026-05-05 16:09:17 -0400

    ejercicios

---
+++ EA2/002D_game_mica.py/002D_ejersicios.py/15_ejercicio.py
@@ -0,0 +1,7 @@
+total_compra_cliente = 25000
+destino_origen_cliente = "magallanes"
+
+total_costo_envio = 0
+costo_envio_zona_pelirosa = 20000
+
+if total_compra_cliente > 20000 and 
---
+++ EA2/002D_game_mica.py/002D_ejersicios.py/4_ejercicio.py
@@ -0,0 +1,6 @@
+notebook = 1200000
+envio_casa =  15000
+
+cuotas = notebook * 0.15
+
+total_pagar_dia = cuotas + envio_casa
