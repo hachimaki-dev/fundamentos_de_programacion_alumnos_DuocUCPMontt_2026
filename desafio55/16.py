bc578273f5af643d16eac40a15211dbb71941377
Impox0 <benjaminmaldonadojeldes@gmail.com>
2026-05-04 22:16:57 -0400

    Resuelto Ejercicio 16: Plan de Datos WOM con Penalización

---
+++ desafio55/16.py
@@ -0,0 +1,18 @@
+plan = 15
+usados = 18
+postpago = "postpago"
+prepago = "prepago"
+cobro = 0
+extra = 1000
+
+
+cliente = input("¿Que tipo de cliente es?")
+if cliente == postpago:
+    gb = usados - plan
+    cobro = extra * gb
+else:
+    cliente == prepago
+    cobro = cobro + 0
+    
+
+print(cobro)
