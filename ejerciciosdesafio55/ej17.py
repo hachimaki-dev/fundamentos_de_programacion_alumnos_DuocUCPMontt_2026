14db6bd9588b5416c922918d9300aa273774dda3
benjamin476 <alexander.10benjamin@gmail.com>
2026-05-05 19:54:35 -0400

    Resuelto Ejercicio 17: Multa Autopista por Tipo de Vehículo

---
+++ ejerciciosdesafio55/ej17.py
@@ -0,0 +1,7 @@
+vheiculo = "camion"
+velocidad = 95
+limite_camion = 80
+if vheiculo == "auto" and velocidad > 120:
+    print("multa gravisima")
+elif vheiculo == "camion" and velocidad > 80:
+    print("multa grave camion")
