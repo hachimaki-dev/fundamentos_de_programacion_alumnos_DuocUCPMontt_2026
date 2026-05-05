04191c89a4178b32d85090c3d8ce3afaf95450a1
Maxiidxss <levmaximiliano@gmail.comm>
2026-05-05 19:08:37 -0400

    Ejercicio 16

--- ejercicio 16-55.py
+++ ejercicio 16-55.py
@@ -1,4 +1,18 @@
 #Su plan le da 15 GB,
 # pero gastó 18 GB.
 #clientes 'Postpago' se les cobra 1000 pesos por cada giga extra que usen.
-#Si el cliente fuera 'Prepago', no se le cobra nada extra, solo se le corta el internet (mensaje 'Sin saldo').
+#Si el cliente fuera 'Prepago', no se le cobra nada extra, 
+# solo se le corta el internet (mensaje 'Sin saldo').
+
+cliente_postpago = "postapago"
+plan_gigas = 15
+gigas_gastados = 18
+
+if cliente_postpago == "postapago":
+    if gigas_gastados > plan_gigas:
+        extra_gigas = gigas_gastados - plan_gigas
+        cobro = extra_gigas * 1000
+        print(cobro)
+
+else:
+    print("sin saldo")
