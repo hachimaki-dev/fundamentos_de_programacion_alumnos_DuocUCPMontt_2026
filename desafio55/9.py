2409a6ed1ec3e32eab896f9a5cdbc60f33f5b661
Impox0 <benjaminmaldonadojeldes@gmail.com>
2026-05-04 20:41:04 -0400

    Resuelto Ejercicio 9: Costos de Producción (Emprendimiento)

---
+++ desafio55/9.py
@@ -0,0 +1,10 @@
+vendes_poleras = 15000
+tela = 4000
+estampado = 2500
+empaque = 500
+venta_mes = 50
+
+total = vendes_poleras - tela - estampado - empaque
+ganancia = venta_mes * total
+
+print(f"Total ganancia al mes es de: {ganancia}")
