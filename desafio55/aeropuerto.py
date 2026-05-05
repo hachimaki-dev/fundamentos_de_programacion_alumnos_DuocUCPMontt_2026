798647c1bd9e17da210537392bb7ba676c556147
MatiasValdivia327 <matias.felipe7777>
2026-05-05 16:57:07 -0400

    Resuelto Ejercicio 10: Estacionamiento Aeropuerto

---
+++ desafio55/aeropuerto.py
@@ -0,0 +1,21 @@
+# Estacionar la primera hora cuesta 1200. Después de eso, cada media hora extra cuesta 500. Estuviste estacionado 3 horas en total.
+#  La primera hora se cobra a 1200. Las 2 horas que sobran equivalen a 4 bloques de media hora extra.
+primera_hora = 1200
+media_hora = 500
+precio_final = primera_hora + media_hora * 4
+
+print(f"estuviste estacionado 3 horas en el aeropuerto, la priemra hora vale {primera_hora} mil pesos despues cada media hora sale {media_hora} pesos ,tu cuota final es de {precio_final}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
