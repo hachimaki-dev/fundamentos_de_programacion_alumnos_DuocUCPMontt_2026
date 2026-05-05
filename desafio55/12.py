d4d39102c269a5ed5643975d7a2515212d8f30bf
amarooig <ama.gonzaleza@duocuc.cl>
2026-05-05 14:04:58 -0400

    Resuelto Ejercicio 12: Aprobación de Crédito (AND simple)

---
+++ desafio55/12.py
@@ -0,0 +1,10 @@
+cliente = 550000
+deudas = 1
+#la empresa exige
+sueldo_mayor = 500000
+deudas1 = 0
+if cliente > sueldo_mayor and deudas == deudas1:
+    #si el cliente cumple con las condiciones, se le aprueba el credito
+    print("El cliente es apto para el crédito")
+else:    
+    print("rechazado")
