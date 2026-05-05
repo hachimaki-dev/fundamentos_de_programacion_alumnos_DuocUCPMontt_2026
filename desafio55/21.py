bff5e7c04e1e7352810b424726b8328887959f13
consumo23 <consuelomb15@gmail.com>
2026-05-05 15:03:39 -0400

    Resuelto Ejercicio 21: Cuenta Regresiva Básica (SpaceX)

---
+++ desafio55/21.py
@@ -0,0 +1,16 @@
+#Ejercicio 21: Cuenta Regresiva Básica (SpaceX)
+
+cronometro = 10
+
+
+while True:
+    if cronometro > 0:
+        print(cronometro)
+        cronometro = cronometro - 1
+       
+    else:
+       print("Despegue")
+       break
+
+
+
