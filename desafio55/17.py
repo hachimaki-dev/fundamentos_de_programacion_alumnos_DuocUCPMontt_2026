24f26418882f15c474eca712e97145f5c3ee41d2
bisente777 <vic.aponte@duocuc.cl>
2026-05-05 20:18:03 +0000

    ejercicio 18

---
+++ desafio55/17.py
@@ -0,0 +1,6 @@
+tipo_de_vehiculo = "camion"
+velocidad_en_km = 95
+if tipo_de_vehiculo == "camion" and velocidad_en_km > 80:
+    print("multa grave camion")
+elif tipo_de_vehiculo == "auto" and velocidad_en_km > 120:
+    print("multa gravisima")
