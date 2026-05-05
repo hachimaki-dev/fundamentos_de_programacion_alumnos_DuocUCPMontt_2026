18d73531773aad1b07a3e6d6b1e380684e9ee3be
seba1304256 <seba13email.com@gmail.com>
2026-05-05 09:08:33 -0400

    Resuelto ejercicio 18: Sistema anti-robo inteligente (smart home)

---
+++ desafio_55/18.py
@@ -0,0 +1,14 @@
+#Configura la alarma de tu casa para que suene solo cuando debe.
+
+#Datos Iniciales: Hubo movimiento (`movimiento = True`). Son las 3 de la mañana (`hora = 3`). El dueño está en casa (`dueño_en_casa = True`).
+
+#Reglas de Negocio: La alarma suena si se cumple ALGUNA de estas dos situaciones: Situación A: Hay movimiento y el dueño NO está en casa. Situación B: Hay movimiento y es de madrugada (antes de las 6 AM), no importa si el dueño está o no.
+
+#Restricciones: Usa paréntesis para separar ambas situaciones con un `or` al medio. Imprime `'Alarma Sonando'` o `'Modo Silencioso'`.
+movimiento = True
+hora = 3 
+dueño_en_casa = True
+if (movimiento and not dueño_en_casa) or (movimiento and hora < 6):
+    print("Alarma Sonando")
+else:
+    print("Modo Silencioso")
