c07f987e6a667497ea1beabd1baf8f1367d6c520
consumo23 <consuelomb15@gmail.com>
2026-05-05 02:16:51 -0400

    Resuelto Ejercicio 12: Aprobación de Crédito (AND simple)

---
+++ desafio55/12.py
@@ -0,0 +1,10 @@
+#Ejercicio 12: Aprobación de Crédito (AND simple)
+
+sueldo_cliente = 550000
+deuda_activa = 1
+
+if sueldo_cliente > 500000 and deuda_activa == 0 :
+    print("Aprobado")
+
+else: 
+    print("Rechazado")
