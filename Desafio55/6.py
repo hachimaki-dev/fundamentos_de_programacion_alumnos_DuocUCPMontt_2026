c0d298b669fcc4ac61256ad3da7ebb97538ed710
CodePalmer <sloomby87@gmail.com>
2026-05-05 01:16:39 -0400

    Resuelto Ejercicio 6 : Ganancias en criptomonedas (binance)

---
+++ Desafio55/6.py
@@ -0,0 +1,9 @@
+btc = 0.50
+precio_antiguo = 50000
+precio_actual = 62000
+dolar = 900
+ganancia_btc = precio_actual - precio_antiguo
+ganancias_usd = ganancia_btc * btc
+ganancias_clp = ganancias_usd * dolar
+
+print(ganancias_clp)
