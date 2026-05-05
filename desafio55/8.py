a1e1f2c6d85d47b92e8c451741e398fef75fd040
BenjaminV-Ak <benjamin.vargas.catrilef@gmail.com>
2026-05-04 20:51:22 -0400

    Resuelto Ejercicio 8: Rendimiento de Servidor (AWS EC2)

---
+++ desafio55/8.py
@@ -0,0 +1,6 @@
+gb_servidor = 16 *1024
+sistema_operativo = 2.5 *1024
+cantidad_de_programas = 4 *1024 * 1.2
+
+gasto_total =  gb_servidor - sistema_operativo - cantidad_de_programas 
+print(gasto_total)
