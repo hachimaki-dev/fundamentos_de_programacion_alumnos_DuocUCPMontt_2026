6753fb341e3f65b282c09b118ce348683871fef3
Maxiidxss <levmaximiliano@gmail.comm>
2026-05-05 17:36:09 -0400

    Ejercicio 10

---
+++ ejercicio 10-55.py
@@ -0,0 +1,9 @@
+#Estacionar la primera hora cuesta 1200
+#cada media hora extra cuesta 500. 
+#Estuviste estacionado 3 horas en total
+
+primera_hora = 1200
+treinta_minutos = 500
+total_horas = 3
+
+print(primera_hora + treinta_minutos * 4)
