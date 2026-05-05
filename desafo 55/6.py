b0095a96824913107da248aee68a563865aec050
ñoquita <valentina19.ism@0gmail.com>
2026-05-04 22:49:20 -0400

    Resuelto Ejercicio 6: Ganancia en Criptomonedas (Binance)

--- desafo 55/6.py
+++ desafo 55/6.py
@@ -1,3 +1,9 @@
 valor_bitcoin = 50000
-porcentaje_bitcoin_comprado = 5
+segundo_valor_bitcoin = 62000
+porcentaje_bitcoin_comprado = 0.05
 valor_dolar = 900
+
+valor_diferencia = (segundo_valor_bitcoin - valor_bitcoin) * porcentaje_bitcoin_comprado
+
+dolares_a_pesos = valor_diferencia * valor_dolar
+print(dolares_a_pesos)
