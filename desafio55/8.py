a5b42c03383d3db157db6093110bdf4e15c13593
nicoleasd <nicaguilar2006@gmail.com>
2026-05-05 17:55:51 -0400

    Resuelto Ejercicio 8: Rendimiento de Servidor (AWS EC2)

---
+++ desafio55/8.py
@@ -0,0 +1,14 @@
+servidor = 16
+
+sistema_operativo = 2.5
+
+programa = 1.2
+
+consumo = sistema_operativo + programa * 4
+#________________________________________
+
+servidor = servidor - consumo
+
+servidor= servidor * 1024
+
+print(servidor)
