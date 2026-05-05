25df214cdfe96893c6790822b8f35d518c904b95
Stello17 <sptellocaceres@gmail.com>
2026-05-04 20:09:01 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ desafio55/3.py
@@ -0,0 +1,11 @@
+amigo_total = 5
+pinchanga_total = 15000 * 2
+schops_total = 3500 * 5
+
+#total consumo
+total_consumo = (pinchanga_total + schops_total)
+total_consumido_final = total_consumo  + (total_consumo * 0.1)
+print(f" El total consumido fue {total_consumido_final}")
+#consumo de amigos
+consumo_amigos = (total_consumido_final // amigo_total)
+print(f" El total a cancelar por cada uno es de {consumo_amigos}")
