99847240a01a0c633d27a33efccd61714624b9af
Stello17 <sptellocaceres@gmail.com>
2026-05-04 20:48:22 -0400

    Resuelto Ejercicio 5: Administrador de Carga Bip! Avanzado

--- desafio55/2.py
+++ desafio55/2.py
@@ -3,7 +3,6 @@ ahorro_actual = 1500000
 ahorro_mes = 150000
 #valor faltante
 valor_faltante = (valor_auto - ahorro_actual)
-print(f"Lo que me falta para el auto es {valor_faltante}")
 #meses para ahorrar
 meses = (valor_faltante // ahorro_mes)
 print(f"Los meses que me faltan son {meses}")
--- desafio55/4.py
+++ desafio55/4.py
@@ -5,4 +5,4 @@ valor_envio = 15000
 valor_cuota = notebook // cuotas
 valor_cuota_con_envio = valor_cuota + valor_envio
 
-print(f"El vlaor de lap rimera cuota con envio es de {valor_cuota_con_envio}")
+print(f"El valor de la primera cuota con envio es de {valor_cuota_con_envio}")
