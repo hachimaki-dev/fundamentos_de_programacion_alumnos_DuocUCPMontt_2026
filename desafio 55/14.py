5acbddb0ebc88f78b864166d2034d4b0b887e15f
diego0954-alt <diego0954@icloud.com>
2026-05-05 19:05:51 -0400

    Sistema de Grados con Bonificación

---
+++ desafio 55/14.py
@@ -0,0 +1,14 @@
+nota = 3.6
+asistencia = 85
+
+if asistencia >= 80:
+    nota += 0.5
+    
+if nota >= 4.0:
+    print("aprobado")
+
+elif nota >=3.5:
+    print("examen")
+
+else:
+    print("reprobado")
