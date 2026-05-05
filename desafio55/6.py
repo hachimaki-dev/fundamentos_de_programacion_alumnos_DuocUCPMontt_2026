0b8b3c7ae9c07190fcd4fa9f2a997bdeb6e07df2
yosmer1106 <yos.carrillo@duocuc.cl>
2026-05-05 00:24:38 -0400

    resuelto ejercicio 6: ganancia en criptomonedas

---
+++ desafio55/6.py
@@ -0,0 +1,15 @@
+compra_btc = 0.05
+btc_antes = 50000
+btc_hoy =  62000
+dolar = 900
+ 
+diferencia_btc = btc_hoy - btc_antes
+
+total_ganado_dolares = diferencia_btc * compra_btc 
+
+conversion = total_ganado_dolares * dolar
+
+print(conversion)
+
+
+
