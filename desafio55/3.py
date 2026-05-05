f20594b6d16ff1aba3b25b5a1ea553775a8dd2db
yosmer1106 <yos.carrillo@duocuc.cl>
2026-05-04 23:56:44 -0400

    resuelto ejercicio 3: repartiendo la cuenta en el bar

---
+++ desafio55/3.py
@@ -0,0 +1,8 @@
+total_personas = 5 
+pichangas = 2 * 15000
+schops = 5 * 3500
+
+consumo = (pichangas + schops)
+total_propina = consumo * 1.10
+monto_final = total_propina // total_personas
+print(monto_final)
