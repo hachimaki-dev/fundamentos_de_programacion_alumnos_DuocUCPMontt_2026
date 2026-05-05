6e1e521bf881b1a7cdbca1295b47cd3227a5bf34
camilo765 <camiloogutierrez21@gmail.com>
2026-05-04 23:06:46 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ 003D_desafio55_camilo765/e3.py
@@ -0,0 +1,8 @@
+pichanga = 15000 * 2
+schops = 3500 * 5
+propina = 0.10
+consumo = (pichanga + schops )
+propina = consumo * 0.10
+monto_final = consumo + propina
+pago_persona = monto_final // 5
+print(int(pago_persona))
