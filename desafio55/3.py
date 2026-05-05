686095bfe953325dbcc723524c2af7e240d04c77
BenjaminV-Ak <benjamin.vargas.catrilef@gmail.com>
2026-05-04 20:15:28 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ desafio55/3.py
@@ -0,0 +1,9 @@
+total_personas = 5
+total_cuenta = (2*15000)+(5*3500)
+porcentaje_de_propina = 10
+
+propina = total_cuenta / porcentaje_de_propina
+
+total_a_pagar_entre_todos = (propina + total_cuenta) / 5
+
+print(total_a_pagar_entre_todos)
