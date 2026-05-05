393c2f3c3243b7e27e95482d5de6d4584c9847cd
amarooig <ama.gonzaleza@duocuc.cl>
2026-05-04 20:18:22 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

--- desafio55/3.py
+++ desafio55/3.py
@@ -1,6 +1,15 @@
-pichanga = 15000 * 2
-schops = 3500 * 5
-total_gastos = pichanga + schops
-print("Total de gastos: ", total_gastos)
-propina = total_gastos * 0.1
-print("Propina a pagar: ", propina)
+personas = 5
+pichanga = 2
+precio_pichanga = 15000
+schops = 5
+precio_schop = 3500
+
+total_comida = pichanga * precio_pichanga
+total_bebida = schops * precio_schop
+consumo_total = total_comida + total_bebida
+
+propina = consumo_total * 0.1
+total_a_pagar = consumo_total + propina
+pago_por_persona = total_a_pagar // personas
+
+print("Total a pagar por persona: ", pago_por_persona)
