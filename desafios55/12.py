155666a1a6a860b6e33d52925ac0b0aefce05be3
AustralMoth <siderquiroz12@gmail.com>
2026-05-05 23:26:13 -0400

    Resuelto Ejercicio 12: Aprobación de Crédito (AND simple)

---
+++ desafios55/12.py
@@ -0,0 +1,10 @@
+sueldo = 550000
+deudas_activas = 1
+
+sueldo_minimo = 500000
+deudas_permitidas = 0
+
+if sueldo > sueldo_minimo and deudas_activas == deudas_permitidas:
+    print("Aprobado")
+else:
+    print("Rechazado")
