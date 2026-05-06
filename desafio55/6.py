cb293a8ca9089458ab7841430beb319cc0c8d22e
Markito33 <markitoaguilar080@gmail.com>
2026-05-05 20:47:00 -0400

    Resuelto Ejercicio 7: Armando el PC Gamer

---
+++ desafio55/6.py
@@ -0,0 +1,12 @@
+valor_bitcoin = 50000
+
+valor_bitcoin_2 = 62000
+
+valor_comprado = 0.05
+valor_dolar = 900
+
+diferencia = (valor_bitcoin_2 - valor_bitcoin) * valor_comprado
+
+dolares_pesos = diferencia * valor_dolar
+
+print(dolares_pesos)
---
+++ desafio55/7.py
@@ -0,0 +1,18 @@
+procesador = 250000
+
+placa_madre = 120000
+
+tarjeta_video = 450000
+
+multiplicacion = tarjeta_video * 15
+
+valor_tdv = multiplicacion / 100
+
+valor_tarjeta_video_nvo = tarjeta_video- valor_tdv
+
+valor_total = procesador + placa_madre + valor_tarjeta_video_nvo
+
+print(valor_total)
+
+
+
