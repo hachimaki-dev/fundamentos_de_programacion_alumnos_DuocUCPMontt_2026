a95852673541c2ce85757511630a7747d5b47270
jp-sanchez6 <megajuanx@gmail.com>
2026-05-05 20:59:27 -0400

    ejercicio 9 completado

---
+++ desafios55/9.py
@@ -0,0 +1,14 @@
+precio_venta = 15000
+costo_tela = 4000
+costo_estampado = 2500
+costo_empaque = 500
+
+poleras_vendidas = 50
+
+costo_total_unitario = costo_tela + costo_estampado + costo_empaque
+
+ganancia_por_polera = precio_venta - costo_total_unitario
+
+ganancia_mensual = ganancia_por_polera * poleras_vendidas
+
+print(ganancia_mensual)
