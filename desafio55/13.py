772f30778a4964aa84c73a9ea9f379c14c948d92
Shiekd-L <ordonezbenja22@gmail.com>
2026-05-05 21:13:31 -0400

    Resuelto Ejercicio 14: Sistema de Grados con Bonificación

--- desafio55/13.py
+++ desafio55/13.py
@@ -3,5 +3,3 @@ años_en_banco = 6
 deudas = 0
 if cliente_ganancias >= 1000000 or años_en_banco >= 5 and deudas == 0:
     print("cliente VIP")
-else:
-    print("cliente normal")
---
+++ desafio55/14.py
@@ -0,0 +1,11 @@
+nota = 3.6
+asistencia = 85
+if asistencia >= 80:
+    nota = nota + 0.5
+
+if nota >= 4:
+    print("aprobado")
+elif nota >= 3.5 and nota < 4:
+    print("examen")
+else:
+    print("reprobado")
