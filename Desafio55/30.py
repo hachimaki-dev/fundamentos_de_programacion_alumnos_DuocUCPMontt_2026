2caf1ec7fea5fe4cde998e778cf71a44fdfe3b2e
AphidCiro <navarrogamboajulian@gmail.com>
2026-05-06 15:45:49 -0400

    Resuelto Ejercicio 30: Procesamiento Resiliente (AWS)

---
+++ Desafio55/30.py
@@ -0,0 +1,18 @@
+#El Mini-Boss: Haz un script que sepa qué hacer si se cae el servidor de tu página web.
+#Datos Iniciales: Tienes una lista de códigos de respuesta web: `[200, 404, 500, 200, 500]`. Tienes solo 1 'reintento' salvavidas guardado en una variable.
+#Reglas de Negocio: Un 200 es bueno (imprime 'Ok'). Un 404 no es grave (imprime 'No encontrado'). Pero un 500 significa que el servidor falló. Si sale un 500, gastas tu 'reintento' salvavidas (réstale 1) e imprime 'Reintentando'. PERO si te sale otro 500 y ya te quedaste sin reintentos, el sistema muere: imprime 'Servidor Caído' y cierra todo.
+#Restricciones: Usa `for`. Usa un `if` para restarle a tus reintentos. Si el reintento es menor a 0, usa `break` para parar todo. Imprime cada mensaje en orden.
+respuesta_web = [200, 404, 500, 200, 500]
+reintentos = 1
+for i in respuesta_web:
+    if i == 500 and reintentos > 0:
+        print("reintentando...")
+        reintentos -= 1
+    elif i == 500:
+        print("Servidor Caido")
+        break
+    if i == 200:
+        print("Ok")
+    elif i == 404:
+        print("No Encontrado")
+        
