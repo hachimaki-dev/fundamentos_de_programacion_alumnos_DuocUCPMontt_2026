6114faddb7531af77663eaa5a690831c911c72c1
jp-sanchez6 <megajuanx@gmail.com>
2026-05-06 09:30:16 -0400

    ejercicio 23 completado

---
+++ desafios55/23.py
@@ -0,0 +1,22 @@
+contraseña = 'hola123'
+contador = 0
+contraseña_erronea = 'chao123'
+
+while True:
+    if contraseña_erronea != contraseña:
+        contador += 1
+  
+    if contador >= 3:
+        print("bloqueo de tarjeta")
+        break
+    
+
+
+
+
+
+
+        
+
+        
+
