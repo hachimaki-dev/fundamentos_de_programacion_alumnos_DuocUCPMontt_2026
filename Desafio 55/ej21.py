4a5aad4c14a666fc0f4c5ebb717ac2f7191a3f86
Roten-chan <henriquezzobeiba@gmail.com>
2026-05-06 15:03:20 -0400

    Resuelto Ejercicio 21: Cuenta Regresiva Básica (SpaceX)

---
+++ Desafio 55/ej21.py
@@ -0,0 +1,18 @@
+#Ejercicio 21: Cuenta Regresiva Básica (SpaceX)
+#Crea el clásico reloj de cuenta regresiva para el despegue de un cohete.
+
+#Datos Iniciales: Tienes un cronómetro que parte en 10.
+
+#Reglas de Negocio: El reloj debe imprimir los números hacia atrás. Mientras el reloj sea mayor a cero, imprime el número actual y luego réstale 1. Cuando termine el tiempo, imprime 'Despegue 🚀'.
+
+#Restricciones: No hagas trampa escribiendo diez `prints`. Tienes que usar un ciclo `while` para hacerlo de forma automática.
+
+cronometro = 11
+
+while cronometro > 0:
+    cronometro -= 1
+    print(f"n{cronometro}")
+    if cronometro == 0:
+        print("Despegue🚀")
+        
+
