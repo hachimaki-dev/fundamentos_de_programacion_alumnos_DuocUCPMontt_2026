9bb73bb6b59bd223338d7cdcf05c47cb124fab78
fe-macias <fernando.macias19@icloud.comgit config --global user.name fernando.macias19@icloud.com>
2026-05-05 20:05:10 -0400

    ejercicio 19

---
+++ desafio55/matchmaking.py
@@ -0,0 +1,17 @@
+#Busca un oponente justo para una partida en línea.
+
+#Datos Iniciales: Tú tienes 1500 puntos. Encontraste un rival que tiene 1570 puntos.
+
+#Reglas de Negocio: Para saber si la pelea es justa, hay que calcular la diferencia de puntos entre ambos. Si la diferencia es de 50 puntos o menos, la partida es 'Perfecta'. Si es de 100 o menos, es 'Justa'. Si la diferencia es mayor a 100, se debe mostrar 'Buscando otro rival...'.
+
+#Restricciones: Usa la función matemática `abs(tus_puntos - sus_puntos)` para que el resultado siempre sea positivo, sin importar quién tenga más puntos. Imprime el resultado.
+puntos = 1500
+rival = 1570
+diferencia = abs(puntos-rival)
+print(diferencia)
+if diferencia <= 50:
+    print("Perfecta")
+elif diferencia <= 100:
+    print("Justa")
+else:
+    print("buscando otro rival")
