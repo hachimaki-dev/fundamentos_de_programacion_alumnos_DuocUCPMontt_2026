736587f8b5b611948296847bef25459b4fabc919
AustralMoth <siderquiroz12@gmail.com>
2026-05-05 23:09:29 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ desafios55/3.py
@@ -0,0 +1,15 @@
+pichanga = 15000
+schop = 3500
+cantidad_personas = 5
+
+total_schops = 3500 * 5
+total_pichangas = 15000 * 2
+
+total_cuenta = total_schops + total_pichangas
+
+propina = total_cuenta * 0.10
+
+total_cuenta_con_propina = total_cuenta + propina
+
+
+print (total_cuenta_con_propina / cantidad_personas)
