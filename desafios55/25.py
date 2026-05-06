5d3c0e99433a5e5e233e1d991c76087205951237
jp-sanchez6 <megajuanx@gmail.com>
2026-05-06 09:50:24 -0400

    ejercicio 25 completado

---
+++ desafios55/25.py
@@ -0,0 +1,10 @@
+mensajes = ['hola', 'noob', 'genial', 'manco']
+
+for palabra in mensajes:
+    
+    if palabra in ['noob', 'manco']:
+        print("CENSURADO")
+        
+    else:
+        print(palabra)
+    
