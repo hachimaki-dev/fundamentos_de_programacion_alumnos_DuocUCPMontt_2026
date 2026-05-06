bc346d8dc2f570c8fc88e0f279e3c8bfb4153ba8
Gtvq32 <gtvq1718@gmail.com>
2026-05-06 14:39:31 -0400

    Resuelto Ejercicio 18: Sistema Anti-Robo Inteligente (Smart Home)

---
+++ desafio55_Gustavo/18.py
@@ -0,0 +1,12 @@
+movimiento = True 
+hora = 3
+dueño_en_casa = True
+
+if movimiento == True and dueño_en_casa == False :
+    print("Alarma sonando")
+
+elif movimiento == True and hora < 6 :
+    print("Alarma sonando")
+
+else :
+    print("Modo silencioso")
