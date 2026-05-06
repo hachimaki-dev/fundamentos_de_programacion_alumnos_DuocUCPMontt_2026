3e4e0a5bd828acf716e284b06a8cc3515d96b2d2
AG26535 <alangodoy235@gmail.com>
2026-05-05 21:16:12 -0400

    Resuelto Ejercicio 22: Ahorro Meta Mensual (PS5)

--- desafio55/21.py
+++ desafio55/21.py
@@ -1,6 +1,6 @@
 reloj = 10
 while reloj > 0:
-    print (reloj)
+    print(f"n",reloj)
     reloj -= 1
     if reloj == 0:
         print ("despegue")
---
+++ desafio55/22.py
@@ -0,0 +1,8 @@
+alcancia = 0
+meses = 0
+ahorro = 80000 
+consola = 450000
+while alcancia <= consola:
+    alcancia += ahorro
+    meses += 1
+print (meses)
