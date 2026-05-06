48cdca1dee15b7102dacd2f132aadfaf01215ad8
axel-trujillo <axeltrujillo1110@gmail.com>
2026-05-06 00:08:44 -0400

    Resuelto ejercicio 19: Matchmaking por ELO (Juegos)

---
+++ desafio55/19.py
@@ -0,0 +1,9 @@
+tus_puntos = 1500
+sus_puntos = 1570 
+diferencia = abs(tus_puntos - sus_puntos)
+if diferencia <= 50:
+    print("Partida Perfecta")
+elif diferencia <= 100:
+    print("Partida Justa")
+else:
+    print("Buscando otro rival...")
