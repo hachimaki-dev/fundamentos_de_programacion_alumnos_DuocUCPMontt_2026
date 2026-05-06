66509bbd385e711d35b3b0599922f3d6e6a8c98e
Esteban2006 <olaguierpro@gmail.com>
2026-05-06 20:09:29 +0000

    Veinticinco vamoooooooooo

---
+++ desafio55/25.py
@@ -0,0 +1,7 @@
+mensajes = ["Hola", "noob", "genial", "manco"]
+
+for i in mensajes:
+    if i != "Hola" and "genial":
+        mensajes[1] = "CENSURADO"
+        mensajes[3] = "CENSURADO"
+print(mensajes)
