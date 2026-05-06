f29a0851bd0ffec77dd22e010d785254d5bf86de
RubiZero <abcmudkip@gmail.com>
2026-05-06 16:30:26 -0400

    actualizacion clase 06-05 v1

--- Desafios55_Conner/20.py
+++ Desafios55_Conner/20.py
@@ -3,9 +3,9 @@
 
 #Datos Iniciales: Tienes 50000 de saldo. Tu límite máximo diario para sacar plata es 200000. Quieres retirar 60000.
 
-#Reglas de Negocio: Regla 1: Si lo que pides es más que tu límite diario, rechaza con 'Excede límite diario'.
-#Regla 2: Si pides más de lo que tienes en el saldo, rechaza con 'Saldo insuficiente'.
-#Regla 3: Si lo que pides no es múltiplo de 5000 (el cajero no da monedas), rechaza con 'Monto inválido'.
+#Reglas de Negocio: Regla 1: Si lo que pides es más que tu límite diario, rechaza con 'Excede límite diario'.    LISTO
+#Regla 2: Si pides más de lo que tienes en el saldo, rechaza con 'Saldo insuficiente'.                           FALTA
+#Regla 3: Si lo que pides no es múltiplo de 5000 (el cajero no da monedas), rechaza con 'Monto inválido'.        FALTA
 #Si pasas todas las reglas, 'Retiro exitoso'.
 
 #Restricciones: Usa un bloque `if / elif / elif / else` para revisar regla por regla. Apenas falle una, debe imprimir el error y detenerse.
@@ -14,6 +14,13 @@
 saldo=50000
 limite_diario=200000
 retiro=60000
-if retiro > limite_diario:
+if retiro>limite_diario:
     print('Excede límite diario')
-    
+elif retiro>saldo:
+    print("Saldo insuficiente")
+elif retiro%5000!=0:
+    print("Monto invalido")
+else:
+    saldo=saldo-retiro
+    print(saldo)
+    print("Retiro exitoso")
