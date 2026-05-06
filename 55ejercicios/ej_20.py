526c1b6eec5524f3b53499a12396729a306651d2
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-06 09:25:46 -0400

    Ejercicio 20: Simulador de Cajero: Retiro Complejo

---
+++ 55ejercicios/ej_20.py
@@ -0,0 +1,19 @@
+# Ejercicio 20: Simulador de Cajero: Retiro Complejo
+
+print("Bienvenido al cajero automático")
+
+saldo = 50000
+limite_diario = 200000
+retiro = 60000
+
+if retiro > limite_diario:
+    print("Excede límite diario")
+
+elif retiro > saldo:
+    print("Saldo insuficiente")
+
+elif retiro % 5000 != 0:
+    print("Monto inválido")
+
+else:
+    print("Retiro exitoso")
