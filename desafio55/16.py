951ee98136b7e542fa235b42802719627dee037b
Esteban2006 <olaguierpro@gmail.com>
2026-05-06 07:52:13 -0600

    Update 16.py

--- desafio55/16.py
+++ desafio55/16.py
@@ -1,13 +1,17 @@
-Postpago = 18
+Postpago_gigas = 18
+Cliente = "Postpago"
+Cliente_1 = "Prepago"
 Giga_extra = 1000
-Prepago = 18
+Prepago_gigas = 18
 Total_postpago = 0
 Prepago_corte = ""
-if Postpago > 15:
-  Total_postpago = Giga_extra * 3
-  print(Total_postpago)
-  if Prepago > 15:
-    Prepago_corte = "Sin saldo"
-    print(Prepago_corte)
+if Cliente == Cliente:
+  if Postpago_gigas > 15:
+    Total_postpago = Giga_extra * 3
+    print(Total_postpago)
+    if Cliente_1 == Cliente_1:
+      if Prepago_gigas > 15:
+        Prepago_corte = "Sin saldo"
+        print(Prepago_corte)
   
   
