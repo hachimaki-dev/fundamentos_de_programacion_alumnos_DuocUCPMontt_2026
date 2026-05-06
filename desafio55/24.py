fb6fd2ca7d4976dc553aa2ab31ae29096d85d06b
Alvas-cod3 <alvargonz507@gmail.com>
2026-05-06 15:56:55 -0400

    Resuelto Ejercicio 24: Generador de Nombres (Instagram)

---
+++ desafio55/24.py
@@ -0,0 +1,9 @@
+base_nombre = "user_"
+contador_nombre = 1
+nombre = ""
+for i in base_nombre:
+    base_nombre = base_nombre + str(contador_nombre)
+    print(base_nombre)
+    base_nombre = "user_"
+    contador_nombre = contador_nombre + 1
+   
