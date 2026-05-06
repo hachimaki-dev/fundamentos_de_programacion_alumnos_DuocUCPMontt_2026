8a73cf0d089e178ec440c63845e701224d6d5020
rominadanaee <rominacocio08@gmail.com>
2026-05-06 02:09:52 -0400

    Resuelto Ejercicio 10: Estacionamiento Aeropuerto

---
+++ 10.py
@@ -0,0 +1,10 @@
+primera_hora = 1200
+valor_cada_media_hora = 500
+horas_estacionado = 3
+horas_restantes = 4
+
+costo_bloques_extras = valor_cada_media_hora * horas_restantes
+total = costo_bloques_extras + primera_hora
+
+print(total)
+
