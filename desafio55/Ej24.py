a5d2d22328a9faad42046abb4225786e6179ce7e
LeCoward <sebabarri2710@gmail.com>
2026-05-06 11:01:59 -0400

    ej24

---
+++ desafio55/Ej24.py
@@ -0,0 +1,18 @@
+#Ejercicio : Ejercicio 24: Generador de Nombres (Instagram)
+#MEDIUM
+#Vas a automatizar la creación de perfiles falsos para hacer pruebas.[cite: 2]
+
+#Datos Iniciales: La palabra base de todos los nombres será 'user_'.[cite: 2]
+
+#Reglas de Negocio: Necesitas generar exactamente 5 usuarios, numerados del 1 al 5. (ejemplo: user_1, user_2, etc).[cite: 2]
+
+#Restricciones: Usa un ciclo for con la herramienta range(). Recuerda que en Python, el range se detiene un número ANTES del final, así que ajusta el límite para que llegue a 5. Imprime los nombres generados en cada vuelta.[cite: 2]
+
+base_usuario = "user_"
+numero_usuario = 0
+for usuarios in base_usuario :
+    base_usuario = "user_"
+    numero_usuario = numero_usuario + 1
+    base_usuario = base_usuario + str(numero_usuario)
+    print(base_usuario)
+
