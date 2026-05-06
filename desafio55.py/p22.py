4560e9ec0fc7bbb7f0a64dcd778e40a318d2610d
Manueldbz <manuelbustos36611@gmail.com>
2026-05-06 16:13:35 -0400

    Resuelto Ejercicio 23: Bloqueo de Seguridad (Cajero)

---
+++ desafio55.py/p22.py
@@ -0,0 +1,13 @@
+alcancia = 0
+ahorro_por_mes = 80000
+valor_consola = 450000
+meses_ahorrando = 0
+
+#Se incluye el ahorro mensual a la alcancía
+
+while alcancia < valor_consola:
+    alcancia = alcancia + ahorro_por_mes
+    meses_ahorrando = meses_ahorrando + 1
+
+print(meses_ahorrando)
+
