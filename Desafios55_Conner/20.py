fc6c2c44576d42ab6f6b503f59883e64944f750f
RubiZero <abcmudkip@gmail.com>
2026-05-06 15:52:56 -0400

    actualizacion

---
+++ Desafios55_Conner/20.py
@@ -0,0 +1,19 @@
+#Ejercicio 20: Simulador de Cajero: Retiro Complejo
+#El Mini-Boss: Haz que el cajero automático valide 3 reglas antes de dar dinero.
+
+#Datos Iniciales: Tienes 50000 de saldo. Tu límite máximo diario para sacar plata es 200000. Quieres retirar 60000.
+
+#Reglas de Negocio: Regla 1: Si lo que pides es más que tu límite diario, rechaza con 'Excede límite diario'.
+#Regla 2: Si pides más de lo que tienes en el saldo, rechaza con 'Saldo insuficiente'.
+#Regla 3: Si lo que pides no es múltiplo de 5000 (el cajero no da monedas), rechaza con 'Monto inválido'.
+#Si pasas todas las reglas, 'Retiro exitoso'.
+
+#Restricciones: Usa un bloque `if / elif / elif / else` para revisar regla por regla. Apenas falle una, debe imprimir el error y detenerse.
+#Imprime el error de este caso.
+
+saldo=50000
+limite_diario=200000
+retiro=60000
+if retiro > limite_diario:
+    print('Excede límite diario')
+    
