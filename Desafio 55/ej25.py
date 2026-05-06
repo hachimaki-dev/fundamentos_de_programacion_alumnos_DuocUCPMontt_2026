ad83469be4e284ff79791c47ac9b75dc23e614c3
Zobeiba Henriquez <henriquezzobeiba@gmail.com>
2026-05-06 17:42:35 -0400

    Resuelto Ejercicio 25: Filtro de Palabras (Discord)
    Implement a chat moderation bot that censors specific words.

---
+++ Desafio 55/ej25.py
@@ -0,0 +1,16 @@
+#Ejercicio 25: Filtro de Palabras (Discord)
+#Crea un robot moderador de chat que censure insultos.
+
+#Datos Iniciales: Tienes una lista de mensajes en el chat: `['hola', 'noob', 'genial', 'manco']`.
+
+#Reglas de Negocio: El bot revisa las palabras una por una. Las palabras 'noob' y 'manco' están prohibidas. Si el bot ve una de esas palabras, debe cambiarla por el texto '[CENSURADO]'. Si la palabra es buena, la deja igual.
+
+#Restricciones: Recorre la lista con un `for`. Adentro, usa un `if` para revisar si la palabra es mala e imprimir la censura, sino, imprime la palabra original.
+
+palabras_baneadas = ['hola', 'noob', 'genial', 'manco']
+
+for palabra in palabras_baneadas:
+    if palabra == 'noob' or palabra == 'manco':
+        print("[CENSURADO]")
+    else:
+        print(palabra)
