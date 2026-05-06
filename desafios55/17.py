2e95b47452fffda2d5532a219d83fad850c9e57a
jp-sanchez6 <megajuanx@gmail.com>
2026-05-05 23:47:28 -0400

    ejercicio 17 compeltado

---
+++ desafios55/17.py
@@ -0,0 +1,14 @@
+vehiculo = 'Camion'
+velocidad = 95
+
+if vehiculo == 'Auto' and velocidad > 120:
+    
+    print('Multa Gravísima')
+    
+elif vehiculo == 'Camion' and velocidad > 80:
+    
+    print('Multa Grave Camión')
+
+else:
+    
+    print('Sin multa')
