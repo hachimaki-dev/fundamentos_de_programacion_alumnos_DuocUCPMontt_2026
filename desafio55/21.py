123c9b7f149c6e3999aaf48c31e6de14cb6405e3
bisente777 <Vic.aponte@duocuc.cl>
2026-05-06 19:32:02 +0000

    ejercicio 21

---
+++ desafio55/21.py
@@ -0,0 +1,6 @@
+conometro = 10
+while conometro > 0:
+    print(conometro)
+    conometro = conometro - 1
+if conometro == 0:
+    print("despegar")
---
+++ desafio55/22.py
@@ -0,0 +1,4 @@
+alcancia = 0
+ahorro_mensual = 80000
+consola = 450000
+while alcancia > 450000
