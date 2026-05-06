c30f631e620ddbcd0a169c65834fb9d03c4aef76
Nacho27 <ignacionacho2000x@gmail.com>
2026-05-05 23:12:52 -0400

    Resuelto Ejercicio 10: Estacionamiento Aeropuerto

---
+++ game/desafio55_diego/10.py
@@ -0,0 +1,9 @@
+primera_hora = 1200
+costo_media_hora = 500
+horas_totales = 3
+
+bloques_extras = (horas_totales - 1) *2
+
+costo_total = primera_hora + (bloques_extras * costo_media_hora)
+
+print(costo_total)
