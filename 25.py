70fefe0cad69b46899ac9e7d77623f6054e54470
Joaox342 <joaoburgox15@gmail.com>
2026-05-06 02:25:22 -0300

    Resuelto Ejercicio 25: Filtro de Palabras (Discord)

---
+++ 25.py
@@ -0,0 +1,9 @@
+mensajes = ["hola", "noob", "genial", "manco"]
+
+for palabra in mensajes:
+    if palabra in ["noob", "manco"]:
+        print("[censurado]")
+    
+    else:
+        print(palabra)
+    
