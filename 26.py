8fe64a71a5204407841345f6fc79fed643bf390d
Joaox342 <joaoburgox15@gmail.com>
2026-05-06 02:30:58 -0300

    Resuelto Ejercicio 27: Saltar Errores del Sistema (Kiosko)

---
+++ 26.py
@@ -0,0 +1,9 @@
+hojas = 5
+cola = ["TEXTO", "FOTO", "TEXTO", "FOTO"]
+
+for documento in cola:
+    if documento == "TEXTO":
+        hojas_necesarias = 1
+    elif documento == "FOTO":
+        hojas_necesarias = 3
+        
