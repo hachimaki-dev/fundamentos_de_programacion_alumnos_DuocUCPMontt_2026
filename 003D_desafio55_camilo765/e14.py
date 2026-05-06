524d97d815bf6df41178e19212d1bf55d426dacd
camilo765 <camiloogutierrez21@gmail.com>
2026-05-06 16:16:10 -0400

    Resuelto Ejercicio 14: Sistema de Grados con Bonificación

---
+++ 003D_desafio55_camilo765/e14.py
@@ -0,0 +1,11 @@
+nota = 3.6
+asistencia = 85
+
+if asistencia >= 80:
+    nota += 0.5
+if nota >= 4.0:
+    print("Aprobado")
+elif 3.5 <= nota 4.0:
+    print("Examen")
+else:
+    print("Reprobado")
