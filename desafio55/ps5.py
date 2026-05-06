8be30c655714cff9cdf2bf3f20968bb1e6c9d4d3
fe-macias <fernando.macias19@icloud.comgit config --global user.name fernando.macias19@icloud.com>
2026-05-05 21:53:45 -0400

    ejercicio 22

--- desafio55/ps5.py
+++ desafio55/ps5.py
@@ -1,7 +1,14 @@
+#Simula tu chanchito de ahorros para saber cuántos meses te faltan para comprar una consola.
+
+#Datos Iniciales: Tu alcancía parte en 0. Ahorras religiosamente 80000 todos los meses. La consola cuesta 450000.
+
+#Reglas de Negocio: Cada mes que pasa, tú echas plata a la alcancía. Tienes que ir sumando hasta que tu ahorro iguale o supere el precio de la consola.
+
+#Restricciones: Usa un ciclo `while`. Aumenta tu ahorro y suma 1 a tu contador de meses en cada vuelta. Imprime solo cuántos meses te tomó llegar a la meta.
+
 meses = 0 
 alcancia = 0 
 ahorro_meses = 80000
-ps5 = 450000
 while True:
     meses = meses + 1
     alcancia = alcancia + ahorro_meses
