8fee0198b028f4a33a47b57b48c714c01f9691b5
maximobv <barrigamaximo845@gmail.com>
2026-05-06 20:09:35 +0000

    Resuelto Ejercicio 25: Filtro de Palabras (Discord)

--- desafio55/25.py
+++ desafio55/25.py
@@ -1,4 +1,11 @@
 
 mensajes = ["hola","noob","genial","manco"]
+for i in  mensajes:
+    if i == "noob" and "manco":
+        mensajes[1] = "--censurado--"
+        mensajes[3] = "--censurado--"
+
+print(mensajes)
+    
 
 
