f1d3b4163bd158a34e81e9b2786818e79d577c93
moenita <davidmoena2@gmail.com>
2026-05-05 22:16:04 -0400

    18Vo desafio resuelto!

---
+++ desafio55/18.py
@@ -0,0 +1,8 @@
+hora= 3
+movimiento= True
+dueño_en_casa= True
+
+if movimiento == True and dueño_en_casa == False or (movimiento == True and hora < 6):
+    print("Alarma sonando")
+else:
+    print("Modo silencioso")
