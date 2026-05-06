746af560fcae30ad94e12ddb58327cd8d71af360
moenita <davidmoena2@gmail.com>
2026-05-05 20:11:54 -0400

    13Vo desafio resuelto!

---
+++ desafio55/13.py
@@ -0,0 +1,8 @@
+sueldo= 800000
+años_banco= 6
+deudas= 0
+
+if sueldo > 1000000 or (años_banco > 5 and deudas == 0):
+    print("Felicidades, haz entrado a la seccion VIP, cumples una o más condiciones")
+else:
+    print("Lo siento, no cumples las condiciones")
