044e410335e1c7e460e3e4a3e4e7cb3aa1a3a43e
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-05 20:43:40 -0400

    Resuelto Ejercicio 22: Ahorro Meta Mensual (PS5)

---
+++ desafio55/22.py
@@ -0,0 +1,14 @@
+alcancia = 0
+ahorras = 80000 
+consola = 450000
+meses = 0
+
+while True:
+    if alcancia < consola:
+        alcancia += ahorras
+        meses += 1
+    else: 
+        alcancia == consola
+        print(meses)
+        break
+
