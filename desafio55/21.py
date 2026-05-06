a5901951338c7f33754b1bceda080287fb8e27dc
TomandoSoylent <therealdiegoxd@gmail.com>
2026-05-06 10:28:42 -0400

    Resuelto Ejercicio 21: Cuenta Regresiva Básica (SpaceX)

---
+++ desafio55/21.py
@@ -0,0 +1,6 @@
+contador = 10
+for i in range(0, 10):
+    print(contador)
+    contador -= 1
+if contador == 0:
+    print("Despegue!")
---
+++ desafio55/22.py
@@ -0,0 +1,10 @@
+alcancia = 0
+ahorro_mensual = 80000 
+precio_consola = 450000
+contador_de_meses = 0
+
+while alcancia <= precio_consola:
+    alcancia = alcancia + ahorro_mensual
+    contador_de_meses += 1
+    if alcancia >= precio_consola:
+        print(contador_de_meses)
