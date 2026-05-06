2cf7ede5d4a5c020690ade79c664820e1c402b3d
MatiasValdivia327 <matias.felipe7777>
2026-05-06 10:48:54 -0400

    Resuelto Ejercicio 22: Ahorro Meta Mensual (PS5)

---
+++ desafio55/ps5.py
@@ -0,0 +1,18 @@
+
+alcancia = 0
+mes = 80000
+consola = 450000
+
+contador_numero_mes = 1
+
+while alcancia < consola:
+    alcancia += mes
+    
+    print(f"el valor de la consola es {consola} y llevo ahorrado {alcancia}")
+    print(contador_numero_mes)
+    contador_numero_mes += 1
+
+
+
+
+
