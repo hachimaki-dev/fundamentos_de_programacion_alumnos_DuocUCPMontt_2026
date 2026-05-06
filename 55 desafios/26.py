d13e39f09866b0769fe8dbaec830a63300634515
nanyasg <nany.asg10.com>
2026-05-06 13:11:08 -0400

    ejercicio 26

---
+++ 55 desafios/26.py
@@ -0,0 +1,12 @@
+hojas_disponibles = 5
+cola_de_impresion = ['TEXTO', 'FOTO', 'TEXTO', 'FOTO']
+
+for documento in cola_de_impresion:
+    if documento == "TEXTO" and hojas_disponibles >= 1:
+        hojas_disponibles -= 1
+        print("Imprimiendo texto")
+    elif documento == "FOTO" and hojas_disponibles >= 3:
+        hojas_disponibles -= 3
+        print("Imprimiendo foto")
+    else:
+        print("Sin papel")
