043266ff48d284d55a3f88c2e68dc359ebca8408
bisente777 <Vic.aponte@duocuc.cl>
2026-05-06 19:35:41 +0000

    ps5

--- desafio55/22.py
+++ desafio55/22.py
@@ -1,4 +1,8 @@
 alcancia = 0
 ahorro_mensual = 80000
+meses = 0
 consola = 450000
-while alcancia > 450000
+while alcancia  < consola:
+    alcancia = alcancia + ahorro_mensual
+    meses = meses + 1
+print(meses)
