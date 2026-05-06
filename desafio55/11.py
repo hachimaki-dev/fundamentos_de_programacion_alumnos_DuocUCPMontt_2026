5a3691b4edce3facbc55b938cc7a978e5424a8e9
Shiekd-L <ordonezbenja22@gmail.com>
2026-05-05 20:15:51 -0400

    Resuelto Ejercicio 11: Clave Única Básico

--- desafio55/11.py
+++ desafio55/11.py
@@ -1,4 +1,10 @@
 intentos_falidos = 0
 clave = "admin123"
 introducir_contra = input("introdusca contraseña")
-if introducir_contra == clave
+if introducir_contra == clave:
+    print("entraste")
+else:
+    intentos_falidos += 1
+    print("intentelo de nuevo")
+    print("intentos fallidos")
+    print(intentos_falidos)
