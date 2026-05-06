6bf98e0c7a0197c7c38c0f71d59dd43dfff3bd40
AphidCiro <navarrogamboajulian@gmail.com>
2026-05-06 09:37:42 -0400

    Resuelto Ejercicio 27: Saltar Errores del Sistema (Kiosko)

--- Desafio55/27.py
+++ Desafio55/27.py
@@ -2,3 +2,10 @@
 #Datos Iniciales: Las ventas del día son: `[1500, -200, 5000, 0, 100]`. Tienes una variable total en 0.
 #Reglas de Negocio: Evidentemente, una venta no puede ser menor o igual a 0. Esos son errores del sistema. Necesitas sumar todas las ventas buenas y simplemente IGNORAR las malas, sin detener el ciclo.
 #Restricciones: Adentro de tu `for`, pon un `if` que detecte las ventas falsas y use la palabra mágica `continue` para saltárselas y pasar directo al siguiente número. Imprime la suma total limpia al final.
+ventas_dia = [1500, -200, 5000, 0 ,100]
+sumas_dia = 0
+for i in ventas_dia:
+    if i <= 0 :
+        continue
+    sumas_dia += i
+print(sumas_dia)
