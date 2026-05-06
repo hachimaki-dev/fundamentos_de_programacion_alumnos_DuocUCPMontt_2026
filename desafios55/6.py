97a5ae1d96fbf5e99834947549c138850a4c35be
AustralMoth <siderquiroz12@gmail.com>
2026-05-05 23:05:17 -0400

    e

--- desafios55/6.py
+++
@@ -1,9 +0,0 @@
-billetera_de_btc = 0.05
-dia_de_compra_btc = 50000
-hoy_en_dia_btc = 62000
-dolar = 900
-
-diferencia = hoy_en_dia_btc - dia_de_compra_btc
-ganancia = diferencia * billetera_de_btc
-ganancia_en_dolares = ganancia *dolar
-print(f"ganancia en pesos es {ganancia_en_dolares}")
