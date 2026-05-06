5df6b72096bfb2735bd33e75be22d989e70c6efd
Crstxn1 <cmancillat9@gmail.com>
2026-05-06 10:59:54 -0400

    Resuelto Ejercicio 22: Ahorro Meta Mensual (PS5)

---
+++ desafio 55/22.py
@@ -0,0 +1,10 @@
+alcancia = 0
+valor_consola = 450000
+ahorro_mensual = 80000
+contador_meses = 0
+
+while alcancia < valor_consola:
+    alcancia = alcancia + ahorro_mensual
+    contador_meses += 1
+
+print(contador_meses)
