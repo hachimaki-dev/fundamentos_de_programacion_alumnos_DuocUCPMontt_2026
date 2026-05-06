934e577930dc326492982b07e19ed4de725bb265
Joaox342 <joaoburgox15@gmail.com>
2026-05-06 01:31:23 -0300

    Resuelto Ejercicio 14: Sistema de Grados con Bonificación

---
+++ 14.py
@@ -0,0 +1,16 @@
+nota = 3.6
+asistencia = 85
+
+if asistencia >= 80:
+    nota = nota + 0.5
+
+if nota >= 4.0:
+    print("aprobado")
+
+elif nota >= 3.5:
+    print("examen")
+
+else:
+    print("reprobado")
+
+    
