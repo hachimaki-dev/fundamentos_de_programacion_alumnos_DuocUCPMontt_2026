595b13b93c1395c5fbcdab388322a508d9fdddd3
Shiekd-L <ordonezbenja22@gmail.com>
2026-05-05 21:15:48 -0400

    Resuelto Ejercicio 15: Envíos MercadoLibre (Zonas Extremas)

---
+++ desafio55/15.py
@@ -0,0 +1,7 @@
+cliente_ganancias = 800000
+años_en_banco = 6
+deudas = 0
+if cliente_ganancias >= 1000000 or años_en_banco >= 5 and deudas == 0:
+    print("cliente VIP")
+else:
+    print("cliente normal")
