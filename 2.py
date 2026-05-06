78849fdd214e0c5525e379aaad9b085399f2cb77
rominadanaee <rominacocio08@gmail.com>
2026-05-05 23:50:25 -0400

    Resuelto Ejercicio 1: Liquidación de Sueldo Básico

---
+++ 2.py
@@ -0,0 +1,8 @@
+costo_auto = 5000000
+ahorro_actual = 1500000
+ahorro_mensual = 150000
+
+dinero_faltante = costo_auto - ahorro_actual
+meses_necesarios = dinero_faltante // ahorro_mensual
+
+print(meses_necesarios)
