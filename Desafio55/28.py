30759b19f6ceb27839509a8f908009da3c3e4d62
AphidCiro <navarrogamboajulian@gmail.com>
2026-05-06 09:41:45 -0400

    Resuelto Ejercicio 28: Búsqueda del Sospechoso (Cámaras)

---
+++ Desafio55/28.py
@@ -0,0 +1,10 @@
+#La policía está buscando un auto robado en la lista de cámaras del peaje.
+#Datos Iniciales: Las patentes que pasaron son: `['XY11', 'ZZ99', 'AB12', 'XX00']`. La policía busca la patente 'AB12'.
+#Reglas de Negocio: El programa revisa patente por patente y va avisando en pantalla qué patente está mirando ('Buscando en XY11...'). Si encuentra la patente robada, dice 'Sospechoso encontrado' y DEJA DE BUSCAR (no tiene sentido mirar los autos que pasaron después).
+#Restricciones: Usa un ciclo `for`. Apenas encuentres el auto, usa `break` para apagar la búsqueda. Imprime todo paso a paso.
+patentes = ["XY11", "ZZ99", "AB12", "XX00"]
+for i in patentes:
+    print(f"buscando en {i}")
+    if i == "AB12":
+        print("Sospechoso Encontrado")
+        break
