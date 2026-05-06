5ab9f1e809a58eac3f69061cdd6c84239dcf903f
Esteban2006 <olaguierpro@gmail.com>
2026-05-06 07:38:24 -0600

    Update 15.py

---
+++ desafio55/15.py
@@ -0,0 +1,9 @@
+productos = 25000
+ubicacion = "Magallanes"
+envio_normal = 3000
+if productos > 20000 and ubicacion == "Magallanes":
+  productos = 0
+  productos = productos + 2000
+else:
+  print(envio_normal)
+  
