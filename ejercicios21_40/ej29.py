6582df7bfdd1a6841cd15b541d4eb93e0f40facc
benjamin476 <alexander.10benjamin@gmail.com>
2026-05-06 19:11:11 -0400

    Resuelto Ejercicio 29: Simulación de Trote (Apple Watch)

---
+++ ejercicios21_40/ej29.py
@@ -0,0 +1,12 @@
+meta_a_quemar = 300
+quemado = 0
+minuto = 0
+while quemado < meta_a_quemar:
+    minuto += 1 
+    if minuto <= 10:
+        quemado += 20
+    else:
+        minuto > 10
+        quemado += 10
+print(minuto)
+
