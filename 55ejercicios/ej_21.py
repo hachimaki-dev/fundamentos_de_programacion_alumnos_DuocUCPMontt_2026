773ce28968f07be17dd18347f342ba3903467b4d
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-06 10:24:14 -0400

    Resuelto Ejercicio 21: Cuenta Regresiva Básica (SpaceX)

---
+++ 55ejercicios/ej_21.py
@@ -0,0 +1,13 @@
+# Ejercicio 21: Cuenta Regresiva Básica (SpaceX)
+
+print("==========================")
+print("Inicio de cuenta Regresiva")
+print("==========================")
+
+cronometro = 10
+
+while cronometro > 0:
+    print(cronometro)
+    cronometro = cronometro - 1
+
+print("Despegue 🚀")
