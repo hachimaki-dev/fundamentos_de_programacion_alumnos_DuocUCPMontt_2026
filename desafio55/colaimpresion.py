76631c9557365dfaf4fa3702f62f89a594c11016
fe-macias <fernando.macias19@icloud.comgit config --global user.name fernando.macias19@icloud.com>
2026-05-06 10:18:11 -0400

    ejercicio 26

---
+++ desafio55/colaimpresion.py
@@ -0,0 +1,15 @@
+#Controla cuántas hojas le quedan a la impresora antes de imprimir un documento.
+
+#Datos Iniciales: A la impresora le quedan 5 hojas. Mandaste a imprimir esto: `['TEXTO', 'FOTO', 'TEXTO', 'FOTO']`.
+
+#Reglas de Negocio: Imprimir un 'TEXTO' gasta 1 hoja. Imprimir una 'FOTO' gasta 3 hojas. Antes de imprimir cada cosa, la impresora revisa si tiene hojas suficientes. Si se queda sin hojas, cancela todo y tira el mensaje 'Sin papel'.
+
+#Restricciones: Resta las hojas que vas usando. Usa `break` para abortar la impresión si las hojas que te quedan son menos de las que necesitas. Imprime 'Imprimiendo [documento]' cuando sí alcance.
+impresora = ["TEXTO" , "FOTO" , "TEXTO" , "FOTO"]
+for hojas in impresora:
+    if hojas == "FOTO":
+        print("impresora gasto 5 hojas")
+        print("se acabaron las hojas!")
+        break
+    elif hojas == "TEXTO":
+        print("impresora gasto 2 hojas")
