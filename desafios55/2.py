a16221a6194cafa8d60666a700000c25917ad422
AustralMoth <siderquiroz12@gmail.com>
2026-05-05 23:08:50 -0400

    Resuelto Ejercicio 2: Proyección de Ahorro para un Auto

---
+++ desafios55/2.py
@@ -0,0 +1,10 @@
+#Trabajo en progreso
+
+valor_auto = 5000000
+total_ahorrado = 1500000
+ahorro_x_mes = 150000
+
+while total_ahorrado < 5000000:
+    total_ahorrado = total_ahorrado + ahorro_x_mes
+else:
+    print (total_ahorrado)
