b96b42c67af9b84b9c4e61e7bd4bc11a072db00e
Martin Muñoz <martin.andres.munoz.c@gmail.com>
2026-05-05 22:12:31 -0400

    Resuelto Ejercicio 35: Filtrar por Umbral (Banco)

---
+++ Desafio55/35.py
@@ -0,0 +1,9 @@
+transferencias = [15000, 80000, 2000, 150000]
+
+importantes = []
+
+for transferencia in transferencias:
+    if transferencia > 50000:
+        importantes.append(transferencia)
+
+print(importantes)
