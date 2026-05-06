b8d455e989ec97242a75d3cb1e82f1bf7cc21c75
Amaro-07 <amaro.loos2007gmail.com>
2026-05-06 10:25:57 -0400

    Amaro-Respuesta EJ20: Simulador de Cajero

--- amaro_ej_20.py
+++ amaro_ej_20.py
@@ -1,1 +1,12 @@
-saldo =
+saldo = 50000
+limite_dias = 200000
+retiro = 60000
+
+if retiro > limite_dias:
+    print("Excede limite diario")
+elif retiro > saldo:
+    print("Saldo insuficiente")
+elif retiro % 5000 != 0:
+    print("Monto inválido")
+else:
+    print("Retiro exitoso")
