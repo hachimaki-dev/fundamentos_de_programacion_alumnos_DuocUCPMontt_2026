0c1e70cb450512269bcb8c522f9ca26e0c393c65
Nacho27 <ignacionacho2000x@gmail.com>
2026-05-06 00:47:41 -0400

    Resuelto Ejercicio 20: Simulador de Cajero: Retiro Complejo

---
+++ game/desafio55_diego/20.py
@@ -0,0 +1,13 @@
+saldo = 50000
+limite_diario = 200000
+retiro = 60000
+
+if retiro > limite_diario:
+    print("exelente limite diario")
+elif retiro > saldo:
+    print("Saldo insuficiente")
+elif retiro % 5000 != 0:
+    print("Monto invalido")
+else:
+    print("Retiro exitoso")
+    
