20696f965f332e11ca4ac6dd5243fa9a5c024472
Markito33 <markitoaguilar080@gmail.com>
2026-05-05 21:46:03 -0400

    Resuelto Ejercicio 13: Promoción VIP BancoEstado (OR y AND)

---
+++ desafio55/13.py
@@ -0,0 +1,8 @@
+sueldo = 800000
+
+tiempo = 6
+
+deudas = 0
+
+if sueldo > 1000000 or (tiempo > 5 and deudas == 0):
+    print("Cliente VIP")
---
+++ desafio55/14.py
@@ -0,0 +1,13 @@
+nota = 3.6
+
+porcentaje_asistencia = 85
+
+if porcentaje_asistencia > 80:
+    nota = nota + 0.5
+
+if nota >= 4.0:
+    print("Aprobado")
+elif nota > 3.5 and nota < 3.9:
+    print("Examen")
+else:
+    print("Reprobado")
