f2f16de1ad9762196e571b20e309d17cd2f000ac
fe-macias <fernando.macias19@icloud.comgit config --global user.name fernando.macias19@icloud.com>
2026-05-06 10:32:27 -0400

    ejercicio 28

---
+++ desafio55/busquedasospechoso.py
@@ -0,0 +1,6 @@
+patentes = ["XY11" , "ZZ99" , "AB12" , "XX00"]
+for busquedapatente in patentes:
+    print(f"buscando a sospechoso..{busquedapatente}")
+    if busquedapatente == "AB12":
+        print("sospechoso encontrado AB12")
+        break
