be92c9743e4edd6a0df86b6c663aeee10455fdce
diego0954-alt <diego0954@icloud.com>
2026-05-06 16:13:15 -0400

    Bloqueo de Seguridad (Cajero)

---
+++ desafio 55/23.py
@@ -0,0 +1,9 @@
+intentos_fallidos = 0
+
+while True: 
+    intentos_fallidos < 3
+    print("Intento fallido")
+    intentos_fallidos += 1
+    if intentos_fallidos == 3:
+        print("Bloqueo de tarjeta")
+        break
