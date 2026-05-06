2888d824e7742e68405b1725676e3d7995954d42
Evelin-Vidal <evelynpatriciavidalbustamante@gmail.com>
2026-05-06 16:00:14 -0400

    Resuelto Ejercicio 37: Multiplicador de Exp In-Place (RPG)

---
+++ desafio55/37.py
@@ -0,0 +1,12 @@
+#Aplica un evento de 'Doble Experiencia' a los puntajes de un jugador.
+#Datos Iniciales: El jugador ganó esta experiencia en sus partidas: `[100, 200, 300]`.
+#Reglas de Negocio: Es fin de semana de doble EXP. Tienes que tomar la lista original y multiplicar cada uno de sus números por 2, sobreescribiendo los valores antiguos.
+#Restricciones: Para cambiar un número directamente dentro de una lista, necesitas saber su 'dirección' (su índice). 
+#Usa un `for` combinado con `range(len(lista))` para recorrer las posiciones. Sobreescribe multiplicando por 2 e imprime la lista modificada.
+partidas = [100, 200, 300]
+for i in range(len(partidas)):
+    partidas[i] = partidas[i] * 2
+
+
+
+print(partidas)
