51971a24b3c3fbda3c7b2f577f94c75de2fc7d6e
jp-sanchez6 <megajuanx@gmail.com>
2026-05-06 09:06:49 -0400

    ejercicio 22 completado

---
+++ desafios55/22.py
@@ -0,0 +1,12 @@
+alcancia = 0
+ahorro_mensual = 80000
+consola_precio = 450000
+meses = 0
+
+while alcancia < consola_precio:
+    alcancia += ahorro_mensual
+    meses += 1
+    
+print(meses)
+    
+    
