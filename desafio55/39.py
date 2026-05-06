0afe3c2a282e9c229070a2219ec677f92add135d
Evelin-Vidal <evelynpatriciavidalbustamante@gmail.com>
2026-05-06 16:13:52 -0400

    Resuelto Ejercicio 39: Limpieza de Formulario (Data Science)

---
+++ desafio55/39.py
@@ -0,0 +1,15 @@
+#Limpia los datos sucios que dejó la gente al llenar mal un formulario.
+#Datos Iniciales: Recibiste estas respuestas: `['Juan', None, '', 'Ana', ' ']`.
+#Reglas de Negocio: Solo sirven los nombres reales. Los espacios en blanco (' '), 
+#las respuestas vacías ('') o los datos nulos (`None`) se consideran basura y hay que botarlos. Solo guarda los nombres válidos en una lista nueva.
+#Restricciones: Recorre las respuestas. Usa `is not None` para asegurarte de que no sea nulo. 
+#Luego usa `.strip()` para borrarle los espacios extra y asegúrate de que no quede vacío `!= ''`. Si pasa las pruebas, guárdalo. Imprime la lista limpia.
+#Pista de Ayuda:
+#Usa if elem is not None and elem.strip() != '':
+respuestas = ["Juan", None, "", "Ana", " "]
+lista_nueva = []
+for nombre in respuestas:
+    if nombre is not None and nombre.strip() != "":
+        lista_nueva.append(nombre.strip())
+
+print(lista_nueva)
