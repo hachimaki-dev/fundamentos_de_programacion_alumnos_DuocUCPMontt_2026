0d3a27a72f35e0403ed8eb5d0021d772719aae99
amarooig <ama.gonzaleza@duocuc.cl>
2026-05-06 00:41:26 -0400

    Resuelto Ejercicio 26: Procesar Cola de Impresión

--- desafio55/26.py
+++ desafio55/26.py
@@ -1,18 +1,15 @@
 
 hojas_restantes = 5
 
-cola_de_imprecion = ['TEXTO', 'FOTO', 'TEXTO', 'FOTO']
+for i in ['TEXTO', 'FOTO', 'TEXTO', 'FOTO']:
+   if hojas_restantes <= 0:
+      print("no tiene mas hojas")
+      break
 
-contador = 0
-for impresion in cola_de_imprecion:
-    if impresion == 'TEXTO':
-        if hojas_restantes >= 1:
-            print(f"imprimimiendo el documento N {contador + 1}")
-            hojas_restantes -= 1
-    elif impresion == 'FOTO':
-        if hojas_restantes >= 3:
-            print(f"imprimimiendo el documento N {contador + 1}")
-            hojas_restantes -= 2
-        contador += 1
-    else:
-        print("tipo de documento no reconocido")
+        
+   print ("imprimiendo",i)
+   
+   if i == 'TEXTO':
+      hojas_restantes -= 1
+   else:
+        hojas_restantes -= 3
