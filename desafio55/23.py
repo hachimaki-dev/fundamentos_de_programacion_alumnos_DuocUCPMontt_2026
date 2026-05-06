8eda137f18298aaf9e53cca7b23f687387ee9f94
moenita <davidmoena2@gmail.com>
2026-05-06 10:20:09 -0400

    22Vo desafio resuelto!

---
+++ desafio55/23.py
@@ -0,0 +1,13 @@
+intentos_fallidos= 0
+clave_correcta= 1234
+
+while intentos_fallidos < 3:
+    clave_ingresada= int(input("Ingrese una clave: "))
+    if clave_ingresada == clave_correcta:
+        print("Haz entrado!")
+        break
+    else:
+        print("Clave incorrecta, intenta de nuevo")
+        intentos_fallidos += 1
+
+print("Bloqueando tarjeta")
