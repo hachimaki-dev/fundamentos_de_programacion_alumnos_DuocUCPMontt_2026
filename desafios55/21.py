22db85bfe47dd0c494ab8d36e0aec44ee11cbd7f
jp-sanchez6 <megajuanx@gmail.com>
2026-05-06 08:58:01 -0400

    ejercicio 21 completado

---
+++ desafios55/21.py
@@ -0,0 +1,23 @@
+cuenta_regresiva = 10
+
+while cuenta_regresiva > 0:
+    print(cuenta_regresiva)
+    cuenta_regresiva -= 1
+    
+    if cuenta_regresiva == 0:
+        print("despegue")
+    
+    
+    
+
+    
+
+    
+
+
+
+        
+    
+
+    
+        
