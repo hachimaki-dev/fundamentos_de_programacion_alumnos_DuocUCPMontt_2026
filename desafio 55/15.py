589fa1e986032fe8dd65028ba8aaec3caa6ef580
benjaminb80 <benjaminbusta45@gmail.com>
2026-05-06 03:19:38 +0000

    Resuelto Ejercicio 15: Envíos MercadoLibre (Zonas Extremas)

---
+++ desafio 55/15.py
@@ -0,0 +1,14 @@
+
+monto_compra = 25000
+destino = 'Magallanes'
+
+if monto_compra > 20000:
+    costo_envio = 0
+else:
+    costo_envio = 3000
+
+
+if destino == 'Magallanes' or destino == 'Aysen':
+    costo_envio += 2000
+
+print(costo_envio)
