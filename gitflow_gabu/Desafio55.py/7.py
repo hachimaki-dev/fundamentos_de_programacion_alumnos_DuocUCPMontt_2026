9b8c854d5bbe81552b2677848f6f2a9f1edc7a35
gabriela-pezo <gabrielapezo69@gmail.com>
2026-05-06 08:33:27 -0400

    Resuelto Ejercicio 7: Armando el PC Gamer

---
+++ gitflow_gabu/Desafio55.py/7.py
@@ -0,0 +1,7 @@
+procesador = 250000
+placa_madre = 120000
+tarjeta_video = 450000
+
+tarjeta_rebajada = tarjeta_video * 0.85
+total_compra = procesador + placa_madre + tarjeta_rebajada
+print("EL total de la compra es: ", total_compra)
