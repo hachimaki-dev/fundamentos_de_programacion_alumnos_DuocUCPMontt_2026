968d534e766b4813b91ba35f222c1167a15af96f
Neiram374 <neiramatias553@gmail.com>
2026-05-06 00:44:20 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ desafios55/3.py
@@ -0,0 +1,8 @@
+personas = 5
+pichangas = 2 * 15000
+shop = 5 * 3500
+total_consumo = pichangas + shop
+propina = total_consumo * 0.10
+total_final = total_consumo + propina
+pago_persona = total_final / personas
+print(pago_persona)
