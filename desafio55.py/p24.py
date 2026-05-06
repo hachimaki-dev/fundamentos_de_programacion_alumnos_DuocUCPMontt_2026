1e386a54de9e01baf868a106230a806a171755b5
Manueldbz <manuelbustos36611@gmail.com>
2026-05-06 17:52:42 -0400

    Resuelto Ejercicio 25: Filtro de Palabras (Discord)

---
+++ desafio55.py/p24.py
@@ -0,0 +1,4 @@
+palabra_base = "user_"
+
+for i in range (1, 6):
+    print(f"{palabra_base}{i}")
---
+++ desafio55.py/p25.py
@@ -0,0 +1,8 @@
+mensajes_chat = ["Hola", "Noob", "Genial", "Manco"]
+
+for cada_palabra in mensajes_chat:
+    if cada_palabra in ["Noob", "Manco"]:
+                        print("[CENSURADO]")
+    else:
+            print(cada_palabra)
+                        
