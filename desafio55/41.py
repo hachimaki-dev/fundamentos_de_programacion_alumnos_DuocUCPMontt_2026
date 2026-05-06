c77a4c59c502470f82ba4b82b039a9547d9f1eb8
Evelin-Vidal <evelynpatriciavidalbustamante@gmail.com>
2026-05-06 16:24:34 -0400

    Resuelto Ejercicio 41: Creación de Perfil Base

---
+++ desafio55/41.py
@@ -0,0 +1,11 @@
+#Crea el perfil de un jugador usando diccionarios (la mejor forma de guardar datos complejos).
+#Datos Iniciales: Vas a crear un jugador nuevo.
+#Reglas de Negocio: Un diccionario guarda datos en parejas (una 'llave' y su 'valor'). 
+#El jugador debe tener la llave `'nombre'` (pon tu nombre de valor) y la llave `'nivel'` (que parte en 1).
+#Restricciones: Escribe el diccionario a mano usando las llaves `{}`. 
+#Luego, haz que Python extraiga tu nombre accediendo a la llave `'nombre'` usando corchetes. Imprime solo tu nombre.
+diccionario = {
+    "Nombre": "Evelin",
+    "Nivel": 1 
+}
+print(diccionario["Nombre"])
