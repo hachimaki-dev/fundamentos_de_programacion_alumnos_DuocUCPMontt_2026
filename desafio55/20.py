7752fd62db1987f767ac1a77265fbaae038ee758
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-05 21:28:28 -0400

    Resuelto Ejercicio 20: Simulador de Cajero: Retiro Complejo

---
+++ desafio55/20.py
@@ -0,0 +1,28 @@
+#Ejercicio 20: Simulador de Cajero: Retiro Complejo
+#El Mini-Boss: Haz que el cajero automático valide 3 reglas antes de dar dinero.
+
+#Datos Iniciales: Tienes 50000 de saldo. Tu límite máximo diario para sacar plata es 200000. Quieres retirar 60000.
+
+#Reglas de Negocio: Regla 1: Si lo que pides es más que tu límite diario, rechaza con 'Excede límite diario'. Regla 2: Si pides más de lo que tienes en el saldo, rechaza con 'Saldo insuficiente'. Regla 3: Si lo que pides no es múltiplo de 5000 (el cajero no da monedas), rechaza con 'Monto inválido'. Si pasas todas las reglas, 'Retiro exitoso'.
+
+#Restricciones: Usa un bloque `if / elif / elif / else` para revisar regla por regla. Apenas falle una, debe imprimir el error y detenerse. Imprime el error de este caso.
+
+
+#Resultado esperado en consola:
+#Saldo insuficiente
+
+saldo = 50000
+limite_diario = 200000
+retiro_de_dinero = 60000
+
+
+if limite_diario > retiro_de_dinero:
+    print("Excede límite diario")
+
+    
+if saldo < retiro_de_dinero:
+    print("Saldo insuficiente")
+    
+if retiro_de_dinero % 5000 == 0:
+    print("Monto inválido")
+
---
+++ desafio55/22.py
@@ -0,0 +1,14 @@
+#Ejercicio 22: Ahorro Meta Mensual (PS5)
+#Simula tu chanchito de ahorros para saber cuántos meses te faltan para comprar una consola.
+
+#Datos Iniciales: Tu alcancía parte en 0. Ahorras religiosamente 80000 todos los meses. La consola cuesta 450000.
+
+#Reglas de Negocio: Cada mes que pasa, tú echas plata a la alcancía. Tienes que ir sumando hasta que tu ahorro iguale o supere el precio de la consola.
+
+#Restricciones: Usa un ciclo `while`. Aumenta tu ahorro y suma 1 a tu contador de meses en cada vuelta. Imprime solo cuántos meses te tomó llegar a la meta.
+
+#Resultado esperado en consola:
+#6
+#🚀 Sube este ejercicio a tu GitHub
+
+
