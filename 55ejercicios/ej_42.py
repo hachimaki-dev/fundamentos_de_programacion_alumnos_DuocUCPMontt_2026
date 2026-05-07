f45c2c4aa5a863d19959e8ae897c8f87fe6c0f92
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-07 10:35:15 -0400

    Ejercicio 42: Actualización de Puntos (Wallet)

---
+++ 55ejercicios/ej_42.py
@@ -0,0 +1,11 @@
+# Ejercicio 42: Actualización de Puntos (Wallet)
+
+print("===================================")
+print("Hora de Actulizar tus pontos Wallet")
+print("===================================")
+
+cliente = {'puntos': 1500}
+
+cliente['puntos'] = cliente['puntos'] + 300
+
+print(cliente)
