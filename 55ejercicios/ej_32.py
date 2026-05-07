5bf6c1b9c041c33c5da12ff65d776a8394506791
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-06 21:16:50 -0400

    Ejercicio 32: Moderador de Lobby (LOL)

---
+++ 55ejercicios/ej_32.py
@@ -0,0 +1,11 @@
+# Ejercicio 32: Moderador de Lobby (LoL)
+
+print("==================================")
+print("Sistema de moderación de jugadores")
+print("==================================")
+
+jugadores = ['P1', 'Troll99', 'P2']
+
+jugadores.remove('Troll99')
+
+print(jugadores)
