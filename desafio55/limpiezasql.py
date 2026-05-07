0e4a8684473a0f3c960b9f78a1629c941b335b31
fe-macias <fernando.macias19@icloud.comgit config --global user.name fernando.macias19@icloud.com>
2026-05-07 10:49:24 -0400

    ejercicio 39

---
+++ desafio55/limpiezasql.py
@@ -0,0 +1,12 @@
+#Limpia los datos sucios que dejó la gente al llenar mal un formulario.
+
+#Datos Iniciales: Recibiste estas respuestas: `['Juan', None, '', 'Ana', ' ']`.
+
+#Reglas de Negocio: Solo sirven los nombres reales. Los espacios en blanco (' '), las respuestas vacías ('') o los datos nulos (`None`) se consideran basura y hay que botarlos. Solo guarda los nombres válidos en una lista nueva.
+
+#Restricciones: Recorre las respuestas. Usa `is not None` para asegurarte de que no sea nulo. Luego usa `.strip()` para borrarle los espacios extra y asegúrate de que no quede vacío `!= ''`. Si pasa las pruebas, guárdalo. Imprime la lista limpia.
+respuestas_data = ["Juan" , None ,"" , "Ana" , ""]
+
+for i in respuestas_data:
+    if i is not None and i.strip()!="":
+        print(i)
