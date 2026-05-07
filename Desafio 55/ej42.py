424af8de2ec25c8275dea49698045e7063e2c140
Zobeiba Henriquez <henriquezzobeiba@gmail.com>
2026-05-07 15:43:50 -0400

    Resuelto Ejercicio 42: Actualización de Puntos (Wallet)
    Updated customer points by adding 300 to existing points.

---
+++ Desafio 55/ej42.py
@@ -0,0 +1,16 @@
+#Ejercicio 42: Actualización de Puntos (Wallet)
+#Súmale puntos de regalo a la cuenta de un cliente fiel.
+
+#Datos Iniciales: El cliente tiene un diccionario con sus datos: `{'puntos': 1500}`.
+
+#Reglas de Negocio: Por comprar hoy, el cliente se ganó 300 puntos extra. Tienes que sumárselos a los que ya tiene.
+
+#Restricciones: No escribas el resultado final mentalmente (eso es trampa). Dile a Python que vaya a la llave `'puntos'` y le sume `+ 300` a lo que sea que haya adentro. Imprime el diccionario completo para ver el cambio.
+
+datos = {
+    'puntos':1500
+}
+
+datos['puntos'] += 300
+
+print(datos)
