f8b9bd392032266004a2ac25039681a2faa2eabb
LeCoward <sebabarri2710@gmail.com>
2026-05-07 10:49:24 -0400

    Ej43.py

---
+++ desafio55/Ej43.py
@@ -0,0 +1,17 @@
+#💀 Rango: Infernal
+#Ejercicio 43: Alta de Medio de Pago
+#Guarda la nueva tarjeta de crédito del usuario en su base de datos.
+
+#Datos Iniciales: El usuario tiene su billetera vacía: `pagos = {}`.
+
+#Reglas de Negocio: Acaba de inscribir su tarjeta Visa. Hay que meter ese nuevo dato al diccionario.
+
+#Restricciones: Para agregar cosas a un diccionario, solo inventa una llave nueva y ponle un valor. Crea la llave `'visa'` y asígnale el texto `'activa'`. Imprime el diccionario para comprobar.
+
+pagos = {
+
+}
+
+pagos["Visa"] = "Activa"
+
+print(pagos)
