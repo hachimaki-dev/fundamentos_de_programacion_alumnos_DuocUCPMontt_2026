d68692d7c283282f6bc4aeb7cbd0128c0c1a4bee
nanyasg <nany.asg12.com>
2026-05-07 12:04:06 -0400

    ejercicio 42

---
+++ 55 desafios/42.py
@@ -0,0 +1,5 @@
+cliente = {"puntos": 1500}
+
+print(f"Puntos actuales: {cliente["puntos"]} \nPor su compra, recibe 300 puntos mas")
+cliente["puntos"] += 300
+print(f"Puntos actuales: {cliente["puntos"]}")
