a9785c741441f9829847dfdf9961bad8393912c8
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-07 10:07:31 -0400

    Ejercicio 37: Multiplicador de Exp In-Place (RPG)

---
+++ 55ejercicios/ej_37.py
@@ -0,0 +1,13 @@
+# Ejercicio 37: Multiplicador de Exp In-Place (RPG)
+
+print("=============================================")
+print("Evento Especial: ¡Doble Experiencia Activada!")
+print("=============================================")
+
+exp = [100, 200, 300]
+
+# recorremos los índices de la lista
+for i in range(len(exp)):
+    exp[i] = exp[i] * 2
+
+print(exp)
