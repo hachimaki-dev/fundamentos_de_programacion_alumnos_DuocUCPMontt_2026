68ecba65a4c3ea9b09418d6bbaa3780a50fdb2cd
Evelin-Vidal <evelynpatriciavidalbustamante@gmail.com>
2026-05-07 16:14:05 -0400

    Resuelto Ejercicio 51: Análisis de Sentimiento Básico (Twitter)

---
+++ desafio55/51.py
@@ -0,0 +1,14 @@
+#Haz que un programa cuente cuántas veces se repite una palabra en un texto.
+#Datos Iniciales: El comentario de un cliente es: `'buen servicio mal precio buen ambiente'`. Tienes un diccionario contador: `freq = {'buen': 0, 'mal': 0}`.
+#Reglas de Negocio: Tienes que desarmar la frase en palabras sueltas. Luego, revisas si la palabra que estás mirando está en tu diccionario. Si está, le sumas 1 a su contador.
+#Restricciones: Usa `.split()` para picar la frase en una lista de palabras. 
+#Usa un `for` para recorrerlas. Usa `if palabra in freq:` para evitar que el código explote con palabras que no te interesan (como 'servicio' o 'precio').
+#Imprime tu diccionario contador al terminar. Pista de Ayuda:Usa if palabra in freq: para asegurar que sumas correctamente.
+freq = {'buen': 0, 'mal': 0}
+comentario = "buen servicio mal precio buen ambiente"
+palabras = comentario.split()
+for palabra in palabras:
+    if palabra in freq:
+        freq[palabra] += 1
+
+print(freq)
