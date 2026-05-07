8f277bcce4420e40465ad6c7b5e3bfa1e4fcb267
KAT-000 <sebastianignaciocorderospier@gmail.com>
2026-05-07 15:17:13 -0400

    Ejercicios hasta el 25

---
+++ Desafio_55/24.py
@@ -0,0 +1,4 @@
+base_name = "user_"
+
+for i in range(1, 6):
+    print(f"{base_name}{i}")
---
+++ Desafio_55/25.py
@@ -0,0 +1,7 @@
+mensajes = ['hola', 'noob', 'genial', 'manco']
+
+for palabra in mensajes:
+    if palabra in ['noob', 'manco']:
+        print("[CENSURADO]")
+    else:
+        print(palabra)
