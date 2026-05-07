49a2b787fc0a2fe98236c853f21db9fa21ec2c90
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-07 10:51:12 -0400

    Ejercicio 44: Baja de Suscripción

---
+++ 55ejercicios/ej_44.py
@@ -0,0 +1,11 @@
+# Ejercicio 44: Baja de Suscripción
+
+print("============================")
+print("Actualizacion de Suscripcion")
+print("============================")
+
+cobros = {'spotify': 4000, 'netflix': 7000}
+
+del cobros['spotify']
+
+print(cobros)
