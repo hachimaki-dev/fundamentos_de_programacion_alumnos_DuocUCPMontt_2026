6532fbfde53de1150cfa092c7caa607b178e076b
Markito33 <markitoaguilar080@gmail.com>
2026-05-07 00:42:37 -0400

    Resuelto Ejercicio 17: Multa Autopista por Tipo de Vehículo

--- desafio55/17.py
+++ desafio55/17.py
@@ -1,17 +1,7 @@
 tipo_vehiculo = "camion"
-
-tipo_vehiculo1 = "auto"
-tipo_vehiculo2 = "camion"
-
 velocidad = 95
 
-regla_camion = 80
-
-regla_auto = 120
-
-if tipo_vehiculo == tipo_vehiculo1 and velocidad > 120:
+if tipo_vehiculo == "auto" and velocidad > 120:
     print("Multa Grave")
-elif tipo_vehiculo == tipo_vehiculo2 and velocidad > 80:
+elif tipo_vehiculo == "camion" and velocidad > 80:
     print("Multa Grave Camión")
-else:
-    print("Velocidad adecuada, no hay multa")
