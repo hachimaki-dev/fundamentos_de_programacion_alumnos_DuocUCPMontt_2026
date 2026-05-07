6dec0b592e7913b21b1d1cf097b03d5ab13ef1e0
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-07 10:25:40 -0400

    Ejercicio 41: Creación de Perfil Base

---
+++ 55ejercicios/ej_41.py
@@ -0,0 +1,12 @@
+# Ejercicio 41: Creación de Perfil Base
+
+print("=============================")
+print("Creación de perfil de jugador")
+print("=============================")
+
+jugador = {
+    'nombre': 'TuNombre',
+    'nivel': 1
+}
+
+print(jugador['nombre'])
