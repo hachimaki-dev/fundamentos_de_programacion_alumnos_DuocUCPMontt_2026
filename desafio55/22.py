047a7cdc824dc05fafdc3a3699d5f7c38b98462d
axel-trujillo <axeltrujillo1110@gmail.com>
2026-05-07 17:58:48 -0400

    Resuelto ejercicio 22: Ahorro Meta Mensual(PS5)

---
+++ desafio55/22.py
@@ -0,0 +1,8 @@
+alcancia = 0 
+ahorro_mensual =  80000 
+precio_consola = 450000
+meses = 0
+while alcancia <= precio_consola:
+    alcancia = alcancia + ahorro_mensual
+    meses = meses + 1
+    print(meses)
