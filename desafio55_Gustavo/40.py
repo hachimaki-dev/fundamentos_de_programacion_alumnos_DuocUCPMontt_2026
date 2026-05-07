d7bbacabffaf3518d6900d28cdc52d169e5f8709
Gtvq32 <gtvq1718@gmail.com>
2026-05-07 15:19:16 -0400

    Resuelto Ejercicio 40: Gestión de Inventario (Minecraft)

--- desafio55_Gustavo/40.py
+++ desafio55_Gustavo/40.py
@@ -1,11 +1,12 @@
-mochila=["mapa","espada"]
+mochila=["mapa","espada"
+         ]
 piso=["madera","piedra","oro"]
 capacidad_max=4
 
 for i in piso:
-    mochila.append(i)
-
-    if len(mochila)==capacidad_max:
+    if len(mochila) >= capacidad_max:
         print("lleno")
         break
+    else :
+        mochila.append(i)
 print(mochila)
