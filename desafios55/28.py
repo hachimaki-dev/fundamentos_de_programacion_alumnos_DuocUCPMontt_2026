e98ab4a596c41cd03456fecbe253e31215c44062
jp-sanchez6 <megajuanx@gmail.com>
2026-05-07 17:38:21 -0400

    ejercicio 28 completado

---
+++ desafios55/28.py
@@ -0,0 +1,12 @@
+patentes = ['XY11', 'ZZ99', 'AB12', 'XX00']
+
+for patente in patentes:
+    if patente == 'AB12':
+        print(f"SOSPECHOSO ENCONTRADO: {patente}")
+        
+        break
+        
+    else:
+        print(f"Buscando en {patente}")
+
+    
