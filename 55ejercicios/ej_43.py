815247e2d068455df24106b0933916148b87d99e
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-07 10:42:28 -0400

    Ejercicio 43: Alta de Medio de Pago

---
+++ 55ejercicios/ej_43.py
@@ -0,0 +1,11 @@
+# Ejercicio 43: Alta de Medio de Pago
+
+print("===============================")
+print("Registrando nuevo medio de pago")
+print("===============================")
+
+pagos = {}
+
+pagos['visa'] = 'activa'
+
+print(pagos)
