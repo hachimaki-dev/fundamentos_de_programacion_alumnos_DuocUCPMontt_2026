42df5b0ac84d51857be57e7dece0ac534b118499
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-06 21:22:19 -0400

    Ejercicio 33: Últimos Vistos (YouTube

---
+++ 55ejercicios/ej_33.py
@@ -0,0 +1,11 @@
+# Ejercicio 33: Últimos Vistos (YouTube)
+
+print("==============================")
+print("Sistema de historial de videos")
+print("==============================")
+
+historial = ['Intro', 'Tutorial', 'Gameplay']
+
+historial.reverse()
+
+print(historial[0])
