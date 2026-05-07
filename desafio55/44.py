127e9b502041e6e20c5c03d82a415c5ce0faf7f9
Evelin-Vidal <evelynpatriciavidalbustamante@gmail.com>
2026-05-07 15:28:14 -0400

    Resuelto Ejercicio 44: Baja de Suscripción

---
+++ desafio55/44.py
@@ -0,0 +1,8 @@
+#Borra la suscripción que el usuario acaba de cancelar para que no le cobren más.
+#Datos Iniciales: Tus cobros mensuales son: `{'spotify': 4000, 'netflix': 7000}`.
+#Reglas de Negocio: Ya no quieres pagar Spotify. Hay que borrarlo del registro.
+#Restricciones: Usa la palabra reservada `del` seguida de la llave que quieres matar (ej. `del diccionario['llave']`), o usa la herramienta `.pop()`. 
+#Imprime cómo quedaron tus cuentas. {'netflix': 7000}
+cuentas = {"spotify": 4000, "netflix": 7000}
+del cuentas["spotify"] 
+print(cuentas)
