d1b53f3d8a8e6bb1f9de62dd62356782b3c57b55
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-06 21:28:19 -0400

    Ejercicio 34: TOP NOTAS

---
+++ 55ejercicios/ej_34.py
@@ -0,0 +1,12 @@
+# Ejercicio 34: Top Notas (Portal Alumno)
+
+print("============================")
+print("Sistema de revisión de notas")
+print("============================")
+
+notas = [3.5, 6.2, 5.0, 2.1, 7.0]
+
+mayor = max(notas)
+menor = min(notas)
+
+print(mayor, "-", menor)
