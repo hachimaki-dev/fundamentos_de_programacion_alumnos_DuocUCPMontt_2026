b716ecfdb34b9f806eeb808e8cc0918d89ffb218
seba1304256 <seba13email.com@gmail.com>
2026-05-07 15:34:21 -0400

    Resuelto ejercicio 31: Armando Playlist

---
+++ desafio_55/31.py
@@ -0,0 +1,11 @@
+#Controla el orden en que se reproducen las canciones de tu playlist.
+
+#Datos Iniciales: Tienes esta lista de canciones: `['B', 'C']`.
+
+#Reglas de Negocio: El usuario apretó 'Agregar a la cola' en la canción 'D' (eso la manda al final). Luego apretó 'Reproducir siguiente' en la canción 'A' (eso la manda al inicio de todo, empujando a las demás).
+
+#Restricciones: Usa la herramienta `.append()` para meter cosas al final de la lista. Usa `.insert(0, ...)` para obligar a una canción a ponerse en la posición cero (el principio). Imprime la lista completa al terminar.
+lista_de_cancion = ["b", "c"]
+lista_de_cancion.append("d")
+lista_de_cancion.insert(0,"a")
+print(lista_de_cancion)
