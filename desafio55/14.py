afcdac56604905c941fb4c2fb2c6c83f0a98ed0b
yosmer1106 <yos.carrillo@duocuc.cl>
2026-05-07 02:00:44 -0400

    Resuelto Ejercicio 14: Sistema de Grados con Bonificación

---
+++ desafio55/14.py
@@ -0,0 +1,12 @@
+nota = 3.6
+asistencia = 85
+
+if asistencia >= 80:
+    nota += 0.5
+if nota >= 4.0:
+    print("aprobado")
+elif nota >= 3.5 and nota <= 3.9:
+    print("examen")
+else:
+    print("reprobado")
+
