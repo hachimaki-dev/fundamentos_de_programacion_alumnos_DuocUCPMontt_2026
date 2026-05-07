77421f40b8f0982566fbd6ca277d1a0e711177e2
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-06 21:14:06 -0400

    Ejercicio 31: Armando la Playlist

---
+++ 55ejercicios/ej_31.py
@@ -0,0 +1,13 @@
+# Ejercicio 31: Armando la Playlist
+
+print("===================")
+print("Sistema de playlist")
+print("===================")
+
+canciones = ['B', 'C']
+
+canciones.append('D')
+
+canciones.insert(0, 'A')
+
+print(canciones)
