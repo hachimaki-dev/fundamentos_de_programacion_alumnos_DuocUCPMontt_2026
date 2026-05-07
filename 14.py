d4e284821b32a5789ec2eb7223f62d2f0d2c3092
rominadanaee <rominacocio08@gmail.com>
2026-05-06 23:18:25 -0400

    Resuelto Ejercicio 13: Promoción VIP BancoEstado (OR y AND)

---
+++ 14.py
@@ -0,0 +1,10 @@
+ #Revisa si un cliente puede subir a categoría VIP en su banco
+
+sueldo_del_cliente = int(input("porfavor ingrese su sueldo: ") )
+tiempo_en_el_banco = int(input(" porfavor ingrese cuantos años lleva en el bamco: "))
+deudas_activas = int(input("porfavor ingrese cuantas deudas tiene: "))
+
+if sueldo_del_cliente > 1000000 or (tiempo_en_el_banco >= 5 and deudas_activas == 0):
+    print(f" felicidades usted a subido a categoria vip")
+else: 
+    print("cliente normal")
