c55114d4dab20893ed5b4f0572b54f11816dad19
seba1304256 <seba13email.com@gmail.com>
2026-05-07 15:47:04 -0400

    Resuelto ejercicio 33: ultimo vistos (youtube)

---
+++ desafio_55/33.py
@@ -0,0 +1,12 @@
+#Muestra el último video que vio el usuario en su perfil.
+
+#Datos Iniciales: Tu historial está ordenado del más viejo al más nuevo: `['Intro', 'Tutorial', 'Gameplay']`.
+
+#Reglas de Negocio: A nadie le importa lo primero que viste, quieren ver lo más reciente. Necesitas dar vuelta la lista para que lo más nuevo quede de primero, y luego mostrar solo ese video.
+
+#Restricciones: Usa `.reverse()` para dar vuelta la lista físicamente. Luego, imprime solo el elemento que quedó en la posición `0`.
+historial = ["intro", "tutorial", "gamplay"]
+historial.reverse()
+video_reciente = historial[0]
+print(video_reciente)
+
