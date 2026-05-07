f689d9fe17edc8fc1d589b76326e4b1e30d6c532
fe-macias <fernando.macias19@icloud.comgit config --global user.name fernando.macias19@icloud.com>
2026-05-07 14:38:37 -0400

    ejercicio 41

---
+++ desafio55/perfilbase.py
@@ -0,0 +1,9 @@
+#Crea el perfil de un jugador usando diccionarios (la mejor forma de guardar datos complejos).
+
+#Datos Iniciales: Vas a crear un jugador nuevo.
+
+#Reglas de Negocio: Un diccionario guarda datos en parejas (una 'llave' y su 'valor'). El jugador debe tener la llave `'nombre'` (pon tu nombre de valor) y la llave `'nivel'` (que parte en 1).
+
+#Restricciones: Escribe el diccionario a mano usando las llaves `{}`. Luego, haz que Python extraiga tu nombre accediendo a la llave `'nombre'` usando corchetes. Imprime solo tu nombre.
+jugador_nuevo = {"Yikus" : 1}
+print(jugador_nuevo)
