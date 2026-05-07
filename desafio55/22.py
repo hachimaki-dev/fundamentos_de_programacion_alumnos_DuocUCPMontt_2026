1f8772f159ea84f8bdaf6bfe21f8353fca5a3d4e
Markito33 <markitoaguilar080@gmail.com>
2026-05-07 01:17:45 -0400

    Resuelto Ejercicio 22: Ahorro Meta Mensual (PS5)

---
+++ desafio55/22.py
@@ -0,0 +1,12 @@
+alcancia = 0
+
+ahorros_x_mes = 80000
+
+valor_consola = 450000
+
+contador = 0
+
+while alcancia < valor_consola:
+    alcancia = alcancia + ahorros_x_mes
+    contador = contador + 1
+print(contador)
