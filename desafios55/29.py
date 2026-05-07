7ba852c502f39a499a5c2bcae5dd4aa47d248d36
jp-sanchez6 <megajuanx@gmail.com>
2026-05-07 17:49:00 -0400

    ejercicio 29 completado

---
+++ desafios55/29.py
@@ -0,0 +1,19 @@
+minutos = 0
+calorias_quemadas = 0
+
+while calorias_quemadas <= 300:
+    if minutos <= 10:
+        minutos += 1
+        calorias_quemadas += 20
+        
+    else:
+        minutos += 1
+        calorias_quemadas += 10
+        
+print(minutos)
+        
+        
+
+
+
+    
