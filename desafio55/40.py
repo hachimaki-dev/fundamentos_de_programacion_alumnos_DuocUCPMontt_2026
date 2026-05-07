59e5877ccfc564fa58c1de31896d912b8e6d51d9
Shiekd-L <ordonezbenja22@gmail.com>
2026-05-06 23:22:25 -0400

    Resuelto Ejercicio 40: Gestión de Inventario (Minecraft)

---
+++ desafio55/40.py
@@ -0,0 +1,8 @@
+mochila = ["mapa","espada"]
+encontrado = ['madera', 'piedra', 'oro']
+for i in encontrado:
+    mochila.append(i)
+    if len(mochila) == 4:
+        print("mochila llena")
+        break
+print(mochila)
