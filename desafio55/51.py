b7e275bd2cbf0bde3dc38cf09c9d3130f27a5024
SIA <o98290039@gmail.com>
2026-05-07 21:20:28 -0400

    Resuelto Ejercicio 51: Análisis de Sentimiento Básico (Twitter)

---
+++ desafio55/51.py
@@ -0,0 +1,7 @@
+comentario = "buen servicio mal precio buen ambiente"
+freq = {"buen": 0, "mal": 0}
+palabras = comentario.split()
+for palabra in palabras:
+    if palabra in freq:
+        freq[palabra] +=1
+print(freq)
