81b822798b6a61d9b2e846420b8a6cf3057ae11e
AlanRuizg6589 <ala.ruizg@duocuc.cl>
2026-05-08 18:09:32 -0400

    Resuelto Ejercicio 32: Moderador de Lobby (LoL)

---
+++ Desafio55/Ejercicio32.py
@@ -0,0 +1,16 @@
+"""Ejercicio 32: Moderador de Lobby (LoL)
+Echa a un jugador tóxico de la sala de espera antes de empezar el juego.
+
+Datos Iniciales: Los jugadores en la sala son `['P1', 'Troll99', 'P2']`.
+
+Reglas de Negocio: Troll99 fue reportado y no puede jugar. Tienes que sacarlo de la lista para que la partida pueda empezar.
+
+Restricciones: No escribas la lista de nuevo a mano, eso es trampa. 
+
+Usa la herramienta `.remove()` escribiendo exactamente el nombre del jugador que quieres borrar. Imprime la lista limpia."""
+
+Jugadores = ["P1", "Troll99", "P2"]
+
+Jugadores.remove(Jugadores[1])
+
+print(Jugadores)
