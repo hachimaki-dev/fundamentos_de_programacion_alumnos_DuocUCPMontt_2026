de039fa7dcb125bd869084d14658b2df2c65bef3
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-07 21:14:36 -0400

    Resuelto Ejercicio 18: Sistema Anti-Robo Inteligente (Smart Home)

---
+++ desafio55/18.py
@@ -0,0 +1,8 @@
+movimiento = True
+hora = 3
+dueño_en_casa = True
+
+if (movimiento == True and dueño_en_casa == False) or (movimiento == True and hora < 6):
+    print("Alarma Sonado")
+else:
+    print("Alarma en Silencio")
