66d0f3924692e9b6616ffdef0f26f8000cc4f9c3
SIA <o98290039@gmail.com>
2026-05-07 22:58:37 -0400

    Resuelto Ejercicio 54: Carrito Inteligente Múltiple (Amazon)

---
+++ desafio55/54.py
@@ -0,0 +1,7 @@
+carrito = [{"item": "Mouse", "qty": 2, "precio": 15000}, {"item": "Teclado", "qty": 1, "precio": 30000}]
+total = 0
+for producto in carrito:
+    total += producto["qty"] * producto["precio"]
+if total > 50000:
+    total *= 0.9
+print(total)
