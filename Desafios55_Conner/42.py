2a6e43b11ac5a40cc9f4b7506f716e03b73ca9d8
RubiZero <abcmudkip@gmail.com>
2026-05-08 04:09:39 -0400

    actualizacion nocturna 08-05

---
+++ Desafios55_Conner/42.py
@@ -0,0 +1,17 @@
+#Ejercicio 42: Actualización de Puntos (Wallet)
+#Súmale puntos de regalo a la cuenta de un cliente fiel.
+
+#Datos Iniciales: El cliente tiene un diccionario con sus datos: `{'puntos': 1500}`.
+
+#Reglas de Negocio: Por comprar hoy, el cliente se ganó 300 puntos extra. Tienes que sumárselos a los que ya tiene.
+
+#Restricciones: No escribas el resultado final mentalmente (eso es trampa). 
+#Dile a Python que vaya a la llave `'puntos'` y le sume `+ 300` a lo que sea que haya adentro. Imprime el diccionario completo para ver el cambio.
+
+cliente={
+    "puntos": 1500,
+}
+
+bonus=300
+cliente["puntos"]=cliente["puntos"]+bonus
+print(cliente)
