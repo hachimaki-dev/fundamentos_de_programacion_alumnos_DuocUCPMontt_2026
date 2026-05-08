61c223b2e69304bdf78b96d094c41e8ba6a9b460
yvnnng28 <Benjaandres29sanchezzz@gmail.com>
2026-05-07 20:46:11 -0400

    Resuelto Ejercicio 51: Análisis de Sentimiento Básico (Twitter)

--- Desafio55/51.py
+++ Desafio55/51.py
@@ -1,10 +1,12 @@
 comentario_c = 'buen servicio mal precio buen ambiente'
 freq = {'buen': 0, 'mal': 0}
+lista_de_palabras = comentario_c.split()
 
-for i in comentario_c.split():
-    if i in freq['buen' , 'mal'] == i:
-        freq += 1
-    else:
-        continue
 
+for i in lista_de_palabras:
+    if i == "buen":
+        freq["buen"] += 1
+    elif i == "mal":
+        freq["mal"] += 1
+    
 print(freq)
