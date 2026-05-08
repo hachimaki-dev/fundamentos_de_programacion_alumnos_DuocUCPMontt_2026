d11dcee8c8bb4b273864c66b05e68c1a90a83c58
SIA <o98290039@gmail.com>
2026-05-07 21:28:51 -0400

    Resuelto Ejercicio 52: Calculadora de Liquidación Real

---
+++ desafio55/52.py
@@ -0,0 +1,11 @@
+sueldo_base = 800000
+bonos = {"colacion": 50000, "movilizacion": 30000}
+descuentos = {"AFP": 0.10, "Salud": 0.07}
+total_bonos = 0
+for bono in bonos.values():
+    total_bonos += bono
+total_descuentos = 0
+for porcentaje in descuentos.values():
+    total_descuentos += sueldo_base * porcentaje
+sueldo_liquido = sueldo_base + total_bonos - total_descuentos
+print(sueldo_liquido)
