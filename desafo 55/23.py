1e518d4126ee5a982434034a9eafa76f92eccd51
ñoquita <valentina19.ism@0gmail.com>
2026-05-08 01:32:45 -0400

    Resuelto Ejercicio 24: Generador de Nombres (Instagram)

--- desafo 55/23.py
+++ desafo 55/23.py
@@ -2,8 +2,13 @@ contador_fallido = 0
 clave = "secreto"
 clave_ingresada = "hola"
 
-while contador_fallido >= 3:
-    if clave_ingresada != clave:
-        contador_fallido = contador_fallido + 1
-print(contador_fallido)
+while True:
+    if clave_ingresada != clave: 
+        if clave_ingresada != clave: 
+            contador_fallido = contador_fallido + 1 
+            print("bloqueo de tarjeta")
+            break
+    else: 
+        print("Bienvenido")
+        
 
---
+++ desafo 55/24.py
@@ -0,0 +1,3 @@
+base = "user_"
+for numero in range(1,6):
+    print(base + str(numero))
