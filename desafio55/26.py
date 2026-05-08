f9a86de2de0d8a3fb96c0b8d118457659680517f
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-07 21:46:16 -0400

    Resuelto Ejercicio 26: Procesar Cola de Impresión

---
+++ desafio55/26.py
@@ -0,0 +1,16 @@
+hojas = 5
+impresiones = ["TEXTO", "FOTO", "TEXTO", "FOTO"]
+costo_texto = 1
+costo_foto = 3
+
+for documento in impresiones:
+    if documento == "TEXTO":
+        gasto = costo_texto
+    else:
+        gasto = costo_foto
+    if gasto <= hojas:
+        print(f"Imprimiendo {documento}")
+        hojas = hojas - gasto
+    else:
+        print("Sin papel")
+        break
