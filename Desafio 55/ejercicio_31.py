715972de30fe7b8c2bc6716cef5b75df7cc7323e
Her-cloud <carcamoh78@gmail.com>
2026-05-07 22:23:25 -0400

    Resuelto Ejercicio 31: Armando la Playlist

---
+++ Desafio 55/ejercicio_31.py
@@ -0,0 +1,8 @@
+# Armando la Playlist
+# Controla el orden en que se reproducen las canciones de tu playlist.
+
+lista_de_canciones = ['B', 'C']
+lista_de_canciones.append('D')
+lista_de_canciones.insert(0, 'A')
+
+print(lista_de_canciones)
