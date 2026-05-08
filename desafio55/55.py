ea5f2db53489dc60b5eee81c3c932eb19e1f2367
Evelin-Vidal <evelynpatriciavidalbustamante@gmail.com>
2026-05-08 14:20:56 -0400

    Resuelto Ejercicio 55: Simulador de Cajero con Desglose (BancoEstado)

---
+++ desafio55/55.py
@@ -0,0 +1,10 @@
+
+monto_retiro = 37000
+billetes = [20000, 10000, 5000, 1000]
+resultado_vuelto = {}
+for billete in billetes:
+    cantidad = monto_retiro // billete
+    resultado_vuelto[billete] = cantidad
+    monto_retiro = monto_retiro % billete
+
+print(resultado_vuelto)
