9f57d97d7399d4250fd5e390ab58a0f362e66de3
ñoquita <valentina19.ism@0gmail.com>
2026-05-08 00:18:36 -0400

    Resuelto Ejercicio 17: Multa Autopista por Tipo de Vehículo

---
+++ desafo 55/17.py
@@ -0,0 +1,12 @@
+tipo_vehiculo = "Camion"
+velocidad = 95
+
+if tipo_vehiculo == "Auto" and velocidad > 120:
+    print("Multa Gravisima")
+elif tipo_vehiculo == "Camion" and velocidad > 80:
+    print("Multa grave Camion")
+
+else: 
+    print("sin mulda")
+
+
