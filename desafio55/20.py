ad2ac4e1b5ebad035a450ec11f70e45c04191615
ullooacu <ulloacu@gmail.com>
2026-05-07 23:26:45 -0400

    Resuelto Ejercicio 20: Simulador de Cajero: Retiro Complejo

--- desafio55/20.py
+++ desafio55/20.py
@@ -1,11 +1,17 @@
 #Ejercicio 20: Simulador de Cajero: Retiro Complejo
 #El Mini-Boss: Haz que el cajero automático valide 3 reglas antes de dar dinero.
 
-#Datos Iniciales: Tienes 50000 de saldo. Tu límite máximo diario para sacar plata es 200000. Quieres retirar 60000.
+#Datos Iniciales: Tienes 50000 de saldo. Tu límite máximo diario para sacar
+#  plata es 200000. Quieres retirar 60000.
 
-#Reglas de Negocio: Regla 1: Si lo que pides es más que tu límite diario, rechaza con 'Excede límite diario'. Regla 2: Si pides más de lo que tienes en el saldo, rechaza con 'Saldo insuficiente'. Regla 3: Si lo que pides no es múltiplo de 5000 (el cajero no da monedas), rechaza con 'Monto inválido'. Si pasas todas las reglas, 'Retiro exitoso'.
+#Reglas de Negocio: Regla 1: Si lo que pides es más que tu límite diario,
+#  rechaza con 'Excede límite diario'. Regla 2: Si pides más de lo que tienes 
+# en el saldo, rechaza con 'Saldo insuficiente'. Regla 3: Si lo que pides no es 
+# múltiplo de 5000 (el cajero no da monedas), rechaza con 'Monto inválido'. Si 
+# pasas todas las reglas, 'Retiro exitoso'.
 
-#Restricciones: Usa un bloque `if / elif / elif / else` para revisar regla por regla. Apenas falle una, debe imprimir el error y detenerse. Imprime el error de este caso.
+#Restricciones: Usa un bloque `if / elif / elif / else` para revisar regla por regla. 
+# Apenas falle una, debe imprimir el error y detenerse. Imprime el error de este caso.
 
 
 #Resultado esperado en consola:
@@ -15,14 +21,28 @@ saldo = 50000
 limite_diario = 200000
 retiro_de_dinero = 60000
 
+#Regla 1: Si lo que pides es más que tu límite diario,
+#  rechaza con 'Excede límite diario'. Regla 2: Si pides más de lo que tienes 
 
-if limite_diario > retiro_de_dinero:
-    print("Excede límite diario")
+if retiro_de_dinero > limite_diario:
+    print("excede limite diario")
 
     
-if saldo < retiro_de_dinero:
+
+#Regla 2: Si pides más de lo que tienes 
+# en el saldo, rechaza con 'Saldo insuficiente'.
+
+elif retiro_de_dinero > saldo:
     print("Saldo insuficiente")
+
+#Regla 3: Si lo que pides no es 
+# múltiplo de 5000 (el cajero no da monedas), rechaza con 'Monto inválido'        
     
-if retiro_de_dinero % 5000 == 0:
+elif retiro_de_dinero % 5000 != 0:
     print("Monto inválido")
+
+else:
+    print("retiro exitoso")
+
+    
 
