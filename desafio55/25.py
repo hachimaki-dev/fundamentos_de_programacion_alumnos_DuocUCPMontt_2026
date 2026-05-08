cb3e11b7e26d247f9a550f8a11fadc6629aaa2b5
bellium-xls <sanscraftinghd@gmail.com>
2026-05-08 00:03:50 -0400

    Resuelto Ejercicio 55: Simulador de Cajero con Desglose (BancoEstado)

--- desafio55/25.py
+++ desafio55/25.py
@@ -4,4 +4,5 @@ for mensaje in lista_de_mensajes:
     if mensaje == "noob" or mensaje == "manco":
         lista_de_mensajes[contador] = "CENSURADO"
     contador +=1
+    
 print(lista_de_mensajes)
