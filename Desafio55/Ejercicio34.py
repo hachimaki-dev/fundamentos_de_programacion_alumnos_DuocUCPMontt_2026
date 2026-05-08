d59053c9ab1cb3b967b734fb74c616e52995ec0f
AlanRuizg6589 <ala.ruizg@duocuc.cl>
2026-05-08 19:57:34 -0400

    Resuelto Ejercicio 34: Top Notas (Portal Alumno)

---
+++ Desafio55/Ejercicio34.py
@@ -0,0 +1,14 @@
+'''Ejercicio 34: Top Notas (Portal Alumno)
+Encuentra tu mejor y peor nota del semestre rápidamente.
+
+Datos Iniciales: Tus notas finales fueron `[3.5, 6.2, 5.0, 2.1, 7.0]`.
+
+Reglas de Negocio: Necesitas encontrar la nota más alta y la más baja para ver si aplicas a una beca o si necesitas tutoría.
+
+Restricciones: No hagas ciclos manuales complicados. 
+
+Python tiene dos herramientas que hacen esto por ti: `max()` y `min()`. Úsalas e imprime ambas notas separadas por un guion.'''
+
+Notas_Finales = [3.5, 6.2, 5.0, 2.1, 7.0]
+
+print(f'{max(Notas_Finales)} - {(min(Notas_Finales))}')
