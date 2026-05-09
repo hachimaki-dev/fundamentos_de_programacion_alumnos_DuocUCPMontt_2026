a5c6fa15c04f7cc972aafc6ce6a683b6e0fd9ea2
diego0954-alt <diego0954@icloud.com>
2026-05-09 16:44:16 -0400

    Armando la Playlist

---
+++ desafio 55/31.py
@@ -0,0 +1,9 @@
+lista_de_canciones = ["B","C"]
+
+agregar_ala_cola = lista_de_canciones
+agregar_ala_cola.append("D")
+
+reproducir_siguente = lista_de_canciones
+reproducir_siguente.insert(0,"A")
+
+print(lista_de_canciones)
