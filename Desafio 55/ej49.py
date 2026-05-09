809889a6eb8923fca0ec7129218fb966e52e83d5
Zobeiba Henriquez <henriquezzobeiba@gmail.com>
2026-05-09 17:10:13 -0400

    Add files via upload

---
+++ Desafio 55/ej49.py
@@ -0,0 +1,22 @@
+#Ejercicio 49: Ejercicio 49: Verificación de Permisos (AWS)
+
+#Aprende a meterte dentro de un diccionario que tiene otro diccionario adentro (datos anidados).
+
+#Datos Iniciales: Los permisos del usuario son: admin = {'rol': 'dev', 'permisos': {'S3': True, 'EC2': False}}.
+
+#Reglas de Negocio: El usuario quiere prender un servidor 'EC2'. Tienes que revisar en sus permisos si tiene la autorización (True o False) para hacerlo.
+
+#Restricciones: Como es un diccionario dentro de otro, tienes que encadenar los corchetes (ej. diccionario['llave_padre']['llave_hijo']) dentro de tu if. Si tiene permiso imprime 'Creando instancia', sino imprime 'Acceso Denegado'.
+
+admin = {
+    'rol':'dev',
+    'permisos':{
+        'S3':True,
+        'EC2':False
+    }
+}
+
+if admin['permisos']['EC2']:
+    print("Creando instancia")
+else:
+    print("Acceso denegado")
