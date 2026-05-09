e4aae03a72c8845ba1648d23bb00d2ac82ce9f2e
AlanRuizg6589 <ala.ruizg@duocuc.cl>
2026-05-08 20:12:30 -0400

    Resuelto Ejercicio 36: Fusión de Servidores (Clústeres)

---
+++ Desafio55/Ejercicio36.py
@@ -0,0 +1,23 @@
+'''Ejercicio 36: Fusión de Servidores (Clústeres)
+Junta los usuarios de dos servidores distintos sin crear cuentas duplicadas.
+
+Datos Iniciales: El Servidor 1 tiene a `['U1', 'U2']`. El Servidor 2 tiene a  `['U2', 'U3']`.
+
+Reglas de Negocio: Tienes que pasar a los usuarios del Servidor 2 al Servidor 1. 
+
+Pero OJO, el usuario 'U2' ya estaba en el Servidor 1. Si lo copias de nuevo, romperás el sistema. Solo copia a los que sean nuevos.
+
+Restricciones: Recorre el Servidor 2 con un `for`. 
+
+Antes de agregar a un usuario, revisa si NO ESTÁ en el Servidor 1 usando la frase clave `not in`. Imprime cómo queda el Servidor 1 al final.'''
+
+Servidor_1 = ['U1', 'U2']
+
+Servidor_2 = ['U2', 'U3']
+
+
+for Valores in Servidor_2:
+    if Valores not in Servidor_1:
+        Servidor_1.append(Valores)
+
+print(Servidor_1)
