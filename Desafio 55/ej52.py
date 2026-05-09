9c89b4f1b106cce06e1294378fa8417bfbb31dc1
Zobeiba Henriquez <henriquezzobeiba@gmail.com>
2026-05-09 18:53:37 -0400

    Add files via upload

---
+++ Desafio 55/ej52.py
@@ -0,0 +1,32 @@
+#Ejercicio 52: Ejercicio 52: Calculadora de Liquidación Real
+
+#Calcula el sueldo de una persona leyendo datos desde tres lados distintos.
+
+#Datos Iniciales: El sueldo_base = 800000. Sus bonos son: {'colacion': 50000, 'movilizacion': 30000}. Sus descuentos son {'AFP': 0.10, 'Salud': 0.07}.
+
+#Reglas de Negocio: El trabajador gana la suma de su base más los bonos. Pero hay que restarle los descuentos de AFP y Salud. Los descuentos se calculan multiplicando ese porcentaje SÓLO por el sueldo base (a los bonos no se les descuenta nada).
+
+#Restricciones: Usa un for usando .values() para sumar los bonos. Usa otro for usando .values() para recorrer los porcentajes de descuento, calcular la plata que significan, y restarlos. Imprime el sueldo líquido final.
+
+#Sueldo, bonos y descuentos
+sueldo_base = 800000
+bonos = {
+    'colacion':50000,
+    'movilizacion':30000
+}
+descuentos = {
+    'AFP':0.10,
+    'Salud':0.07
+}
+
+base = sueldo_base #Esto es para guardar el sueldo base y realizar los descuentos
+
+#Sumamos bonos
+for bono in bonos.values():
+    sueldo_base += bono 
+
+#Restamos descuentos
+for descuento in descuentos.values():
+    sueldo_base -= (base * descuento)
+
+print(int(sueldo_base))
