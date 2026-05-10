5a4768fc37c6f322ced6de173a31442d59ed138e
4l3xZzz <alexandervillalon54@gmail.com>
2026-05-09 20:54:14 -0400

    Resuelto Ejercicio 28: Búsqueda del Sospechoso (Cámaras)

---
+++ desafio 55/Ejercicio28.py
@@ -0,0 +1,7 @@
+patentes =  ["XY11", "ZZ99", "AB12", "XX00"]
+
+for pantente in patentes:
+    print(f"Buscando en {pantente}")
+    if pantente == "AB12":
+        print("SOSPECHOSO ENCONTRADO")
+        break
