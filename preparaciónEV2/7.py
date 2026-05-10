f120f38909fb523cf635a5ff397da7087f2400d3
diego0954-alt <diego0954@icloud.com>
2026-05-10 17:35:04 -0400

    Adivina el Múltiplo de 4

---
+++ preparaciónEV2/7.py
@@ -0,0 +1,38 @@
+import random
+
+
+inicio = int(input("Ingresa el número inicial del rango: "))
+fin    = int(input("Ingresa el número final del rango: "))
+
+
+numero   = random.randint(inicio, fin)
+ajustado = (numero // 4) * 4
+
+
+if ajustado < inicio:
+    ajustado = inicio
+
+
+for intento in range(1, 4):
+    guess = int(input("Adivina: "))
+
+    if guess == ajustado:
+        print("Felicitaciones, pudiste adivinar.")
+        break
+
+  
+    if guess < ajustado:
+        print("El número es mayor.")
+    else:
+        print("El número es menor.")
+
+
+    if intento == 2:
+        diferencia = abs(ajustado - guess)
+        if diferencia <= 2:
+            print("Estás muy cerca.")
+        else:
+            print("Estás lejos.")
+
+    if intento == 3:
+        print(f"El número era {ajustado}.")
