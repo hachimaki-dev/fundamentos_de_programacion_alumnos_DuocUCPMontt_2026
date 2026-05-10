e92f60e7eefc5b46cfe8a41d474ae76cea7457b2
4l3xZzz <alexandervillalon54@gmail.com>
2026-05-09 21:20:07 -0400

    Resuelto Ejercicio 31: Armando la Playlist

---
+++ desafio 55/Ejercicio31.py
@@ -0,0 +1,8 @@
+lista = ["B", "C"]
+print(lista)
+print("Agregar a la cola la cancion D")
+lista.append("D")
+print(lista)
+print("Agregar a la reproduccion la cancion A")
+lista.insert(0, "A")
+print(lista)
