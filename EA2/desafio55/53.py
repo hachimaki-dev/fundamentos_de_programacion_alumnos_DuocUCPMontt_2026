f7350ac083edf805fe51e895613bee8ed64a81f0
andrewfarias452 <an.fariast@duocuc.cl>
2026-05-10 18:04:06 -0400

    Resuelto Ejercicio 53: Matchmaking Avanzado por ELO (Valorant)

--- EA2/desafio55/53.py
+++ EA2/desafio55/53.py
@@ -11,12 +11,12 @@ Restricciones: Crea una variable `diferencia_minima` y ponle un número gigante 
 mis_puntos = 1500
 rivales = [{'id': 1, 'elo': 1200}, {'id': 2, 'elo': 1490}, {'id': 3, 'elo': 1800}]
 diferencia_minima = 9999
+rival_elegido = None
 
 for rival in rivales:
     diferencia = abs(mis_puntos - rival['elo'])
     if diferencia < diferencia_minima:
         diferencia_minima = diferencia
         rival_elegido = rival['id']
-
+        
 print(rival_elegido)
-    
