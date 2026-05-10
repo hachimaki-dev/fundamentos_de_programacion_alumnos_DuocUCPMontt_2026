858db101a9a2b119f259002419e329681d800ed2
4l3xZzz <alexandervillalon54@gmail.com>
2026-05-09 21:45:34 -0400

    Resuelto Ejercicio 37: Multiplicador de Exp In-Place (RPG)

---
+++ desafio 55/Ejercicio37.py
@@ -0,0 +1,8 @@
+experiencia_jugador = [100, 200, 300]
+
+print(experiencia_jugador)
+
+for i in range(len(experiencia_jugador)):
+    experiencia_jugador[i] = experiencia_jugador[i] * 2
+
+print(experiencia_jugador)
