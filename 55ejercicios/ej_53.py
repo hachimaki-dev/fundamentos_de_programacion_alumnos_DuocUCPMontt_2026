5fe26855f13076ba48f2f2889532fd2ed421f0bf
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-11 11:17:37 -0400

    Ejercicio 53: Matchmaking Avanzado por ELO (Valorant)

---
+++ 55ejercicios/ej_53.py
@@ -0,0 +1,25 @@
+# Ejercicio 53: Matchmaking Avanzado por ELO (Valorant)
+
+print("==========================")
+print("Buscando rival equilibrado")
+print("==========================")
+
+mis_puntos = 1500
+
+rivales = [
+    {'id': 1, 'elo': 1200},
+    {'id': 2, 'elo': 1490},
+    {'id': 3, 'elo': 1800}
+]
+
+diferencia_minima = 9999
+rival_escogido = None
+
+for rival in rivales:
+    diferencia = abs(mis_puntos - rival['elo'])
+
+    if diferencia < diferencia_minima:
+        diferencia_minima = diferencia
+        rival_escogido = rival['id']
+
+print(rival_escogido)
