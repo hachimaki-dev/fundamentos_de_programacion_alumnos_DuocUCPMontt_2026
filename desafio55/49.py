56bc245cebbdbed81a2d5249df7ba2128337229b
axel-trujillo <axeltrujillo1110@gmail.com>
2026-05-10 21:49:19 -0400

    Resuelto ejercicio 49: Verificacion de Permiso(AWS)

---
+++ desafio55/49.py
@@ -0,0 +1,12 @@
+admin ={
+    'rol': 'dev', 
+    'permisos': {
+        'S3': True, 
+        'EC2': False
+}}
+if admin['permisos']['EC2']:
+    print("Creando instancia")
+else:
+    print("Acceso denegado")
+    
+    
