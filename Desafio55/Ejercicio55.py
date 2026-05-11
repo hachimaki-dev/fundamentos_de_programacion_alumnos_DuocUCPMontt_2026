640848660eb5e0125f0e29a9dfc98b939cfe00da
AlanRuizg6589 <ala.ruizg@duocuc.cl>
2026-05-11 19:32:11 -0400

    Resuelto Ejercicio 55: Simulador de Cajero con Desglose (BancoEstado)

---
+++ Desafio55/Ejercicio55.py
@@ -0,0 +1,27 @@
+'''Ejercicio 55: Simulador de Cajero con Desglose (BancoEstado)
+El Boss Final del curso: Programa el cerebro de un cajero automático para que entregue la menor cantidad de billetes posibles al dar un vuelto.
+
+Datos Iniciales: El cliente quiere sacar 37000. Los billetes que existen son `20000, 10000, 5000 y 1000`. Crea un diccionario vacío para guardar tus resultados.
+
+Reglas de Negocio: Para usar pocos billetes, el cajero siempre intenta pasar primero los billetes más grandes. 
+
+Por ejemplo, para 37000, primero ve cuántos de 20000 caben. Con la plata que sobra, ve cuántos de 10000 caben, y así hasta llegar a cero.
+
+Restricciones: Recorre una lista con los billetes ordenados de mayor a menor. 
+
+Usa división entera (`//`) para saber cuántos billetes entregar, 
+y guárdalo en tu diccionario como `diccionario[billete] = cantidad`. 
+Luego, usa el módulo (`%`) para actualizar la plata que te falta por repartir. Imprime el diccionario con el desglose exacto al terminar.'''
+
+Billetera = {20000 : 0, 10000 : 0, 5000 : 0, 1000 : 0}
+
+
+Dinero_Que_Se_Quiere_Sacar = 37000
+
+for Dinero, Cantidad in Billetera.items():
+
+    Cantidad = Dinero_Que_Se_Quiere_Sacar//Dinero
+    Billetera[Dinero] = Cantidad
+    Dinero_Que_Se_Quiere_Sacar -= Dinero
+
+print(Billetera)
