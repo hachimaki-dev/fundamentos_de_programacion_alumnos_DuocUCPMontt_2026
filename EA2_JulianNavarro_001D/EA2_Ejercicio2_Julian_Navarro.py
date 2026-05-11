5c5b56f4ad889cdd555b6d8a915395aa43f0d208
AphidCiro <navarrogamboajulian@gmail.com>
2026-05-11 14:17:42 -0400

    guardando para la posperidad

---
+++ EA2_JulianNavarro_001D/EA2_Ejercicio2_Julian_Navarro.py
@@ -0,0 +1,46 @@
+from random import randint
+ran1 = int(input("Ingrese limite inferior: "))
+ran2 = int(input("ingrese limite inferior: "))
+numerorand = randint(ran1, ran2)
+intentos = 3
+adivinanza = 0
+adivinanza2 = 0
+adivinanza3 = 0
+if numerorand % 2 != 0 and numerorand in range(ran1, ran2):
+    numerorand += 1
+elif numerorand % 2 != 0 and numerorand not in range(ran1, ran2):
+    numerorand -= 1
+print(numerorand)
+while intentos > 0:
+    if intentos == 3:
+        adivinanza = int(input("intente adivinar: "))
+    elif intentos == 2:
+        adivinanza2 = int(input("intente adivinar: "))
+    if adivinanza == numerorand and intentos == 3:
+        print("Felicitaciones, adivino en el primer intento")
+        break
+    if adivinanza2 == numerorand and intentos == 2:
+        print("Felicitaciones adivino en su segundo intento")
+        break
+    if intentos <= 3:
+        if numerorand > adivinanza:
+            print("El numero es mayor.")
+        elif numerorand < adivinanza:
+            print("el numero es menor")
+    if intentos == 2:
+        diferencia1 = abs(numerorand - adivinanza)
+        diferencia2 = abs(numerorand - adivinanza2)
+        print("Te dare una pista:")
+        if diferencia1 > diferencia2:
+            print(f"el numero que buscas esta mas cerca de {adivinanza2} que de {adivinanza}")
+        elif diferencia1 < diferencia2:
+            print(f"el numero que buscas esta mas cerca de {adivinanza} que de {adivinanza2}")
+    if intentos == 1:
+        adivinanza3 = int(input("intente una ultima vez: "))
+        if adivinanza3 != numerorand:
+            print("Perdiste.")
+            print(f"el numero era: {numerorand}")
+        else:
+            print("Felicidades adivinaste el numero")
+    intentos -= 1
+    
