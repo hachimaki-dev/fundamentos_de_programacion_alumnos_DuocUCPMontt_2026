af6794422620cbdd53cfe0ef6c82cf3b885db55c
nanyasg <nany.asg10@gmail.com>
2026-05-11 08:56:19 -0400

    37 y 38

---
+++ 55 desafios/37.py
@@ -0,0 +1,6 @@
+exp_jugador = [100, 200, 300]
+
+for exp in range(len(exp_jugador)):
+    exp_jugador[exp] *= 2
+    
+print(exp_jugador)
---
+++ 55 desafios/38.py
@@ -0,0 +1,10 @@
+Cantidades_vendidas = [500, 1000, 200, 800, 1500]
+
+#.remove(x) .reverse(x) .insert(x, sjjs) .append(x)
+# max(sjd) min(sjb) len(sjb) .sort .sort(reverse=True) 
+#slicing es asi por ejemplo ña[:2]
+
+Cantidades_vendidas.sort(reverse=True)
+
+top3 = Cantidades_vendidas[:3]
+print(top3)
