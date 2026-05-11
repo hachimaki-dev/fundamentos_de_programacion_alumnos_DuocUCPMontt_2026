cb40252abe1a36c2877ebdbc940b7359d1e11e7b
NamiLogic <yulianabelch@gmail.com>
2026-05-10 20:42:46 -0400

    17/55

--- desafio55/17.py
+++ desafio55/17.py
@@ -1,16 +1,15 @@
 """
-🟡 Rango: Cuidado
 Ejercicio 17: Multa Autopista por Tipo de Vehículo
 Programa la cámara de la carretera para sacar multas según el vehículo.
 
 Datos Iniciales: Pasó un 'Camion' a 95 km/h.
 
-Reglas de Negocio: Los autos no pueden pasar de 120 km/h (si se pasan, es 
-'Multa Gravísima'). Los camiones son más pesados y no pueden pasar de 80 km/h 
-(si se pasan, es 'Multa Grave Camión'). Si van bajo sus límites, no hay multa.
+Reglas de Negocio: Los autos no pueden pasar de 120 km/h (si se pasan, es 'Multa Gravísima'). 
+Los camiones son más pesados y no pueden pasar de 80 km/h (si se pasan, es 'Multa Grave Camión'). 
+Si van bajo sus límites, no hay multa.
 
-Restricciones: Usa `if/elif` evaluando ambas cosas (el tipo de vehículo y la velocidad) 
-en la misma línea con un `and`. Imprime la multa correspondiente para el camión.
+Restricciones: Usa `if/elif` evaluando ambas cosas (el tipo de vehículo y la velocidad) en la misma 
+línea con un `and`. Imprime la multa correspondiente para el camión.
 
 Resultado esperado en consola:
 Multa Grave Camión
@@ -19,6 +18,13 @@ Multa Grave Camión
 kilometraje_camion = 95
 
 tipo_1 = "camion"
-tipo_2 = "Auto"
+tipo_2 = "auto"
 maximo_autos = 120
 maximo_camiones = 80
+
+if tipo_1 == "camion" and kilometraje_camion > maximo_camiones:
+    print("Multa Grave Camion")
+elif tipo_2 == "auto" and kilometraje_camion > maximo_autos:
+    print("Multa Gravisima")
+else:
+    print("Sin multa")
