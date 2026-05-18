2b2b4290bcfafcb85d7b73c8c4d40276e6f6c9d8
fe-macias <fernando.macias19@icloud.comgit config --global user.name fernando.macias19@icloud.com>
2026-05-18 13:08:54 -0400

    funciones

---
+++ Python/funciones.py
@@ -0,0 +1,29 @@
+#Funcion solo funciona a lo que mas importante , osea las responsabilidad mas pequeña
+#Funcion recibe una entrada , se le conoce como argumento y entrara como parametro , que se transforma en una salida ,SOLO PALABRAS QUE NO ESTE RESERVADAS , INT , INPUT , WHILE NO!
+#Para ejecutar la funcion yo tendria que invocar la funcion 
+#Ojo con el orden , no puedo ir primero el argumento arriba por que si no , no ejecuta
+#El triple de un numero 
+
+
+
+#def el_triple_de_un_numero(numero_ingresado):
+    #resultado = numero_ingresado * 3
+    #return resultado
+#el_triple_de_un_numero(55)
+
+
+
+#Calculadora 
+
+def sumar (numero_1 , numero_2):
+    resultado = numero_1 + numero_2
+    return resultado
+def resta (numero_1 , numero_2):
+    resultado = numero_1 - numero_2
+    return resultado
+def multiplicar(numero_1,numero_2):
+    resultado = numero_1 * numero_2
+    return resultado
+def division(numero1,numero_2):
+    resultado = numero_1 // numero_2
+    return resultado
