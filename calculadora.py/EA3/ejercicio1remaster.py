84de3a7f4994f0921f6276406cbc0b38d04bd0a9
RubiZero <abcmudkip@gmail.com>
2026-05-18 17:58:27 -0400

    tratando de remasterizar ejercicio 1 de la prueba v1

---
+++ calculadora.py/EA3/ejercicio1remaster.py
@@ -0,0 +1,14 @@
+medicamentos=int(input("Ingrese el valor de su medicamento"))
+descuento=int(input("Ingrese un descuento"))
+envio=8000
+total=0
+
+def calculo_descuentos_medicamento(medicamentos):
+    medicamentos*descuento
+
+def envio(envio):
+    
+
+def programa
+
+def pedir datos personales
