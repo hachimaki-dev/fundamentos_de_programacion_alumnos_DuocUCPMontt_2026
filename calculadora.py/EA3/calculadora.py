f8c18d9c5ed95304f1e78ae4d861caaacaa6634e
RubiZero <abcmudkip@gmail.com>
2026-05-18 17:37:01 -0400

    una calculadora mas bonita

--- calculadora.py/EA3/calculadora.py
+++ calculadora.py/EA3/calculadora.py
@@ -1,4 +1,9 @@
 
 
-def elTripleDeUnNumero(10):
-    
+def elTripleDeUnNumero(numero_ingresado):
+    return numero_ingresado*3
+print("Hola 4")
+
+#elTripleDeUnNumero(45)  #Lo del parentesis es un argumento
+
+print(elTripleDeUnNumero(45))
