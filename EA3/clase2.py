ff4af8e902fa8e21ff2833bb77aac9ca4d674693
diego0954-alt <diego0954@icloud.com>
2026-05-20 09:44:49 -0400

    EA3

---
+++ EA3/clase2.py
@@ -0,0 +1,21 @@
+#ejercicio1:
+#def saludar(nombre):
+#   return f"hola, {nombre}, bienvenido al sistema"
+#respuesta = saludar("pepito") #argumento
+#
+#print(respuesta)
+
+#ejercicio2:
+#def sumar(a,b):
+#    return a + b
+#print(sumar(300, 420))
+
+#ejercicio3:
+def es_par(numero_recibido):
+    if numero_recibido % 2 == 0:
+       return True
+    else:
+       return False
+   
+print(es_par(292929))
+
