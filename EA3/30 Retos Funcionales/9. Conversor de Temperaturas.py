1dcab9720b3dd33a88082e974192ad628793afcd
Shiro341 <jairopineda@duck.com>
2026-05-20 17:57:03 -0400

    Crea celsius_a_fahrenheit(c) que retorne la temperatura en F. Fórmula: (C * 9/5) + 32.

---
+++ EA3/30 Retos Funcionales/9. Conversor de Temperaturas.py
@@ -0,0 +1,8 @@
+#Crea celsius_a_fahrenheit(c) que retorne la temperatura en F. Fórmula: (C * 9/5) + 32.
+
+
+def celsius_a_fahrenheit(c):
+    formula = (c * 9/5) + 32
+    return formula
+
+print(celsius_a_fahrenheit(51))
