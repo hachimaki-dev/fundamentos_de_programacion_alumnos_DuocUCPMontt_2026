ca567a4c15e9b7daa887886ef05ad11836ddf7e9
Roten-chan <henriquezzobeiba@gmail.com>
2026-05-20 12:59:12 -0400

    ejercicios de funciones

---
+++ EA3/clase 02/01.py
@@ -0,0 +1,7 @@
+#Crea saludar(nombre) que retorne el string "Hola [nombre], benvenido al sistema"
+
+def saludar(nombre):
+    return f"Hola, {nombre}, bienvenido al sistema"
+
+respuesta = saludar("Pepito")
+print(respuesta)
---
+++ EA3/clase 02/02.py
@@ -0,0 +1,6 @@
+#Crea sumar(a, b) que retorne la suma de los dos numeros recibidos.
+
+def suma(a, b):
+    return a + b
+
+print(suma(2, 3))
