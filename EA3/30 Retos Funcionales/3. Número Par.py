6a8e50c22f0eefc5a6fa8929c03b84c20c142cce
Shiro341 <jairopineda@duck.com>
2026-05-20 16:19:07 -0400

    Crea es_par(numero) que retorne True si el número es par, y False si es impar.

---
+++ EA3/30 Retos Funcionales/3. Número Par.py
@@ -0,0 +1,9 @@
+#Crea es_par(numero) que retorne True si el número es par, y False si es impar.
+
+def es_par(numero):
+    if numero % 2 == 0:
+        return True
+    else:
+        return False
+    
+print(es_par(5))
