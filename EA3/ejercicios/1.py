a5cab2698e501940f847a1784a6ccc78ad518ea4
CheelaMS <golhabesoto@proton.me>
2026-05-20 10:29:06 -0400

    ejercicios

---
+++ EA3/ejercicios/1.py
@@ -0,0 +1,6 @@
+#Crea saludar(nombre) que retorne el string "Hola, [nombre], bienvenido al sistema".
+
+def saludar(nombre):
+    return (f"Hola {nombre}, bienvenido al sistema.")
+
+print (saludar("Cheela"))
---
+++ EA3/ejercicios/2.py
@@ -0,0 +1,6 @@
+#Crea sumar(a, b) que retorne la suma de los dos números recibidos.
+
+def sumar (a, b):
+    return a + b
+
+print (sumar(2, 99))
