0df09bac0d2b3f0ced2e2283d09aa82d022eae50
agcamila01 <agcamila003@gmail.com>
2026-05-20 11:13:08 -0400

    apunte

--- Clase 1 - Definición Funciones/apuntes_funciones.py
+++ Clase 1 - Definición Funciones/apuntes_funciones.py
@@ -2,10 +2,12 @@
 # ARGUMENTO -> PARAMETROS -> SALIDA
 # snakecase, UPPER_SNAKE_CASE, camelCase
 # codigo boilerplate
+# SCOPE -> GLOBAL
+#       -> LOCAL -> variable local solo esta definida dentro de la funcion 
 
 def el_triple_de_un_numero(numero_ingresado): # () -> PARAMETRO
     resultado = numero_ingresado * 3
-    return resultado
+    return resultado # la variable 'resultado' es local
     # return numero_ingresado * 3
 
 # numero_ingresado = int(input())
