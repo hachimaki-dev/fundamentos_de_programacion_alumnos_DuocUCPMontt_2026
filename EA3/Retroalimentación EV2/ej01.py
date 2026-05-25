306cd0cd1fbbacb3d72c31327f4c98dced831998
Roten-chan <henriquezzobeiba@gmail.com>
2026-05-25 10:14:08 -0400

    ejercicio 1 prueba correjido en clases

---
+++ EA3/Retroalimentación EV2/ej01.py
@@ -0,0 +1,26 @@
+#Variables pedidas en el ejercicio
+medicamentos = 60000
+despacho = 8000
+
+#Se le pide información al usuario
+edad_usuario = int(input("Ingrese su edad: "))
+tramo = input("Ingrese su tramo: ").upper()
+
+#Reglas del descuento en medicamentos
+if (edad_usuario <= 30) and (tramo == 'A' or tramo == 'B'):
+    medicamentos = medicamentos * 0.82
+    despacho = despacho * 0.9    #Reglas del despacho
+elif (edad_usuario <= 30) and (tramo == 'C' or tramo == 'D'):
+    medicamentos = medicamentos * 0.88
+elif (31 <= edad_usuario <= 60) and (tramo == 'A' or tramo == 'B'):
+    if edad_usuario >= 55 and edad_usuario <= 60:
+        medicamentos = medicamentos * 0.88
+        despacho = despacho * 0.85    #Reglas del despacho si el usuario >= 55
+    else:
+        medicamentos = medicamentos * 0.82
+        despacho = despacho * 0.9    #Reglas del despacho
+elif (31 <= edad_usuario <= 60) and (tramo == 'C' or tramo == 'D'):
+    medicamentos = medicamentos * 0.92
+
+print(f"El valor de medicamentos es: ${medicamentos}")
+print(f"El valor del espacho es: ${despacho}")
