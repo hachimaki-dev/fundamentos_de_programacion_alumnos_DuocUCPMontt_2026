f0885b3a987944b114cb03eb593e72ea721e1d5a
Roten-chan <henriquezzobeiba@gmail.com>
2026-05-25 10:16:54 -0400

    ejercicio 1 prueba correjido en clases

--- EA3/Retroalimentación EV2/ej01.py
+++ EA3/Retroalimentación EV2/ej01.py
@@ -15,7 +15,7 @@ elif (edad_usuario <= 30) and (tramo == 'C' or tramo == 'D'):
 elif (31 <= edad_usuario <= 60) and (tramo == 'A' or tramo == 'B'):
     if edad_usuario >= 55 and edad_usuario <= 60:
         medicamentos = medicamentos * 0.88
-        despacho = despacho * 0.85    #Reglas del despacho si el usuario >= 55
+        despacho = despacho * 0.85    #Reglas del despacho si la edad del usuario es >= 55
     else:
         medicamentos = medicamentos * 0.82
         despacho = despacho * 0.9    #Reglas del despacho
