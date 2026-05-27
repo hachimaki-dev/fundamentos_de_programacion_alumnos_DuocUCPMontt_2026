a7a039908ea2af916028d02f6a57e822cd8adc85
RubiZero <abcmudkip@gmail.com>
2026-05-27 16:21:40 -0400

    clase 27-05-2026

--- Aprendiendo3school/listas.py
+++ Aprendiendo3school/listas.py
@@ -1,3 +1,8 @@
+
+
 lista=["Turtwig", "Chimchar", "Piplup"]
 lista.append("Pachirisu")
 print(lista)
+
+
+
--- Evaluacion_2/Ejercicio2.py
+++ Evaluacion_2/Ejercicio2.py
@@ -1,24 +1,64 @@
 from random import randint
 
 
+limite_inferior=int(input("Ingrese limite inferior "))
+limite_superior=int(input("Ingrese limite superior "))
+numero_aleatorio_generado=randint(limite_inferior, limite_superior)
 
-numero1 = int(input("Ingrese limite inferior "))
-numero2 = int(input("Ingrese limite superior "))
-numero=randint(numero1, numero2)
+if numero_aleatorio_generado % 2 != 0:
+    numero_aleatorio_generado+=1
+# print(numero_aleatorio_generado)
+    if numero_aleatorio_generado==limite_superior:
+        numero_aleatorio_generado-=1
+    elif numero_aleatorio_generado!=limite_superior:
+        numero_aleatorio_generado+=1
 
-if numero % 2 != 0:   #LO HIZO EL PROFE :(
-    numero=numero+1
-print(numero)
-
-numero_user= int(input("Por favor adivine el número "))
+numero_usuario= int(input("Por favor adivine el número "))
 intentos=0
 
-while intentos<3:
-    if numero_user < numero:
-        intentos=intentos+1
-        print("El numero es mayor")
-        numero_user= int(input("Por favor adivine el número "))
-    elif numero_user > numero:
-        intentos=intentos+1
-        print("El numero es menor")
-        numero_user= int(input("Por favor adivine el número "))
+#while intentos<3:
+#    if numero_usuario < numero_aleatorio_generado:
+#        intentos=intentos+1
+#        print("El numero es mayor")
+#        numero_usuario= int(input("Por favor adivine el número "))
+#    elif numero_usuario > numero_aleatorio_generado:
+#        intentos=intentos+1
+#        print("El numero es menor")
+#        numero_usuario= int(input("Por favor adivine el número "))
+
+cada_vuelta=1
+
+for cada_vuelta in range(1,4):
+    intento_usuario= int(input("Intente adivinar "))
+    if intento_usuario==numero_aleatorio_generado:
+        print("Felicidades, adivinaste el numero")
+        break
+    else:
+        #Se equivoco
+        #Vienen las pistas
+        if cada_vuelta==1:
+            intento_1_fallido=intento_usuario
+            contador_intento_fallido+=1
+            print("Fallaste")
+            if intento_usuario<numero_aleatorio_generado:
+                print("El número es mayor")
+            elif intento_usuario>numero_aleatorio_generado:
+                print("El número es menor")
+        elif cada_vuelta==2:
+            intento_2_fallido=intento_usuario
+            contador_intento_fallido+=1
+            print("Fallaste")
+            if intento_usuario<numero_aleatorio_generado:
+                print("El número es mayor")
+                if abs(numero_aleatorio_generado-intento_1_fallido)<abs(numero_aleatorio_generado-intento_2_fallido):
+                    print("El primer intento estuvo más cerca")
+                    print(f"Recuerda que el número está entre {intento_1_fallido}")
+            elif intento_usuario>numero_aleatorio_generado:
+                print("El número es menor")
+                if abs(numero_aleatorio_generado-intento_1_fallido)<abs(numero_aleatorio_generado-intento_2_fallido):
+                    print("El primer intento estuvo más cerca")
+                    print(f"Recuerda que el número está entre {intento_1_fallido}")
+        elif contador_intento_fallido==2:
+            print(f"Fallaste, el número era {numero_aleatorio_generado}")
+            break
+        
