d426ceeb1a74e884a2e62dd68f4ef62025d635f3
Shiro341 <jairopineda@duck.com>
2026-05-26 23:05:49 -0400

    Funciones

---
+++ EA3/30 Retos Funcionales/11. Diccionario de Frecuencias.py
@@ -0,0 +1,7 @@
+#Crea contar_caracteres(texto) que retorne un diccionario donde las claves son las letras y los valores son la cantidad de veces que aparecen.
+
+def contar_caracteres(texto):
+    diccionario = []
+
+    for i in diccionario:
+        
---
+++ EA3/buscar.py
@@ -0,0 +1,10 @@
+#cual es la diferencia entre una funcion y un metodo
+#hacer un power point con los metodos de las listas y diccionarios 
+
+lista_numeros = [1, 2, 3, 4, 5]
+
+nuevo_numero = 9
+
+nuevo_numero.apeend()
+
+print(lista_numeros)
