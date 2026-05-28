dbdc411d9ec7986f79c013f1becd6696857467de
Alvas-cod3 <alvargonz507@gmail.com>
2026-05-28 15:30:51 -0400

    ejercicio 1

--- soluciones_ejercicios_preparatorios_ev3/ejercicio_1.py
+++ soluciones_ejercicios_preparatorios_ev3/ejercicio_1.py
@@ -1,6 +1,7 @@
 while True:
     try:
         numero = int(input("Ingresa un numero: "))
+        #esta parte es lo que se ejecuta cuando el usuario ingresa un valor válido:
         break
     except ValueError:
         print("ERROR: eso no es un numero entero mi pana")
