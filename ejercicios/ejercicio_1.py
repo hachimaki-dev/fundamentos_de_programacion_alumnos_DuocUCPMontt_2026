cbbddd4d803db953e526e5e9f4756dd032dcf6d5
alonmunozc-bit <alon.munozc@duocuc.cl>
2026-05-28 10:52:34 -0400

    ejercicio 2

--- ejercicios/ejercicio_1.py
+++
@@ -1,9 +0,0 @@
-# Ejercicio 1A — Solo capturar ValueError (calentamiento)
-
-while True:
-    try:
-        numero_entero = int(input("Ingrese un número entero: "))
-        print(f"Numero recibido {numero_entero}")
-        break
-    except ValueError:
-        print("Error, eso no es un número entero")
