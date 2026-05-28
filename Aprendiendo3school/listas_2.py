fda269a2d2fa147124455c5298e78f807f231ecd
RubiZero <abcmudkip@gmail.com>
2026-05-28 13:06:05 -0400

    actualizacion mañanera

---
+++ Aprendiendo3school/listas_2.py
@@ -0,0 +1,9 @@
+
+
+boolean_list=[True, False]
+
+number_list=[1, 2, 3]
+
+str_list=["Hola genteee"]
+
+
---
+++ Aprendiendo3school/listas_3.py
@@ -0,0 +1,7 @@
+
+equipo_pokemon=["Torterra", "Infernaoe", "Empoleon", "Luxray", "Staraptor", "Garchomp"]
+
+print(equipo_pokemon[2])
+
+print(equipo_pokemon[-1])
+
