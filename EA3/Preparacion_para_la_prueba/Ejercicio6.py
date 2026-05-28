f73043db507cceabdd628efe5e7f6b45c9839543
Shiro341 <jairopineda@duck.com>
2026-05-28 10:50:10 -0400

    Ejercicio6

---
+++ EA3/Preparacion_para_la_prueba/Ejercicio6.py
@@ -0,0 +1,38 @@
+# Ejercicio 6 — Patente de vehículo en un estacionamiento
+# Un sistema de estacionamiento pide la patente del auto. Debe:
+
+# Tener exactamente 6 caracteres
+# No contener espacios
+# Dato cultural: En Chile existen dos formatos de patente, ambos de 6 caracteres:
+
+# Formato antiguo (hasta 2007): 4 letras + 2 números (ej: BBCR45)
+# Formato nuevo (desde 2007): 2 letras + 4 números (ej: AB1234)
+# Para este ejercicio solo validaremos largo y ausencia de espacios, no el patrón exacto de letras/números.
+
+# Si no es válida:
+
+# "Patente inválida. Ingresa exactamente 6 caracteres sin espacios."
+# Válidas: BBCR45, AB1234 Inválidas: BB CR45, AB12, ABCDEFG
+
+while True:
+
+    patente_vehiculo = input("Ingrese su patente: ")
+
+    tiene_espacios = False
+
+    for caracteres in patente_vehiculo:
+        if caracteres == " ":
+            tiene_espacios = True
+
+    if tiene_espacios == True:
+        print("Patente inválida. Ingresa caracteres sin espacios.")
+
+    elif len(patente_vehiculo) != 6:
+        print("Patente inválida. Ingresa exactamente 6 caracteres sin espacio.")
+
+    else:
+        print(f"Patente Ingresada: {patente_vehiculo}")
+        break
+
+
+
