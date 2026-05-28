e727fb55c09d5eabb2e911a0132f4d669b62b8a5
Zizager <zinedineargel12@gmail.com>
2026-05-28 15:47:58 -0400

    Ejercicio 1B listo

---
+++ Soluciones preparacion ev 3/1B.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        pasajeros=int(input("Ingrese el número de pasajeros en el avión: "))
+        if pasajeros>0:
+            print(f"Vuelo registrado con {pasajeros} pasajeros.")
+            break
+        elif pasajeros<0:
+            print("Error: ingresa un número entero positivo de pasajeros.")
+            continue
+    except:
+        print("Error: ingresa un número entero positivo de pasajeros.")
