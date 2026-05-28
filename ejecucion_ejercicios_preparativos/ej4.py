7144d82340a951e38667a0f3f4d793b1810b821e
LeCoward <sebabarri2710@gmail.com>
2026-05-28 11:04:59 -0400

    No terminado, terminar y estudiar en casa

---
+++ ejecucion_ejercicios_preparativos/ej4.py
@@ -0,0 +1,7 @@
+while True:
+    nombre_creado = input("Ingrese su nombre de usuario. (Este debe tener mínimo 6 carácteres y no contener espacios) ")
+    for cantidad_caracteres in nombre_creado :
+        print({cantidad_caracteres})
+
+
+
