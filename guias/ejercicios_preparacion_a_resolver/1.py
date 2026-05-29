c5f9e6c630c4285c09f964be6e6f45f10459b6c6
Tomy30b <barriatomy2gmail.com>
2026-05-29 00:30:37 -0400

    Ejercicio 1 resuelto

---
+++ guias/ejercicios_preparacion_a_resolver/1.py
@@ -0,0 +1,14 @@
+
+
+while True:
+
+    try:
+       ingresar_numero = int(input("Ingresa un numero: "))
+       break
+
+    
+
+
+    except ValueError :
+
+        print("Error: eso no es un numero entero: ")
