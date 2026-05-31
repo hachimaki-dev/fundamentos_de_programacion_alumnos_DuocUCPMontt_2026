c4435009fb3ea46e553dd291435d7b78634cf7ea
Luis <sloomby87@gmail.com>
2026-05-30 23:22:48 -0400

    Revert "Tarea 1a"
    This reverts commit d6e79ca49c86d26677107d057f985571bccd075f.

--- guias/ola1.py
+++
@@ -1,6 +0,0 @@
-while True:
-    try:
-        numero = int(input("Ingrese su numero: "))
-        break
-    except ValueError:
-        print("Error: Eso no es un numero entero.")
--- tarea6.py
+++
@@ -1,9 +0,0 @@
-while True:
-    codigo = input("Ingrese el código del producto: ")
-    
-    if len(codigo) >= 6 and " " not in codigo:
-        
-        print(f"Producto registrado con código: {codigo}")
-        break
-    else:
-        print("Código inválido. Intente nuevamente.")
