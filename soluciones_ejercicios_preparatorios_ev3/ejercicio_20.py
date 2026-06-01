cd26cd3f1c0afc4584b1059f2fa26436d30b8ff7
Alvas-cod3 <alvargonz507@gmail.com>
2026-06-01 15:59:05 -0400

    Ejercicio 20 hecho

--- soluciones_ejercicios_preparatorios_ev3/ejercicio_20.py
+++ soluciones_ejercicios_preparatorios_ev3/ejercicio_20.py
@@ -8,11 +8,11 @@ menu_opciones = {"Ver equipos disponibles":1,
 opcion_del_usuario = None
 
 while opcion_del_usuario != 5:
-    print(f"=== BIBLIOTECA TECNOLÓGICA UNIVERSIDAD DEL SUR ===\n1. Ver equipos disponibles\n2. Prestar equipo(s)\n3. Recibir devolución\n4. Ver historial de préstamos activos\n5. Salir")
+    print(f"\n=== BIBLIOTECA TECNOLÓGICA UNIVERSIDAD DEL SUR ===\n1. Ver equipos disponibles\n2. Prestar equipo(s)\n3. Recibir devolución\n4. Ver historial de préstamos activos\n5. Salir")
     while True:
         try:
-            opcion_del_usuario = int(input("Ingrese el número de una opción (ejemplo: 1 para ver los equipos disponibles)")).strip()
-            if opcion_del_usuario <= 0 and opcion_del_usuario > 5:
+            opcion_del_usuario = int(input("\nIngrese el número de una opción (ejemplo: 1 para ver los equipos disponibles)"))
+            if opcion_del_usuario < 1 or opcion_del_usuario > 5:
                 print("Ingrese una opción válida")
             else:
                 break
@@ -21,14 +21,38 @@ while opcion_del_usuario != 5:
     if opcion_del_usuario == menu_opciones["Ver equipos disponibles"]:
         print(f"Hay {stock_equipos_biblioteca_universidad} equipos disponibles en la biblioteca")
     elif opcion_del_usuario == menu_opciones["Recibir devolución"]:
+        if historial_de_prestamos == 0:
+            print("Actualmente no hay ningun equipo para recibir")
+        else:
+            while True:
+                try:
+                    equipos_para_devolver = int(input("Ingrese la cantidad de equipos a devolver: "))
+                    if equipos_para_devolver <= historial_de_prestamos and equipos_para_devolver >= 1:
+                        print(f"Se recibieron {equipos_para_devolver} equipos")
+                        
+                        historial_de_prestamos -= equipos_para_devolver
+                        stock_equipos_biblioteca_universidad += equipos_para_devolver
+                        break
+                    else:
+                        print("Ingrese un numero de equipos válido")
+                except ValueError:
+                    print("Ingrese un numero de equipos válido")
+    elif opcion_del_usuario == menu_opciones["Ver historial de prestamos activos"]:
+        print(f"Actualmente hay {historial_de_prestamos} equipos prestados")
+    elif opcion_del_usuario == menu_opciones["Prestar equipos"]:
         while True:
             try:
-                equipos_para_devolver = int(input("Ingrese la cantidad de equipos a devolver: ")).strip()
-                if equipos_para_devolver <= historial_de_prestamos:
-                    print(f"Se recibieron {equipos_para_devolver} equipos")
+                equipos_para_prestar = int(input("Ingrese la cantidad de equipos para prestar: "))
+                if equipos_para_prestar <= stock_equipos_biblioteca_universidad and equipos_para_prestar >= 1:
+                    print(f"Se prestaron {equipos_para_prestar} equipos")
+                    historial_de_prestamos += equipos_para_prestar
+                    stock_equipos_biblioteca_universidad -= equipos_para_prestar
                     break
                 else:
-                    print("Ingrese un numero de equipos válido")
+                    print("Ingrese un número de equipos válido")
             except ValueError:
-                print("Ingrese una opción válida")
+                print("Ingrese un número de equipos válido")
+    else:
+        print("si")
+
         
