492c49a8ffac8084b5581dc772dabbe2a17fb6f9
Roten-chan <henriquezzobeiba@gmail.com>
2026-06-02 19:54:31 -0600

    xD

--- EJERCICIOS PREPARATORIOS EV3/NIVEL 6/19_count.py
+++ EJERCICIOS PREPARATORIOS EV3/NIVEL 6/19_count.py
@@ -11,8 +11,8 @@
 #Al finalizar:
 #"La flota cuenta con 3 camiones pesados y 5 camiones livianos. Registro completado."
 
-camion_pesado = []
-camion_liviano = []
+camion_pesado = 0
+camion_liviano = 0
 
 while True:
     try:
@@ -29,10 +29,7 @@ for v in range(registro_vehiculos):
     while True:
         patente = input(f"Patente del camión {v + 1}: ").strip().upper()
         if len(patente) == 6 and " " not in patente:
-            if (patente in camion_liviano) or (patente in camion_pesado):
-                print("Error: La patente ya ha sido registrada.")
-            else:
-                break
+            break
         else:
             print("Error: La patente debe tener 6 caracteres, sin espacios.")
     
@@ -47,10 +44,8 @@ for v in range(registro_vehiculos):
             print("Error: La capacidad del camión debe ingresarse en formato numérico.")
         
     if capacidad > 15:
-        camion_pesado.append(patente)
+        camion_pesado += 1
     else:
-        camion_liviano.append(patente)
-    
-print(f"Camión pesado = {camion_pesado}")
-print(f"Camión liviano = {camion_liviano}")
-print(f"La flota cuenta con {len(camion_pesado)} camiones pesados y {len(camion_liviano)} camiones livianos. Registro completado.")
+        camion_liviano += 1
+
+print(f"La flota cuenta con {camion_pesado} camiones pesados y {camion_liviano} camiones livianos. Registro completado.")
