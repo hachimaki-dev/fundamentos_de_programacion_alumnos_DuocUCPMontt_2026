7675b5ae962cce8d8a43549d51aa16758df1a3f9
Alvas-cod3 <alvargonz507@gmail.com>
2026-06-02 20:57:03 -0400

    Ejercicio 1 de diccionarios listo

---
+++ soluciones_ejercicios_preparatorios_ev3/ejercicio_dicciolistas1.py
@@ -0,0 +1,8 @@
+libro = {"Título":"El Señor de los anillos",
+         "Autor":"J.R.R Tolkien",
+         "Año":1954}
+print(libro.values())
+libro["Año"] = 2021
+print(libro.values())
+libro["Editorial"] = "Santillana"
+print(libro)
