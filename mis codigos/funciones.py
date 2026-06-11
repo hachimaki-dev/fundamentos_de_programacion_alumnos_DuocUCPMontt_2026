15ad89f6f5ec9402aefc01e26ec55bcf216b364e
MatiasValdivia327 <matias.felipe7777>
2026-06-11 10:15:16 -0400

    funciones

---
+++ mis codigos/funciones.py
@@ -0,0 +1,9 @@
+# funciones SIN retorno
+def nombreDeLaFuncion(argumento_recibido):
+    print(f"hola {argumento_recibido}")
+
+def NombreDeLaFuncion():
+    return "hola"
+
+for i in range(1,4):
+    nombreDeLaFuncion(i)
---
+++ mis codigos/hotel.py
@@ -0,0 +1,16 @@
+habitacionestotal = 50
+def MostrarMenu():
+    print("¡Bienvenido al sistema de gestión de habitaciones del Hotel Estelar!")
+    print("=== MENÚ PRINCIPAL ===")
+    print("1. Habitaciones disponibles")
+    print("2. Realizar check-in")
+    print("3. Realizar check-out")
+    print("4. Historial de ocupaciones")
+    print("5. Salir")
+    
+MostrarMenu()
+
+def habitacionesdisponibles():
+    return habitacionesdisponibles
+
+habitacionesdisponibles()
