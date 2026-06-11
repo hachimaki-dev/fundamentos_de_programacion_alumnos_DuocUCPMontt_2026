37c36a89ace07d1abe226fca73d8cb77e8367978
RubiZero <abcmudkip@gmail.com>
2026-06-11 16:36:21 -0400

    clase 11/06/2026

---
+++ Clase 11_06_2026/1.py
@@ -0,0 +1,5 @@
+def mostrar_bienvenida():
+    print("¡Bienvenido al programa!")
+
+x=mostrar_bienvenida()
+print(x)
---
+++ Clase 11_06_2026/2.py
@@ -0,0 +1,7 @@
+def duplicar_print(numero):
+    print(numero * 2)
+def duplicar_return(numero):
+    return numero * 2
+
+a=duplicar_print(5)
+b=duplicar_return(5)
