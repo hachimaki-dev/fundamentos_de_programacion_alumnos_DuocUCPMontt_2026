de8b1236f1ab20f01dd5cb7cbd8159dd350a0bd9
agcamila01 <agcamila003@gmail.com>
2026-06-17 01:28:50 -0400

    bichos

--- Sistema Cazabichos/cazabichos.py
+++ Sistema Cazabichos/cazabichos.py
@@ -20,18 +20,18 @@ def elegir_opcion_menu():
     
 def validar_especie_bicho():
     while True:
-        especie = input("Ingrese la especie del bicho: ")
-        if " " in especie or len(especie) <= 0:
+        especie = input("Ingrese la especie del bicho: ").strip()
+        if len(especie) <= 0:
             print("Este campo no puede estar vacío ni ser solo espacios en blanco.\nVuelva a intentar.")
         else:
             return especie
 
-def validar_longitud_bicho():
+def validar_tamano_bicho():
     while True:
         try:
-            longitud = int(input("Ingrese el tamano del bicho: "))
-            if longitud > 0:
-                return longitud
+            tamano = int(input("Ingrese el tamano del bicho: "))
+            if tamano > 0:
+                return tamano
             else:
                 print("El tamaño del bicho debe ser superior a cero.")
         except ValueError:
@@ -50,12 +50,12 @@ def validar_peligrosidad_bicho():
 
 def agregar_bicho():
     especie_bicho_validada = validar_especie_bicho()
-    longitud_bicho_validada = validar_longitud_bicho()
+    tamano_bicho_validada = validar_tamano_bicho()
     peligrosidad_bicho_validada = validar_peligrosidad_bicho()
 
     datos_del_bicho = {
         'especie': especie_bicho_validada,
-        'longitud': longitud_bicho_validada,
+        'tamano': tamano_bicho_validada,
         'peligrosidad': peligrosidad_bicho_validada,
         'es_peligroso': False
     }
@@ -63,31 +63,36 @@ def agregar_bicho():
     lista_bichos.append(datos_del_bicho)
 
 def buscar_bicho():
-    bicho_buscado = input("Ingrese la especie del bicho a buscar: ")
+    bicho_buscado = input("Ingrese la especie del bicho: ")
     for i in range(len(lista_bichos)):
         if lista_bichos[i]['especie'] == bicho_buscado:
-            return lista_bichos[i]
-        else:
-            print("No esta el bicho")
-            return -1
+            return i
+    return -1
             
 def eliminar_bicho():
-    # agregar validacion cuando no esta el bicho
-
-    dict_bicho_eliminar = buscar_bicho()
-    if dict_bicho_eliminar != -1:
-        lista_bichos.remove(dict_bicho_eliminar)
+    index_bicho_eliminar = buscar_bicho()
+    if index_bicho_eliminar != -1:
+        lista_bichos.remove(lista_bichos[index_bicho_eliminar])
     else:
-        print("No esta el bicho")
+        return -1
 
 def actualizar_estados_bichos():
     for i in range(len(lista_bichos)):
         if lista_bichos[i]['peligrosidad'] >= 7.0:
-            lista_bichos[i]['es_peligroso'] == True
+            lista_bichos[i]['es_peligroso'] = True
 
 def mostrar_todos_los_bichos():
-    return lista_bichos
-
+    actualizar_estados_bichos()
+    print("=== LISTA DE BICHOS ===")
+    for i in range(len(lista_bichos)):
+        print(f"Especie: {lista_bichos[i]['especie']}")
+        print(f"Tamaño: {lista_bichos[i]['tamano']}")
+        print(f"Peligrosidad: {lista_bichos[i]['peligrosidad']}")
+        if lista_bichos[i]['es_peligroso']:
+            print(f"Estado: PELIGROSO")
+        else: 
+            print("Estado: NO PELIGROSO")
+        print("*********************************************")
 
 def iniciar_programa(): # funcion main
     while True:
@@ -96,13 +101,13 @@ def iniciar_programa(): # funcion main
         if opcion_menu_seleccionada == '1':
             agregar_bicho()
         elif opcion_menu_seleccionada == '2':
-            print(buscar_bicho())
+            buscar_bicho()
         elif opcion_menu_seleccionada == '3':
             eliminar_bicho()
         elif opcion_menu_seleccionada == '4':
             actualizar_estados_bichos()
         elif opcion_menu_seleccionada == '5':
-            print(mostrar_todos_los_bichos())
+            mostrar_todos_los_bichos()
         elif opcion_menu_seleccionada == '6':
             break
         else:
