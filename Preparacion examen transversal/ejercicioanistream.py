51debc046151b418c977ae7e52b74f300819a261
CheelaMS <golhabesoto@proton.me>
2026-07-01 09:48:59 -0400

    preparacion parte 2

--- Preparacion examen transversal/ejercicioanistream.py
+++ Preparacion examen transversal/ejercicioanistream.py
@@ -1,18 +1,35 @@
 series = {
-    'AN001': ['Attack on Titan',    'accion',  'MAPPA',      'M',  False, 'Japon'],
-    'AN002': ['Your Name',          'romance', 'CoMix Wave', 'PG', True,  'Japon'],
-    'AN003': ['One Punch Man',      'comedia', 'J.C.Staff',  'PG', False, 'Japon'],
-    'AN004': ['Kimetsu no Yaiba',   'accion',  'ufotable',   'PG', True,  'Japon'],
-    'AN005': ['No Game No Life',    'isekai',  'Madhouse',   'PG', True,  'Japon'],
-    'AN006': ['Violet Evergarden',  'drama',   'KyoAni',     'G',  True,  'Japon'],
+  'AN001': ['Attack on Titan',  'accion', 'MAPPA',   'M', False, 'Japon'],
+  'AN002': ['Your Name',     'romance', 'CoMix Wave', 'PG', True, 'Japon'],
+  'AN003': ['One Punch Man',   'comedia', 'J.C.Staff', 'PG', False, 'Japon'],
+  'AN004': ['Kimetsu no Yaiba', 'accion', 'ufotable', 'PG', True, 'Japon'],
+  'AN005': ['No Game No Life',  'isekai', 'Madhouse', 'PG', True, 'Japon'],
+  'AN006': ['Violet Evergarden', 'drama', 'KyoAni',  'G', True, 'Japon'],
+  'AN007': ['Spirited Away',   'fantasia', 'Ghibli',  'G', True, 'Japon'],
+  'AN008': ['Fullmetal Alchemist','accion', 'Bones',   'PG', False, 'Japon'],
+  'AN009': ['Death Note',    'suspenso', 'Madhouse', 'M', False, 'Japon'],
+  'AN010': ['Jujutsu Kaisen',  'accion', 'MAPPA',   'M', True, 'Japon'],
+  'AN011': ['Kaguya-sama',    'comedia', 'A-1 Pictures', 'PG', True, 'Japon'],
+  'AN012': ['Chainsaw Man',   'accion', 'MAPPA',   'M', False, 'Japon'],
+  'AN013': ['Sousou no Frieren', 'fantasia', 'Madhouse', 'PG', False, 'Japon']
 }
+
+
+
 catalogo = {
-    'AN001': [9990,  75],
-    'AN002': [4990,   0],
-    'AN003': [7990,  12],
-    'AN004': [8990,  26],
-    'AN005': [5990,  13],
-    'AN006': [6990,  13],
+  'AN001': [9990, 75],
+  'AN002': [4990, 0],
+  'AN003': [7990, 12],
+  'AN004': [8990, 26],
+  'AN005': [5990, 13],
+  'AN006': [6990, 13],
+  'AN007': [4500, 5], # Spirited Away
+  'AN008': [9500, 64], # Fullmetal Alchemist
+  'AN009': [8500, 37], # Death Note
+  'AN010': [8990, 24], # Jujutsu Kaisen
+  'AN011': [7500, 36], # Kaguya-sama
+  'AN012': [8990, 12], # Chainsaw Man
+  'AN013': [9990, 28] # Sousou no Frieren
 }
 
 def mostrarMenu():
@@ -43,6 +60,34 @@ def opcionMenu():
         if opcion in ['1','2','3','4','5','6']:
             return opcion
         print('Ingrese una opcion valida.')
+def busquedaPrecios(min,max):
+    listaresultado=[]
+    listanombre=[]
+    for i in catalogo.items():
+        #print (i[1][0])
+        if i[1][0] >= min and i[1][0] <= max:
+            listaresultado.append(i[0])
+        #print (listaresultado)    
+    for i in listaresultado:
+        nombre= series[i][0] + "--" + i
+        listanombre.append(nombre)
+    listanombre.sort()
+    print("---")    
+    for i in listanombre:    
+        print (i)    
+#busquedaPrecios(5000,10000)
+def actualizarPrecios(code,precio):
+    if code in catalogo:
+        valor=catalogo.get(code)
+        valor.pop(0)
+        valor.insert(0,precio)
+        catalogo.update({code: valor})
+       
+    print (catalogo)    
+#actualizarPrecios('AN004',1000)
+def validacionCodigo():
+    print(wip)
+
 def main():
     while True:
         mostrarMenu()
@@ -50,6 +95,32 @@ def main():
         if seleccion == '1':
             generoBuscar=input('¿Que género desea buscar?: ')
             episodios(generoBuscar)
+        if seleccion == '2':
+            while True:
+                try:
+                    minimo=int(input("Ingrese el precio minimo: "))
+                    while True:
+                        maximo=int(input("Ingrese el precio maximo: "))
+                        if maximo > minimo:
+                            break
+                        print ('Ingrese un valor mayor al precio minimo')
+                    break    
+                except ValueError:
+                    print('Ingrese un dato valido')
+            busquedaPrecios(minimo,maximo)    
+        if seleccion == '3':
+            codigo=input('Ingrese el codigo del anime:').upper()
+            if codigo not in catalogo:
+                print('Codigo del anime no encontrado') 
+            if codigo in catalogo:
+                while True:
+                    try:
+                        precionuevo=int(input('Ingrese el nuevo precio: '))
+                        break
+                    except ValueError:
+                        print('Ingrese un dato valido')
+                actualizarPrecios(codigo,precionuevo)        
+
         if seleccion == '6':
             break    
 
